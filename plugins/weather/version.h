7cb1e7723dbf8a6a83abe8dbbcf2f0d70eb3ccbe
Vadim Dashevskiy <watcherhd@gmail.com>
2012-05-17 19:29:36 +0000

    removed Release & Debug configs  Part 2
    git-svn-id: http://svn.miranda-ng.org/main/trunk@24 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/weather/version.h
+++
@@ -1,6 +0,0 @@
-#define __FILEVERSION_STRING        0,3,8,19
-#define __VERSION_STRING            "0.3.8.19"
-#define __VERSION_DWORD             PLUGIN_MAKE_VERSION(0, 3, 8, 19)
-
-#define BETA		                FALSE
-#define AUTH		                "NoName, borkra"
