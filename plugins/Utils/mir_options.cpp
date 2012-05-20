12274c83beaefd2815190ac84d404510eed0394f
George Hazan <george.hazan@gmail.com>
2012-05-20 19:06:03 +0000

    warning fix
    git-svn-id: http://svn.miranda-ng.org/main/trunk@108 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Utils/mir_options.cpp
+++ plugins/Utils/mir_options.cpp
@@ -17,6 +17,7 @@ not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  
 */
 
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <windows.h>
 #include <commctrl.h>
