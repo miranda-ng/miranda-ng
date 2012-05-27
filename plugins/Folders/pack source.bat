ab30760975535269bef135883d3394760bd4eade
Kirill Volinsky <mataes2007@gmail.com>
2012-05-27 07:00:19 +0000

    projects cleanup
    git-svn-id: http://svn.miranda-ng.org/main/trunk@193 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Folders/pack source.bat
+++
@@ -1,8 +0,0 @@
-for /F "tokens=5-9* delims=. " %%i in (docs\folders_readme.txt) do (call :Pack %%i %%j %%k %%l; exit)
-
-:Pack
-d:\usr\7z\7za.exe a -i@files_source.txt -xr!*.zip -xr!*.ncb -xr!*.user -xr!*.sdf "Folders src %1.%2.%3.%4.zip" @files_source.txt 
-exit
-
-error:
-echo "Error packing Folders"
