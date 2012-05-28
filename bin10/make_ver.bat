22dc0fbf6b116e85b7eefc0674ab326d72b22fa0
George Hazan <george.hazan@gmail.com>
2012-05-28 15:37:58 +0000

    fix for the make_ver.bat
    git-svn-id: http://svn.miranda-ng.org/main/trunk@216 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- bin10/make_ver.bat
+++ bin10/make_ver.bat
@@ -1,24 +1,29 @@
-@echo off
+rem @echo off
+
+for /F "tokens=2,3" %%i in (..\include\m_version.h) do if "%%i"=="MIRANDA_VERSION_FILEVERSION" (set OldVer=%%j)
+echo %OldVer%
 
 for /F "tokens=1,2" %%i in ('svn info miranda32.sln') do call :AddBuild %%i %%j
 goto :eof
 
 :AddBuild
 if (%1) == (Revision:) (
-	for /F "tokens=1,2,3 delims= " %%i in (build.no) do call :WriteVer %%i %%j %2
+	for /F "tokens=1,2,3 delims= " %%i in (build.no) do call :WriteVer %%i %%j %%k %2
    )
 
 goto :eof
 
 :WriteVer
+if "%OldVer%" == "%1,%2,%3,%4" (goto :eof)
+
 copy m_version.h.in ..\include\m_version.h
 
-echo #define MIRANDA_VERSION_FILEVERSION 0,%1,%2,%3                                >>..\include\m_version.h
-echo #define MIRANDA_VERSION_STRING      "0.%1.%2.%3"                              >>..\include\m_version.h
-echo #define MIRANDA_VERSION_DISPLAY     "0.%1.%2 alpha build #%3"                 >>..\include\m_version.h
-echo #define MIRANDA_VERSION_DWORD       MIRANDA_MAKE_VERSION(0, %1, %2, %3)       >>..\include\m_version.h
-echo #define MIRANDA_VERSION_CORE        MIRANDA_MAKE_VERSION(0, %1, %2, 0)        >>..\include\m_version.h
-echo #define MIRANDA_VERSION_CORE_STRING "0.%1.%2.0"                               >>..\include\m_version.h
+echo #define MIRANDA_VERSION_FILEVERSION %1,%2,%3,%4                               >>..\include\m_version.h
+echo #define MIRANDA_VERSION_STRING      "%1.%2.%3.%4"                             >>..\include\m_version.h
+echo #define MIRANDA_VERSION_DISPLAY     "%1.%2.%3 alpha build #%4"                >>..\include\m_version.h
+echo #define MIRANDA_VERSION_DWORD       MIRANDA_MAKE_VERSION(%1, %2, %3, %4)      >>..\include\m_version.h
+echo #define MIRANDA_VERSION_CORE        MIRANDA_MAKE_VERSION(%1, %2, %3, 0)       >>..\include\m_version.h
+echo #define MIRANDA_VERSION_CORE_STRING "%1.%2.%3.0"                              >>..\include\m_version.h
 echo.                                                                              >>..\include\m_version.h
 echo #endif // M_VERSION_H__                                                       >>..\include\m_version.h
 goto :eof
