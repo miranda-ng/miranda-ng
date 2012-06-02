54e24c4eea6734678915d8c7384f4469f7c78be5
Vadim Dashevskiy <watcherhd@gmail.com>
2012-06-02 10:06:10 +0000

    git-svn-id: http://svn.miranda-ng.org/main/trunk@258 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/ListeningTo/players/foo_mlt/foo_mlt.cpp
+++ plugins/ListeningTo/players/foo_mlt/foo_mlt.cpp
@@ -1,40 +1,20 @@
 /* 
-ListeningTo plugin for Miranda IM
-==========================================================================
-Copyright	(C) 2005-2011 Ricardo Pescuma Domenecci
-			(C) 2010-2011 Merlin_de
+Copyright (C) 2005-2009 Ricardo Pescuma Domenecci
 
-PRE-CONDITION to use this code under the GNU General Public License:
- 1. you do not build another Miranda IM plugin with the code without written permission
-    of the autor (peace for the project).
- 2. you do not publish copies of the code in other Miranda IM-related code repositories.
-    This project is already hosted in a SVN and you are welcome to become a contributing member.
- 3. you do not create listeningTo-derivatives based on this code for the Miranda IM project.
-    (feel free to do this for another project e.g. foobar)
- 4. you do not distribute any kind of self-compiled binary of this plugin (we want continuity
-    for the plugin users, who should know that they use the original) you can compile this plugin
-    for your own needs, friends, but not for a whole branch of people (e.g. miranda plugin pack).
- 5. This isn't free beer. If your jurisdiction (country) does not accept
-    GNU General Public License, as a whole, you have no rights to the software
-    until you sign a private contract with its author. !!!
- 6. you always put these notes and copyright notice at the beginning of your code.
-==========================================================================
-
-in case you accept the pre-condition,
-this is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
+This is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
 
 This is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-See the GNU General Public License for more details.
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the
-Free Software Foundation, Inc.,
-59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+You should have received a copy of the GNU Library General Public
+License along with this file; see the file license.txt.  If
+not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  
 */
 
 #include "foobar2000/SDK/foobar2000.h"
