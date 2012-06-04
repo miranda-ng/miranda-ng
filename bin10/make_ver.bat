1200c852713d63ea89ad6d7d63b9ef01d5fc960c
George Hazan <george.hazan@gmail.com>
2012-06-04 16:14:43 +0000

    fix for launching several make_ver.bat files simultaneously
    git-svn-id: http://svn.miranda-ng.org/main/trunk@299 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- bin10/make_ver.bat
+++ bin10/make_ver.bat
@@ -17,14 +17,22 @@ goto :eof
 :WriteVer
 if "%OldVer%" == "%1,%2,%3,%4" (goto :eof)
 
-copy m_version.h.in ..\include\m_version.h
+for /f "delims=/ tokens=1-3" %%a in ("%DATE:~4%") do (
+	for /f "delims=:. tokens=1-4" %%m in ("%TIME: =0%") do (
+		set TempFileName=%TEMP%\basename-%%c-%%b-%%a-%%m%%n%%o%%p
+	)
+)
 
-echo #define MIRANDA_VERSION_FILEVERSION %1,%2,%3,%4                               >>..\include\m_version.h
-echo #define MIRANDA_VERSION_STRING      "%1.%2.%3.%4"                             >>..\include\m_version.h
-echo #define MIRANDA_VERSION_DISPLAY     "%1.%2.%3 alpha build #%4"                >>..\include\m_version.h
-echo #define MIRANDA_VERSION_DWORD       MIRANDA_MAKE_VERSION(%1, %2, %3, %4)      >>..\include\m_version.h
-echo #define MIRANDA_VERSION_CORE        MIRANDA_MAKE_VERSION(%1, %2, %3, 0)       >>..\include\m_version.h
-echo #define MIRANDA_VERSION_CORE_STRING "%1.%2.%3.0"                              >>..\include\m_version.h
-echo.                                                                              >>..\include\m_version.h
-echo #endif // M_VERSION_H__                                                       >>..\include\m_version.h
+copy m_version.h.in "%TempFileName%"
+
+echo #define MIRANDA_VERSION_FILEVERSION %1,%2,%3,%4                               >> "%TempFileName%"
+echo #define MIRANDA_VERSION_STRING      "%1.%2.%3.%4"                             >> "%TempFileName%"
+echo #define MIRANDA_VERSION_DISPLAY     "%1.%2.%3 alpha build #%4"                >> "%TempFileName%"
+echo #define MIRANDA_VERSION_DWORD       MIRANDA_MAKE_VERSION(%1, %2, %3, %4)      >> "%TempFileName%"
+echo #define MIRANDA_VERSION_CORE        MIRANDA_MAKE_VERSION(%1, %2, %3, 0)       >> "%TempFileName%"
+echo #define MIRANDA_VERSION_CORE_STRING "%1.%2.%3.0"                              >> "%TempFileName%"
+echo.                                                                              >> "%TempFileName%"
+echo #endif // M_VERSION_H__                                                       >> "%TempFileName%"
+
+move /Y "%TempFileName%" ..\include\m_version.h
 goto :eof
