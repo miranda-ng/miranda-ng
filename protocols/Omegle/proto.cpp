c396f3cd8095b5808061af6c5b532ebf9b810136
Robert Pösel <robyer@seznam.cz>
2012-06-04 10:25:48 +0000

    Omegle smileys support fix.
    git-svn-id: http://svn.miranda-ng.org/main/trunk@295 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/Omegle/proto.cpp
+++ protocols/Omegle/proto.cpp
@@ -95,7 +95,7 @@ DWORD_PTR OmegleProto::GetCaps( int type, HANDLE hContact )
 	switch(type)
 	{
 	case PFLAGNUM_1:
-		return PF1_IM | PF1_CHAT; // TODO: Why we need PF1_IM to activate smileys? Shouldnt be PF1_CHAT enough?
+		return PF1_CHAT;
 	case PFLAGNUM_2:
 		return PF2_ONLINE;
 	case PFLAGNUM_4:
