2ccff7e383bff7353da806a29d0d969991cb271f
Vadim Dashevskiy <watcherhd@gmail.com>
2012-06-09 13:41:44 +0000

    ChangeKeyboardLayout and ModernOpt compilation fix
    git-svn-id: http://svn.miranda-ng.org/main/trunk@372 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/ChangeKeyboardLayout/main.c
+++ plugins/ChangeKeyboardLayout/main.c
@@ -6,11 +6,7 @@ int hLangpack;
 
 PLUGININFOEX pluginInfoEx={
 	sizeof(PLUGININFOEX),
-	#if defined (_UNICODE)
-		"Change Keyboard Layout (Unicode)",
-	#else
-		"Change Keyboard Layout (ANSI)",
-	#endif
+	"Change Keyboard Layout",
 	VERSION,
 	"Plugin for change keyboard layout of text (multilayout).",
 	"Yasnovidyashii",
@@ -18,27 +14,9 @@ PLUGININFOEX pluginInfoEx={
 	"© 2006-2009 Mikhail Yur'ev",
 	"http://lemnews.com/forum/viewtopic.php?t=1493",
 	0,		//not transient
-	0,		//doesn't replace anything built-in
+	UNICODE_AWARE,		//doesn't replace anything built-in
 	{0xc5ef53a8, 0x80d4, 0x4ce9, { 0xb3, 0x41, 0xec, 0x90, 0xd3, 0xec, 0x91, 0x56 }} //{c5ef53a8-80d4-4ce9-b341-ec90d3ec9156}
 };
-
-PLUGININFO pluginInfo={
-	sizeof(PLUGININFO),
-	#if defined (_UNICODE)
-		"Change Keyboard Layout (UNICODE)",
-	#else
-		"Change Keyboard Layout (ANSI)",
-	#endif
-	VERSION,
-	"Plugin for change keyboard layout of text (multilayout)",
-	"Yasnovidyashii",
-	"Yasnovidyashii@gmail.com",
-	"© 2006-2009 Mikhail Yur'ev",
-	"http://lemnews.com/forum/viewtopic.php?t=1493",
-	0,		//not transient
-	0		//doesn't replace anything built-in	
-};
-
 
 LPCTSTR ptszKeybEng=_T("`1234567890-=\\qwertyuiop[]asdfghjkl;'zxcvbnm,./~!@#$%^&*()_+|QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>?");
 HKL hklEng=(HKL)0x04090409;
@@ -62,24 +40,10 @@ __declspec(dllexport) PLUGININFOEX* MirandaPluginInfoEx(DWORD mirandaVersion)
 	return &pluginInfoEx;
 }
 
-__declspec(dllexport) PLUGININFO* MirandaPluginInfo(DWORD mirandaVersion)
-{
-	dwMirandaVersion = mirandaVersion;
-	return &pluginInfo;
-}
-
-
-#if defined (_UNICODE)
-	static const MUUID interfaces[] = {MIID_CKL_UNICODE, MIID_LAST};
-#else
-	static const MUUID interfaces[] = {MIID_CKL_ANSI, MIID_LAST};
-#endif
+static const MUUID interfaces[] = {{0xc5ef53a8, 0x80d4, 0x4ce9, { 0xb3, 0x41, 0xec, 0x90, 0xd3, 0xec, 0x91, 0x56 }}, MIID_LAST};
 
 __declspec(dllexport) const MUUID* MirandaPluginInterfaces(void)
 {
-	#if defined (_UNICODE)
-		pluginInfoEx.flags = 1; // dynamic UNICODE_AWARE
-	#endif
 	return interfaces;
 }
 
--- plugins/ModernOpt/main.cpp
+++ plugins/ModernOpt/main.cpp
@@ -38,7 +38,7 @@ extern "C" __declspec(dllexport) PLUGININFOEX* MirandaPluginInfoEx(DWORD miranda
 /////////////////////////////////////////////////////////////////////////////////////////
 // MirandaPluginInterfaces - returns the protocol interface to the core
 
-static const MUUID interfaces[] = { MIID_MODERNOPTS, MIID_LAST };
+static const MUUID interfaces[] = {{ 0x621f886b, 0xa7f6, 0x457f, { 0x9d, 0x62, 0x8e, 0xe8, 0x4c, 0x27, 0x59, 0x93 }}, MIID_LAST };
 
 extern "C" __declspec(dllexport) const MUUID* MirandaPluginInterfaces(void)
 {
