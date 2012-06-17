80321cf9de02d54471f4e2fc2c69538e1f1cd454
Kirill Volinsky <mataes2007@gmail.com>
2012-06-17 09:24:41 +0000

    Weather: removed Mataes Pack from description
    git-svn-id: http://svn.miranda-ng.org/main/trunk@451 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/Weather/weather.cpp
+++ protocols/Weather/weather.cpp
@@ -69,7 +69,7 @@ static const PLUGININFOEX pluginInfoEx =
 	sizeof(PLUGININFOEX),
 	"Weather Protocol",
 	__VERSION_DWORD,
-	"Retrieve weather information and display them in your contact list. Mod for Mataes Pack.",
+	"Retrieve weather information and display them in your contact list.",
 	AUTH,
 	"borkra@miranda-im.org",
 	"(c) 2002-2005 NoName, 2005-2010 Boris Krasnovskiy",
