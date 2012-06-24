843145196e0aa520ba23b6f6cffc393118bcf46a
George Hazan <george.hazan@gmail.com>
2012-06-24 22:03:42 +0000

    fix for mir_ptr<T> assignment
    git-svn-id: http://svn.miranda-ng.org/main/trunk@619 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- include/m_system_cpp.h
+++ include/m_system_cpp.h
@@ -45,6 +45,7 @@ template<class T> class mir_ptr
 public:
 	__inline mir_ptr(T* _p) : data(_p) {}
 	__inline ~mir_ptr() { mir_free(data); }
+	__inline T* operator= (T* _p) { if (data) mir_free(data); data = _p; return data; }
 	__inline operator T*() const { return data; }
 	__inline operator INT_PTR() const { return (INT_PTR)data; }
 };
