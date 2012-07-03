da0edbaf0b3b80c04c391dafd09436bad2a0c4bb
George Hazan <george.hazan@gmail.com>
2012-07-03 21:54:07 +0000

    two another victims
    git-svn-id: http://svn.miranda-ng.org/main/trunk@744 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/AvatarHistory/AvatarHistory.cpp
+++ plugins/AvatarHistory/AvatarHistory.cpp
@@ -68,7 +68,6 @@ PLUGININFOEX pluginInfo={
 	"© 2006-2012 Matthew Wild, Ricardo Pescuma Domenecci",
 	"http://pescuma.org/miranda/avatarhist",
 	UNICODE_AWARE,
-	0,		//doesn't replace anything built-in
 	{ 0xdbe8c990, 0x7aa0, 0x458d, { 0xba, 0xb7, 0x33, 0xeb, 0x7, 0x23, 0x8e, 0x71 } } // {DBE8C990-7AA0-458d-BAB7-33EB07238E71}
 };
 
