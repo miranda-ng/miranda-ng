a819777ad787b9a5b96fcd567d531fbec98ad5f7
Vadim Dashevskiy <watcherhd@gmail.com>
2012-07-05 13:30:38 +0000

    BasicHistory compilation fix
    git-svn-id: http://svn.miranda-ng.org/main/trunk@774 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/BasicHistory/src/Scheduler.cpp
+++ plugins/BasicHistory/src/Scheduler.cpp
@@ -20,9 +20,9 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 #include "Options.h"
 #include "ExportManager.h"
 #include "HistoryWindow.h"
-#include "zip\zip.h"
-#include "zip\unzip.h"
-#include "zip\iowin32.h"
+#include "..\zip\zip.h"
+#include "..\zip\unzip.h"
+#include "..\zip\iowin32.h"
 
 // Sorry for plain C implementation
 #define MODULE				"BasicHistory"
