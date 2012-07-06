2d72bd7549e0ffb36a3d3391ca979b75a365f5de
Vlad Mironov <mironych@googlemail.com>
2012-07-06 03:30:29 +0000

    Ключ -I на что существует?
    git-svn-id: http://svn.miranda-ng.org/main/trunk@780 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/AVS/src/version.h
+++ plugins/AVS/src/version.h
@@ -1,4 +1,4 @@
-#include "../../include/m_version.h"
+#include <m_version.h>
 
 #define __FILEVERSION_STRING        MIRANDA_VERSION_FILEVERSION
 #define __VERSION_STRING            MIRANDA_VERSION_STRING
