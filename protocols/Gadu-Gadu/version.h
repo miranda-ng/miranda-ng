251fccf249596219337b9ce8d663b59fd02d3eee
Kirill Volinsky <mataes2007@gmail.com>
2012-07-07 20:09:57 +0000

    GG: m_version path fixed
    git-svn-id: http://svn.miranda-ng.org/main/trunk@832 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/Gadu-Gadu/version.h
+++ protocols/Gadu-Gadu/version.h
@@ -18,7 +18,7 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 ////////////////////////////////////////////////////////////////////////////////
 
-#include "../../include/m_version.h"
+#include "m_version.h"
 
 #define __FILEVERSION_STRING        MIRANDA_VERSION_FILEVERSION
 #define __VERSION_STRING            MIRANDA_VERSION_STRING
