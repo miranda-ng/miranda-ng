8bf6eec3ce7b06af557fa83fe3314441fac0ebbe
Kirill Volinsky <mataes2007@gmail.com>
2012-07-07 20:06:17 +0000

    AIM: m_version path fixed
    git-svn-id: http://svn.miranda-ng.org/main/trunk@830 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/AimOscar/version.h
+++ protocols/AimOscar/version.h
@@ -15,7 +15,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#include "../../include/m_version.h"
+#include "m_version.h"
 
 #define __FILEVERSION_STRING        MIRANDA_VERSION_FILEVERSION
 #define __VERSION_STRING            MIRANDA_VERSION_STRING
