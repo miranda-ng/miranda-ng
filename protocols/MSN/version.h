498999aac9e0077b9b52d5f5e15834798b03b7d0
Kirill Volinsky <mataes2007@gmail.com>
2012-07-07 20:10:50 +0000

    MSN: m_version path fixed
    git-svn-id: http://svn.miranda-ng.org/main/trunk@833 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/MSN/version.h
+++ protocols/MSN/version.h
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "../../include/m_version.h"
+#include "m_version.h"
 
 #define __FILEVERSION_STRING        MIRANDA_VERSION_FILEVERSION
 #define __VERSION_STRING            MIRANDA_VERSION_STRING
