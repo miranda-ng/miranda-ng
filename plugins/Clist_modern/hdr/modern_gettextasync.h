ccb4003b6178f4c195350ae29896fdd442412af4
Vadim Dashevskiy <watcherhd@gmail.com>
2012-07-13 07:37:36 +0000

    git-svn-id: http://svn.miranda-ng.org/main/trunk@938 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Clist_modern/hdr/modern_gettextasync.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-void InitCacheAsync();
-void UninitCacheAsync();
-void gtaRenewText(HANDLE hContact);
-int gtaAddRequest(struct ClcData *dat,struct ClcContact *contact,HANDLE hContact);
