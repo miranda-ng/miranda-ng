8593e7594773c30b35488bb6a45fcc782ed5df0c
Vadim Dashevskiy <watcherhd@gmail.com>
2012-07-20 13:13:28 +0000

    ModernOpt: changed folder structure
    git-svn-id: http://svn.miranda-ng.org/main/trunk@1081 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/ModernOpt/src/main.cpp
+++ plugins/ModernOpt/src/main.cpp
@@ -1,4 +1,3 @@
-
 #include "commonheaders.h"
 
 
--- plugins/ModernOpt/src/modernopt.cpp
+++ plugins/ModernOpt/src/modernopt.cpp
@@ -25,7 +25,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "commonheaders.h"
 #include "modernopt.h"
-#include "../../src/resource.h"
+#include "../../../src/resource.h"
 
 //#define MOPT_SAMPLE_PAGES
 
