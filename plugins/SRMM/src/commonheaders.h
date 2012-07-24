a30cc711b287d245e0edc7c63e53da95c0999203
Vadim Dashevskiy <watcherhd@gmail.com>
2012-07-24 06:59:45 +0000

    SplashScreen, SRMM: changed folder structure
    git-svn-id: http://svn.miranda-ng.org/main/trunk@1151 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/SRMM/src/commonheaders.h
+++ plugins/SRMM/src/commonheaders.h
@@ -25,6 +25,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #define WIN32_LEAN_AND_MEAN
 
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <m_stdhdr.h>
 
 #include <windows.h>
