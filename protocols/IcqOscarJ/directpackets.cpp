d5defc979cd71a5f2bb5176382fa2f5f50f23d1b
George Hazan <george.hazan@gmail.com>
2012-07-28 12:26:56 +0000

    - dead files removal - project cleaning
    git-svn-id: http://svn.miranda-ng.org/main/trunk@1222 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/IcqOscarJ/directpackets.cpp
+++ protocols/IcqOscarJ/directpackets.cpp
@@ -33,7 +33,7 @@ void EncryptDirectPacket(directconnect* dc, icq_packet* p);
 
 void packEmptyMsg(icq_packet *packet);
 
-void packDirectMsgHeader(icq_packet* packet, WORD wDataLen, WORD wCommand, DWORD dwCookie, BYTE bMsgType, BYTE bMsgFlags, WORD wX1, WORD wX2)
+static void packDirectMsgHeader(icq_packet* packet, WORD wDataLen, WORD wCommand, DWORD dwCookie, BYTE bMsgType, BYTE bMsgFlags, WORD wX1, WORD wX2)
 {
 	directPacketInit(packet, 29 + wDataLen);
 	packByte(packet, 2);        /* channel */
