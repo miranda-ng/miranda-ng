2077fd0cb27da069e6bc8588f680b6eed261c8fa
Vadim Dashevskiy <watcherhd@gmail.com>
2012-08-01 10:15:44 +0000

    small warinig fixes
    git-svn-id: http://svn.miranda-ng.org/main/trunk@1296 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/New_GPG/src/commonheaders.h
+++ plugins/New_GPG/src/commonheaders.h
@@ -15,6 +15,9 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef COMMONHEADERS_H
 #define COMMONHEADERS_H
+
+#define _CRT_SECURE_NO_WARNINGS
+
 #define MIRANDA_VER 0x0901
 //windows
 #include <windows.h>
