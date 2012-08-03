428dc4b401dbadfb37282789dfb95f74726cf4f3
Vadim Dashevskiy <watcherhd@gmail.com>
2012-08-03 11:26:09 +0000

    git-svn-id: http://svn.miranda-ng.org/main/trunk@1334 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/SecureIM/src/commonheaders.cpp
+++ plugins/SecureIM/src/commonheaders.cpp
@@ -29,7 +29,7 @@ PLUGININFOEX pluginInfoEx = {
 	sizeof(PLUGININFOEX),
 		MODULENAME,
 		__VERSION_DWORD,
-		MODULENAME" plugin for Miranda IM.",
+		MODULENAME" plugin for Miranda NG.",
 		"Johell, Ghost, Nightwish, __alex, Baloo",
 		"Johell@ifrance.com, baloo@bk.ru",
 		"© 2003 Johell, © 2005-09 Baloo",
