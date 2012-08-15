d2727546aff254497b8679cd36e94506356fe248
Vadim Dashevskiy <watcherhd@gmail.com>
2012-08-15 08:08:27 +0000

    Dbx_mmap_SA: returned to it's own version
    git-svn-id: http://svn.miranda-ng.org/main/trunk@1459 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Dbx_mmap_SA/src/version.h
+++ plugins/Dbx_mmap_SA/src/version.h
@@ -1,6 +1,4 @@
-#include "m_version.h"
-
-#define __FILEVERSION_STRING        MIRANDA_VERSION_FILEVERSION
-#define __VERSION_STRING            MIRANDA_VERSION_STRING
-#define __VERSION_DWORD             MIRANDA_VERSION_DWORD
+#define __FILEVERSION_STRING        0,7,5,4
+#define __VERSION_STRING            "0.7.5.4"
+#define __VERSION_DWORD             PLUGIN_MAKE_VERSION(0, 7, 5, 4)
 #define __PLUGIN_NAME				"Miranda NG secure database driver"
