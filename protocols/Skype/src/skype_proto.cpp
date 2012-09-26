ceae8e098fb5f4e3ae1acef7d8d26822ec8e3633
Kirill Volinsky <mataes2007@gmail.com>
2012-09-26 19:04:35 +0000

    added empty functions for proto
    git-svn-id: http://svn.miranda-ng.org/main/trunk@1667 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/Skype/src/skype_proto.cpp
+++ protocols/Skype/src/skype_proto.cpp
@@ -52,3 +52,178 @@ void CSkypeProto::CreateProtoService(const char *szService, ServiceFunc serviceP
 	strcat( str, szService );
 	::CreateServiceFunctionObj( str, ( MIRANDASERVICEOBJ )*( void** )&serviceProc, this );
 }
+
+HANDLE CSkypeProto::AddToList(int flags, PROTOSEARCHRESULT* psr)
+{
+	return NULL/*hContact*/;
+}
+
+HANDLE CSkypeProto::AddToListByEvent(int flags, int iContact, HANDLE hDbEvent)
+{
+	return NULL;
+}
+
+int CSkypeProto::Authorize(HANDLE hDbEvent)
+{
+	return NULL;
+}
+
+int CSkypeProto::AuthDeny(HANDLE hDbEvent, const PROTOCHAR *reason)
+{
+	return NULL;
+}
+
+int CSkypeProto::AuthRecv(HANDLE hContact, PROTORECVEVENT* pre)
+{
+	return NULL;
+}
+
+int CSkypeProto::AuthRequest(HANDLE hContact, const TCHAR *lptszMessage)
+{
+	return NULL;
+}
+
+HANDLE CSkypeProto::ChangeInfo(int type, void *info_data)
+{
+	return NULL;
+}
+
+HANDLE CSkypeProto::FileAllow(HANDLE hContact, HANDLE hTransfer, const TCHAR *szPath)
+{
+	return NULL;
+}
+
+int CSkypeProto::FileCancel(HANDLE hContact, HANDLE hTransfer)
+{
+	return NULL;
+}
+
+int CSkypeProto::FileDeny(HANDLE hContact, HANDLE hTransfer, const TCHAR*)
+{
+	return NULL;
+}
+
+int CSkypeProto::FileResume(HANDLE, int*, const TCHAR**)
+{
+	return 1;
+}
+
+DWORD_PTR CSkypeProto::GetCaps(int type, HANDLE hContact)
+{
+	return NULL;
+}
+
+HICON CSkypeProto::GetIcon(int iconIndex)
+{
+	return NULL;
+}
+
+int CSkypeProto::GetInfo(HANDLE hContact, int infoType)
+{
+	return NULL;
+}
+
+HANDLE CSkypeProto::SearchBasic(const TCHAR *id)
+{
+	return NULL;
+}
+
+HANDLE CSkypeProto::SearchByEmail(const TCHAR *email)
+{
+	return NULL;
+}
+
+HANDLE CSkypeProto::SearchByName(const TCHAR *pszNick, const TCHAR *pszFirstName, const TCHAR *pszLastName)
+{
+	return NULL;
+}
+
+HWND CSkypeProto::SearchAdvanced(HWND hWndDlg)
+{
+	return NULL;
+}
+
+HWND CSkypeProto::CreateExtendedSearchUI(HWND owner)
+{
+	return NULL;
+}
+
+int CSkypeProto::RecvContacts(HANDLE hContact, PROTORECVEVENT* pre)
+{
+	return NULL;
+}
+
+int CSkypeProto::RecvFile(HANDLE hContact, PROTORECVFILET *pre)
+{
+	return NULL;
+}
+
+int CSkypeProto::RecvMsg(HANDLE hContact, PROTORECVEVENT *pre)
+{
+	return NULL;
+}
+
+int CSkypeProto::RecvUrl(HANDLE, PROTORECVEVENT*)
+{
+	return 1;
+}
+
+int CSkypeProto::SendContacts(HANDLE hContact, int flags, int nContacts, HANDLE* hContactsList)
+{
+	return NULL;
+}
+
+HANDLE CSkypeProto::SendFile(HANDLE hContact, const TCHAR* szDescription, TCHAR** ppszFiles)
+{
+	return NULL;
+}
+
+int CSkypeProto::SendMsg(HANDLE hContact, int flags, const char *lpszMessage)
+{
+	return NULL;
+}
+
+int CSkypeProto::SendUrl(HANDLE, int, const char*)
+{
+	return 1;
+}
+
+int CSkypeProto::SetApparentMode(HANDLE hContact, int mode)
+{
+	return NULL;
+}
+
+int CSkypeProto::SetStatus(int iNewStatus)
+{
+	return NULL;
+}
+
+HANDLE CSkypeProto::GetAwayMsg(HANDLE hContact)
+{
+	return NULL;
+}
+
+int CSkypeProto::RecvAwayMsg(HANDLE, int, PROTORECVEVENT*)
+{
+	return 1;
+}
+
+int CSkypeProto::SendAwayMsg(HANDLE, HANDLE, const char* )
+{
+	return 1;
+}
+
+int CSkypeProto::SetAwayMsg(int m_iStatus, const TCHAR* msg)
+{
+	return NULL;
+}
+
+int CSkypeProto::UserIsTyping(HANDLE hContact, int type)
+{
+	return NULL;
+}
+
+int CSkypeProto::OnEvent(PROTOEVENTTYPE eventType, WPARAM wParam, LPARAM lParam)
+{
+	return NULL;
+}
