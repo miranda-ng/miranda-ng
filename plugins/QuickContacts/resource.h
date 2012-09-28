e2c19fd63bb160a76ea4976b50c57ae76e2dbcf7
Tobias Weimer <wishmaster51@googlemail.com>
2012-09-28 12:23:24 +0000

    forgot to commit a file
    git-svn-id: http://svn.miranda-ng.org/main/trunk@1710 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

---
+++ plugins/QuickContacts/resource.h
@@ -0,0 +1,47 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by resource.rc
+//
+#define ACCEL_TABLE                     103
+#define IDD_OPT                         110
+#define IDD_MAIN                        1000
+#define IDC_USERNAME                    1001
+#define IDC_LASTSENTTO                  1004
+#define IDC_GLOBAL                      1009
+#define IDC_LOCAL                       1010
+#define IDC_PROTOCOLS                   1041
+#define IDC_SUBCONTACTS                 1058
+#define IDC_KEEP_OFFLINE                1059
+#define IDC_HIDE_OFFLINE                1060
+#define IDC_SHOW_ALL_CONTACTS           1061
+#define IDC_APPEND_GROUP                1061
+#define IDC_MESSAGE                     1062
+#define IDC_GROUP_COLUMN                1062
+#define IDC_FILE                        1063
+#define IDC_GROUP_LEFT                  1063
+#define IDC_URL                         1064
+#define IDC_USERINFO                    1065
+#define IDC_HISTORY                     1066
+#define IDC_MENU                        1067
+#define IDC_VOICE                       1068
+#define HOTKEY_FILE                     40001
+#define HOTKEY_URL                      40002
+#define HOTKEY_INFO                     40003
+#define HOTKEY_HISTORY                  40004
+#define HOTKEY_ALL_CONTACTS             40005
+#define HOTKEY_MENU                     40006
+#define HOTKEY_VOICE                    40007
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        119
+#define _APS_NEXT_COMMAND_VALUE         40008
+#define _APS_NEXT_CONTROL_VALUE         1063
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
