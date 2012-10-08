a79efabe45cbe4f2ccc87ee5928e1dc0fd408440
George Hazan <george.hazan@gmail.com>
2012-10-08 19:38:29 +0000

    compilation fix
    git-svn-id: http://svn.miranda-ng.org/main/trunk@1824 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Watrack/make.bat
+++ plugins/Watrack/make.bat
@@ -1,26 +1,25 @@
-@echo off
-set myopts=-dMiranda
+rem @echo off
+md tmp
+set myopts=-Sd -dMiranda -FEtmp -Fi..\Utils.pas -Fu..\Utils.pas -Fu..\..\include\delphi -Fu..\..\include\delphi\reserve
 set dprname=watrack.dpr
 
-..\delphi\brcc32.exe res\watrack.rc         -fores\watrack.res
-..\delphi\brcc32.exe lastfm\lastfm.rc       -folastfm\lastfm.res
-..\delphi\brcc32.exe myshows\myshows.rc     -fomyshows\myshows.res
-..\delphi\brcc32.exe players\mradio.rc      -foplayers\mradio.res
-..\delphi\brcc32.exe kolframe\frm.rc        -fokolframe\frm.res
-..\delphi\brcc32.exe popup\popup.rc         -fopopup\popup.res
-..\delphi\brcc32.exe proto\proto.rc         -foproto\proto.res
-..\delphi\brcc32.exe stat\stat.rc           -fostat\stat.res
-..\delphi\brcc32.exe status\status.rc       -fostatus\status.res
-..\delphi\brcc32.exe templates\templates.rc -fotemplates\templates.res
+rem brcc32.exe res\watrack.rc         -fores\watrack.res
+rem brcc32.exe lastfm\lastfm.rc       -folastfm\lastfm.res
+rem brcc32.exe myshows\myshows.rc     -fomyshows\myshows.res
+rem brcc32.exe players\mradio.rc      -foplayers\mradio.res
+rem brcc32.exe kolframe\frm.rc        -fokolframe\frm.res
+rem brcc32.exe popup\popup.rc         -fopopup\popup.res
+rem brcc32.exe proto\proto.rc         -foproto\proto.res
+rem brcc32.exe stat\stat.rc           -fostat\stat.res
+rem brcc32.exe status\status.rc       -fostatus\status.res
+rem brcc32.exe templates\templates.rc -fotemplates\templates.res
 
 if /i '%1' == 'fpc' (
-  ..\FPC\bin\fpc.exe %myopts% %dprname% %2 %3 %4 %5 %6 %7 %8 %9
+  fpc.exe %myopts% %dprname% %2 %3 %4 %5 %6 %7 %8 %9
 ) else if /i '%1' == 'fpc64' (
-  ..\FPC\bin64\ppcrossx64.exe %myopts% %dprname% %2 %3 %4 %5 %6 %7 %8 %9
-) else if /i '%1' == 'xe2' (
-  ..\XE2\BIN\dcc32.exe%myopts% %dprname% %2 %3 %4 %5 %6 %7 %8 %9
-) else if /i '%1' == 'xe64' (
-  ..\XE2\BIN\dcc64.exe %myopts% %dprname% %2 %3 %4 %5 %6 %7 %8 %9
-) else (
-  ..\delphi\dcc32 -b -dKOL_MCK -dUNICODE_CTRLS %myopts% %dprname% %1 %2 %3 %4 %5 %6 %7 %8 %9
+  ppcrossx64.exe %myopts% %dprname% %2 %3 %4 %5 %6 %7 %8 %9
 )
+
+move tmp\watrack.dll .
+del /Q tmp\*
+rd tmp
