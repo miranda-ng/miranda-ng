9ce93f0f5570a1d7910577c9dbbe195f40ec4ab4
George Hazan <george.hazan@gmail.com>
2012-10-08 15:52:19 +0000

    fix for a makefile
    git-svn-id: http://svn.miranda-ng.org/main/trunk@1818 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/!NotAdopted/ShlExt/make.bat
+++ plugins/!NotAdopted/ShlExt/make.bat
@@ -1,9 +1,12 @@
 @echo off
-REM -Fi/u are for include/unit dirs 
+REM -Fi/u are for include/unit dirs
 REM -Mdelphi is delphi mode
 REM -WG - graphical app
 REM -v0 turn off warnings
 REM -O2 -Os // optimise
 REM -Rintel (intel style asm)
 REM -WB (relocatable) -WR (relocate)
-fpc shlext.dpr -Fiinc -Fuinc -Mdelphi -WG -O2 -Os -Rintel -WR -WB49ac0000 -v0
+md out
+fpc shlext.dpr -FEout -Fi..\..\include;..\..\include\delphi -Fu..\..\include;..\..\include\delphi -Mdelphi -WG -O2 -Os -Rintel -WR -WB49ac0000 -v0
+del /Q out\*
+rd /Q out
