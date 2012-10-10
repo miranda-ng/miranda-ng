816323d5759fb9fb15e11676263eed017256f1e4
George Hazan <george.hazan@gmail.com>
2012-10-10 17:28:20 +0000

    version bump
    git-svn-id: http://svn.miranda-ng.org/main/trunk@1866 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/PluginUpdater/src/Version.h
+++ plugins/PluginUpdater/src/Version.h
@@ -1,7 +1,7 @@
 #define __MAJOR_VERSION           0
-#define __MINOR_VERSION           0
-#define __RELEASE_NUM             1
-#define __BUILD_NUM               3
+#define __MINOR_VERSION           1
+#define __RELEASE_NUM             0
+#define __BUILD_NUM               1
 
 #define __FILEVERSION_STRING      __MAJOR_VERSION,__MINOR_VERSION,__RELEASE_NUM,__BUILD_NUM
 #define __FILEVERSION_DOTS        __MAJOR_VERSION.__MINOR_VERSION.__RELEASE_NUM.__BUILD_NUM
@@ -14,7 +14,7 @@
 #define __INTERNAL_NAME           "PluginUpdater"
 #define __FILENAME                "PluginUpdater.dll"
 #define __DESCRIPTION             "Plugin updater for Miranda NG."
-#define __AUTHOR                  "Mataes, ZERO_BiT"
-#define __AUTHOREMAIL             "mataes2007@gmail.com, zero-bit@mail.ru"
+#define __AUTHOR                  "Mataes, George Hazan"
+#define __AUTHOREMAIL             "mataes2007@gmail.com"
 #define __AUTHORWEB               "http://miranda-ng.org/"
-#define __COPYRIGHT               "© 2012 Mataes, 2007 ZERO_BiT"
+#define __COPYRIGHT               "© 2012 Mataes, George Hazan"
