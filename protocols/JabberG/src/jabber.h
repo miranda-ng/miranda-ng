e2f7c4a63d22016c2cc81407ce9ec2bacae736d6
Kirill Volinsky <mataes2007@gmail.com>
2012-10-14 16:13:04 +0000

    compilation fix
    git-svn-id: http://svn.miranda-ng.org/main/trunk@1931 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/JabberG/src/jabber.h
+++ protocols/JabberG/src/jabber.h
@@ -79,7 +79,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <m_fingerprint.h>
 #include <win2k.h>
 
-#include "../../plugins/zlib/zlib.h"
+#include "../../plugins/zlib/src/zlib.h"
 
 #include "resource.h"
 #include "version.h"
