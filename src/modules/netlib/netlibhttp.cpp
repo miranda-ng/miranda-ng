ba2a7ef4deda8cfc3d0291ddf38b83c422f06fee
Kirill Volinsky <mataes2007@gmail.com>
2012-10-14 16:14:39 +0000

    compilation fix
    git-svn-id: http://svn.miranda-ng.org/main/trunk@1933 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- src/modules/netlib/netlibhttp.cpp
+++ src/modules/netlib/netlibhttp.cpp
@@ -21,7 +21,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 #include "..\..\core\commonheaders.h"
-#include "../plugins/zlib/zlib.h"
+#include "../plugins/zlib/src/zlib.h"
 #include "netlib.h"
 
 #define HTTPRECVHEADERSTIMEOUT   30000  //in ms
