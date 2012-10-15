7f3a660fe4e1f22c46c4a148f055e977f1f625f7
George Hazan <george.hazan@gmail.com>
2012-10-15 19:22:02 +0000

    useless header file removed
    git-svn-id: http://svn.miranda-ng.org/main/trunk@1950 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Dbx_tree/src/DatabaseLink.h
+++ plugins/Dbx_tree/src/DatabaseLink.h
@@ -24,5 +24,4 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <windows.h>
 #include "Interface.h"
 #include "Database.h"
-#include "Services.h"
 #include "Compatibility.h"
