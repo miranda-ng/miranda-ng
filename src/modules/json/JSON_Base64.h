8768290e05cf27d6d291639fb64e636363ddbb67
Kirill Volinsky <mataes2007@gmail.com>
2012-11-19 14:43:17 +0000

    tabs and spaces cleaning
    git-svn-id: http://svn.miranda-ng.org/main/trunk@2374 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- src/modules/json/JSON_Base64.h
+++ src/modules/json/JSON_Base64.h
@@ -6,8 +6,8 @@
 
 class JSONBase64 {
 public:
-    static json_string json_encode64(const unsigned char * binary, size_t bytes);
-    static std::string json_decode64(const json_string & encoded);
+	static json_string json_encode64(const unsigned char * binary, size_t bytes);
+	static std::string json_decode64(const json_string & encoded);
 };
 
 #endif
