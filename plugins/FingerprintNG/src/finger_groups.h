5eff7a74aa83c3d2e6b328a317bffb8712fb36ae
Vadim Dashevskiy <watcherhd@gmail.com>
2012-12-04 10:30:24 +0000

    - FingerprintNG: cosmetics
    git-svn-id: http://svn.miranda-ng.org/main/trunk@2637 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/FingerprintNG/src/finger_groups.h
+++ plugins/FingerprintNG/src/finger_groups.h
@@ -1,122 +1,122 @@
 case MIRANDA_CASE:
 	if ( db_get_b(NULL, "Finger", "GroupMiranda", 1))
-		return _T("Client Icons/Miranda clients");
+		return _T("Client icons/Miranda clients");
 	break;
 case MIRANDA_VERSION_CASE:
 	if ( db_get_b(NULL, "Finger", "GroupMirandaVersion", 1))
-		return _T("Client Icons/Miranda clients");
+		return _T("Client icons/Miranda clients");
 	break;
 case MIRANDA_PACKS_CASE:
 	if ( db_get_b(NULL, "Finger", "GroupMirandaPacks", 1))
-		return _T("Client Icons/Miranda clients/pack overlays");
+		return _T("Client icons/Miranda clients/Pack overlays");
 	break;
 
 
 case MULTI_CASE:
 	if ( db_get_b(NULL, "Finger", "GroupMulti", 1))
-		return _T("Client Icons/multi-proto clients");
+		return _T("Client icons/Multi-proto clients");
 	break;
 
 
 case AIM_CASE:
 	if ( db_get_b(NULL, "Finger", "GroupAIM", 1))
-		return _T("Client Icons/AIM clients");
+		return _T("Client icons/AIM clients");
 	break;
 
 case GG_CASE:
 	if ( db_get_b(NULL, "Finger", "GroupGG", 1))
-		return _T("Client Icons/Gadu-Gadu clients");
+		return _T("Client icons/Gadu-Gadu clients");
 	break;
 
 	break;
 case ICQ_CASE:
 	if ( db_get_b(NULL, "Finger", "GroupICQ", 1))
-		return _T("Client Icons/ICQ clients");
+		return _T("Client icons/ICQ clients");
 	break;
 
 case IRC_CASE:
 	if ( db_get_b(NULL, "Finger", "GroupIRC", 1))
-		return _T("Client Icons/IRC clients");
+		return _T("Client icons/IRC clients");
 	break;
 
 case JABBER_CASE:
 	if ( db_get_b(NULL, "Finger", "GroupJabber", 1))
-		return _T("Client Icons/Jabber clients");
+		return _T("Client icons/Jabber clients");
 	break;
 
 case MRA_CASE:
 	if ( db_get_b(NULL, "Finger", "GroupMRA", 1))
-		return _T("Client Icons/MRA clients");
+		return _T("Client icons/MRA clients");
 	break;
 
 case MSN_CASE:
 	if ( db_get_b(NULL, "Finger", "GroupMSN", 1))
-		return _T("Client Icons/MSN clients");
+		return _T("Client icons/MSN clients");
 	break;
 
 case QQ_CASE:
 	if ( db_get_b(NULL, "Finger", "GroupQQ", 1))
-		return _T("Client Icons/QQ clients");
+		return _T("Client icons/QQ clients");
 	break;
 
 case RSS_CASE:
 	if ( db_get_b(NULL, "Finger", "GroupRSS", 1))
-		return _T("Client Icons/RSS clients");
+		return _T("Client icons/RSS clients");
 	break;
 
 case TLEN_CASE:
 	if ( db_get_b(NULL, "Finger", "GroupTlen", 1))
-		return _T("Client Icons/Tlen clients");
+		return _T("Client icons/Tlen clients");
 	break;
 
 case WEATHER_CASE:
 	if ( db_get_b(NULL, "Finger", "GroupWeather", 1))
-		return _T("Client Icons/Weather clients");
+		return _T("Client icons/Weather clients");
 	break;
 
 case YAHOO_CASE:
 	if ( db_get_b(NULL, "Finger", "GroupYahoo", 1))
-		return _T("Client Icons/Yahoo clients");
+		return _T("Client icons/Yahoo clients");
 	break;
 
 
 case OTHER_PROTOS_CASE:
 	if ( db_get_b(NULL, "Finger", "GroupOtherProtos", 1))
-		return _T("Client Icons/other protocols");
+		return _T("Client icons/Other protocols");
 	break;
 
 case OTHERS_CASE:
 	if ( db_get_b(NULL, "Finger", "GroupOthers", 1))
-		return _T("Client Icons/other icons");
+		return _T("Client icons/Other icons");
 	break;
 
 
 case OVERLAYS_RESOURCE_CASE:
 	if ( db_get_b(NULL, "Finger", "GroupOverlaysResource", 1))
-		return _T("Client Icons/overlays/resource");
+		return _T("Client icons/Overlays/Resource");
 	break;
 
 case OVERLAYS_PLATFORM_CASE:
 	if ( db_get_b(NULL, "Finger", "GroupOverlaysPlatform", 1))
-		return _T("Client Icons/overlays/platform");
+		return _T("Client icons/Overlays/Platform");
 	break;
 
 case OVERLAYS_PROTO_CASE:
 	if ( db_get_b(NULL, "Finger", "GroupOverlaysProtos", 1))
-		return _T("Client Icons/overlays/protocol");
+		return _T("Client icons/Overlays/Protocol");
 	break;
 
 case OVERLAYS_SECURITY_CASE:
 	if ( db_get_b(NULL, "Finger", "GroupOtherProtos", 1))
-		return _T("Client Icons/overlays/security");
+		return _T("Client icons/Overlays/Security");
 	break;
 
 /*
 case OVERLAYS_RESOURCE_ALT_CASE:
 	if ( db_get_b(NULL, "Finger", "GroupOverlays", 1))
-		return _T("Client Icons/overlays");
+		return _T("Client icons/Overlays");
 	break;
 */
 
 default:
-	return _T("Client Icons");
+	return _T("Client icons");
