5fbf250098265c75653f41a87bfa6c579b56ea74
Kirill Volinsky <mataes2007@gmail.com>
2012-12-23 20:27:07 +0000

    fix for memory corruption
    git-svn-id: http://svn.miranda-ng.org/main/trunk@2820 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- src/core/stdauth/authdialogs.cpp
+++ src/core/stdauth/authdialogs.cpp
@@ -190,27 +190,27 @@ INT_PTR CALLBACK DlgProcAuthReq(HWND hwndDlg, UINT msg, WPARAM wParam, LPARAM lP
 			TCHAR name[128] = _T("");
 			int off = 0;
 			if (firstT[0] && lastT[0])
-				off = mir_sntprintf(name, SIZEOF(name), _T("%s %s"), firstT, lastT);
+				off = mir_sntprintf(name, SIZEOF(name), _T("%s %s"), (TCHAR*)firstT, (TCHAR*)lastT);
 			else if (firstT[0])
-				off = mir_sntprintf(name, SIZEOF(name), _T("%s"), firstT);
+				off = mir_sntprintf(name, SIZEOF(name), _T("%s"), (TCHAR*)firstT);
 			else if (lastT[0])
-				off = mir_sntprintf(name, SIZEOF(name), _T("%s"), lastT);
+				off = mir_sntprintf(name, SIZEOF(name), _T("%s"), (TCHAR*)lastT);
 			if (nickT[0]) {
 				if (off)
-					mir_sntprintf(name + off, SIZEOF(name) - off, _T(" (%s)"), nickT);
+					mir_sntprintf(name + off, SIZEOF(name) - off, _T(" (%s)"), (TCHAR*)nickT);
 				else
-					mir_sntprintf(name, SIZEOF(name), _T("%s"), nickT);
+					mir_sntprintf(name, SIZEOF(name), _T("%s"), (TCHAR*)nickT);
 			}
 			if ( !name[0])
 				_tcscpy(name, TranslateT("<Unknown>"));
 
 			TCHAR hdr[256];
 			if (uin && emailT[0])
-				mir_sntprintf(hdr, SIZEOF(hdr), TranslateT("%s requested authorization\n%u (%s) on %s"), name, uin, emailT, acc->tszAccountName);
+				mir_sntprintf(hdr, SIZEOF(hdr), TranslateT("%s requested authorization\n%u (%s) on %s"), name, uin, (TCHAR*)emailT, acc->tszAccountName);
 			else if (uin)
 				mir_sntprintf(hdr, SIZEOF(hdr), TranslateT("%s requested authorization\n%u on %s"), name, uin, acc->tszAccountName);
 			else
-				mir_sntprintf(hdr, SIZEOF(hdr), TranslateT("%s requested authorization\n%s on %s"), name, emailT[0] ? emailT : TranslateT("(Unknown)"), acc->tszAccountName);
+				mir_sntprintf(hdr, SIZEOF(hdr), TranslateT("%s requested authorization\n%s on %s"), name, emailT[0] ? (TCHAR*)emailT : TranslateT("(Unknown)"), acc->tszAccountName);
 
 			SetDlgItemText(hwndDlg, IDC_HEADERBAR, hdr);
 			SetDlgItemText(hwndDlg, IDC_REASON, reasonT);
