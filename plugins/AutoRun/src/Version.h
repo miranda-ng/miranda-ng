2538efc836bb5e7389457b5e663c3b03eadf7fad
Kirill Volinsky <mataes2007@gmail.com>
2013-01-04 16:31:34 +0000

    removed not used defines
    git-svn-id: http://svn.miranda-ng.org/main/trunk@2963 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/AutoRun/src/Version.h
+++ plugins/AutoRun/src/Version.h
@@ -4,14 +4,8 @@
 #define __BUILD_NUM					1
 
 #define __FILEVERSION_STRING		__MAJOR_VERSION,__MINOR_VERSION,__RELEASE_NUM,__BUILD_NUM
-#define __FILEVERSION_DOTS			__MAJOR_VERSION.__MINOR_VERSION.__RELEASE_NUM.__BUILD_NUM
-
-#define __STRINGIFY_IMPL(x)			#x
-#define __STRINGIFY(x)				__STRINGIFY_IMPL(x)
-#define __VERSION_STRING			__STRINGIFY(__FILEVERSION_DOTS)
 
 #define __PLUGIN_NAME				"AutoRun"
-#define __INTERNAL_NAME				"AutoRun"
 #define __FILENAME					"AutoRun.dll"
 #define __DESCRIPTION 				"This plugin is a simple way to enable/disable to launch Miranda NG with system startup."
 #define __AUTHOR					"Sergey V. Gershovich a.k.a. Jazzy$ (fixed by Wolfram3D)"
--- plugins/AutoRun/src/autorun.h
+++ plugins/AutoRun/src/autorun.h
@@ -1,5 +1,4 @@
 #define _CRT_SECURE_NO_WARNINGS
-#define MIRANDA_VER 0x0A00
 
 #include <windows.h>
 
