7ff07d109833bb4e9d4bdb0c69cd3e27dd39cb56
George Hazan <george.hazan@gmail.com>
2013-01-06 22:41:53 +0000

    fix for loading Tlen proto in 64-bit mode
    git-svn-id: http://svn.miranda-ng.org/main/trunk@3002 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/Tlen/src/tlen.cpp
+++ protocols/Tlen/src/tlen.cpp
@@ -74,11 +74,7 @@ extern "C" __declspec(dllexport) PLUGININFOEX *MirandaPluginInfoEx(DWORD miranda
 	return &pluginInfoEx;
 }
 
-extern "C" __declspec(dllexport) const MUUID interfaces[] = {MIID_PROTOCOL, MIID_LAST};
-extern "C" __declspec(dllexport) const MUUID* MirandaPluginInterfaces(void)
-{
-	return interfaces;
-}
+extern "C" __declspec(dllexport) const MUUID MirandaInterfaces[] = {MIID_PROTOCOL, MIID_LAST};
 
 static IconItem iconList[] =
 {
