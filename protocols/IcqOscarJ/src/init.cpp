7850772c318d3d5c227ab5674faa50e33235bb1c
Vadim Dashevskiy <watcherhd@gmail.com>
2013-01-20 20:24:13 +0000

    - typo fix
    git-svn-id: http://svn.miranda-ng.org/main/trunk@3206 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/IcqOscarJ/src/init.cpp
+++ protocols/IcqOscarJ/src/init.cpp
@@ -4,7 +4,7 @@
 //
 // Copyright © 2000-2001 Richard Hughes, Roland Rabien, Tristan Van de Vreede
 // Copyright © 2001-2002 Jon Keating, Richard Hughes
-// Copyright © 2002-2004 Martin Öberg, Sam Kothari, Robert Rainwater
+// Copyright © 2002-2004 Martin Цberg, Sam Kothari, Robert Rainwater
 // Copyright © 2004-2010 Joe Kucera
 //
 // This program is free software; you can redistribute it and/or
@@ -46,7 +46,7 @@ PLUGININFOEX pluginInfo = {
 	"ICQ protocol support for Miranda NG.",
 	"Joe Kucera, Bio, Martin Öberg, Richard Hughes, Jon Keating, etc",
 	"jokusoftware@miranda-im.org",
-	"(C) 2000-2010 M.Öberg, R.Hughes, J.Keating, Bio, Angeli-Ka, G.Hazan, J.Kucera",
+	"(C) 2000-2010 M.Цberg, R.Hughes, J.Keating, Bio, Angeli-Ka, G.Hazan, J.Kucera",
 	"http://miranda-ng.org/",
 	UNICODE_AWARE,   //doesn't replace anything built-in
 	{0x73a9615c, 0x7d4e, 0x4555, {0xba, 0xdb, 0xee, 0x5, 0xdc, 0x92, 0x8e, 0xff}} // {73A9615C-7D4E-4555-BADB-EE05DC928EFF}
