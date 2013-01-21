7dc4ae6ba8aacf7c9b79592b95861e9b3365c984
Vadim Dashevskiy <watcherhd@gmail.com>
2013-01-21 20:37:09 +0000

    - TabSRMM: weirs symbols clean up
    git-svn-id: http://svn.miranda-ng.org/main/trunk@3220 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/TabSRMM/src/utils.cpp
+++ plugins/TabSRMM/src/utils.cpp
@@ -47,8 +47,8 @@ HANDLE			CWarning::hWindowList = 0;
 static TCHAR *w_bbcodes_begin[] = { _T("[b]"), _T("[i]"), _T("[u]"), _T("[s]"), _T("[color=") };
 static TCHAR *w_bbcodes_end[] = { _T("[/b]"), _T("[/i]"), _T("[/u]"), _T("[/s]"), _T("[/color]") };
 
-static TCHAR *formatting_strings_begin[] = { _T("b1 "), _T("i1 "), _T("u1 "), _T("s1 "), _T("c1 ") };
-static TCHAR *formatting_strings_end[] = { _T("b0 "), _T("i0 "), _T("u0 "), _T("s0 "), _T("c0 ") };
+static TCHAR *formatting_strings_begin[] = { _T("b1 "), _T("i1 "), _T("u1 "), _T("s1 "), _T("c1 ") };
+static TCHAR *formatting_strings_end[] = { _T("b0 "), _T("i0 "), _T("u0 "), _T("s0 "), _T("c0 ") };
 
 #define NR_CODES 5
 
