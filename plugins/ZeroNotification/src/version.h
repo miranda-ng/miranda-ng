ebddf3e1b1b0b9e9ca72dd04a743a839ac272876
Vadim Dashevskiy <watcherhd@gmail.com>
2013-01-29 10:16:08 +0000

    - muuid standardization
    git-svn-id: http://svn.miranda-ng.org/main/trunk@3333 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/ZeroNotification/src/version.h
+++ plugins/ZeroNotification/src/version.h
@@ -8,5 +8,3 @@
 #define DESCRIPTION				"Adds options to automatically disable notifications when a protocol is in a certain status."
 #define AUTHOR					"bidyut (<0.0.0.5), Maat"
 #define COPYRIGHT				"© 2005 bidyut, © 2009 Maat"
-// {47D489D3-310D-4ef6-BD05-699FFFD5A4AA}
-#define UID { 0x47d489d3, 0x310d, 0x4ef6, { 0xbd, 0x5, 0x69, 0x9f, 0xff, 0xd5, 0xa4, 0xaa } }
