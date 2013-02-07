cdb28ade7ab614517ed29923ba4a02a326c2ba37
Kirill Volinsky <mataes2007@gmail.com>
2013-02-07 06:44:01 +0000

    removed not used defines
    git-svn-id: http://svn.miranda-ng.org/main/trunk@3458 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/AVS/src/version.h
+++ plugins/AVS/src/version.h
@@ -4,14 +4,8 @@
 #define __BUILD_NUM					1
 
 #define __FILEVERSION_STRING		__MAJOR_VERSION,__MINOR_VERSION,__RELEASE_NUM,__BUILD_NUM
-#define __FILEVERSION_DOTS			__MAJOR_VERSION.__MINOR_VERSION.__RELEASE_NUM.__BUILD_NUM
-
-#define __STRINGIFY_IMPL(x)			#x
-#define __STRINGIFY(x)				__STRINGIFY_IMPL(x)
-#define __VERSION_STRING			__STRINGIFY(__FILEVERSION_DOTS)
 
 #define __PLUGIN_NAME				"Avatar service"
-#define __INTERNAL_NAME				"AVS"
 #define __FILENAME					"AVS.dll"
 #define __DESCRIPTION 				"Loads and manages contact pictures for other plugins."
 #define __AUTHOR					"Nightwish, Pescuma"
