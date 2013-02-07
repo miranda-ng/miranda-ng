b0a7cd54833fdd9c159026f9c5eed592018518dd
Kirill Volinsky <mataes2007@gmail.com>
2013-02-07 06:43:51 +0000

    removed not used defines
    git-svn-id: http://svn.miranda-ng.org/main/trunk@3457 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/AvatarHistory/src/Version.h
+++ plugins/AvatarHistory/src/Version.h
@@ -4,14 +4,8 @@
 #define __BUILD_NUM					3
 
 #define __FILEVERSION_STRING		__MAJOR_VERSION,__MINOR_VERSION,__RELEASE_NUM,__BUILD_NUM
-#define __FILEVERSION_DOTS			__MAJOR_VERSION.__MINOR_VERSION.__RELEASE_NUM.__BUILD_NUM
 
-#define __STRINGIFY_IMPL(x)			#x
-#define __STRINGIFY(x)				__STRINGIFY_IMPL(x)
-#define __VERSION_STRING			__STRINGIFY(__FILEVERSION_DOTS)
-
-#define __PLUGIN_NAME				"Avatar History"
-#define __INTERNAL_NAME				"AvatarHistory"
+#define __PLUGIN_NAME				"Avatar history"
 #define __FILENAME					"AvatarHistory.dll"
 #define __DESCRIPTION 				"This plugin keeps backups of all your contacts' avatar changes and/or shows popups."
 #define __AUTHOR					"Matthew Wild (MattJ), Ricardo Pescuma Domenecci"
