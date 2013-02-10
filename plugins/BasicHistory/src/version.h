ce189c9e1f83da2ba8dbc8bea939320d8e72bc85
Kirill Volinsky <mataes2007@gmail.com>
2013-02-10 10:06:42 +0000

    removed not used defines
    git-svn-id: http://svn.miranda-ng.org/main/trunk@3521 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/BasicHistory/src/version.h
+++ plugins/BasicHistory/src/version.h
@@ -4,14 +4,8 @@
 #define __BUILD_NUM					9
  
 #define __FILEVERSION_STRING		__MAJOR_VERSION,__MINOR_VERSION,__RELEASE_NUM,__BUILD_NUM
-#define __FILEVERSION_DOTS			__MAJOR_VERSION.__MINOR_VERSION.__RELEASE_NUM.__BUILD_NUM
 
-#define __STRINGIFY_IMPL(x)			#x
-#define __STRINGIFY(x)				__STRINGIFY_IMPL(x)
-#define __VERSION_STRING			__STRINGIFY(__FILEVERSION_DOTS)
-
-#define __PLUGIN_NAME				"Basic History"
-#define __INTERNAL_NAME				"BasicHistory"
+#define __PLUGIN_NAME				"Basic history"
 #define __FILENAME					"BasicHistory.dll"
 #define __DESCRIPTION 				"History viewer for Miranda NG."
 #define __AUTHOR					"Krzysztof Kral"
