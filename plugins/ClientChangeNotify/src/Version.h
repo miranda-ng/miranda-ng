b4ff82df0b826fa17d6e5dd1f3b337995b6fe8d1
Kirill Volinsky <mataes2007@gmail.com>
2013-02-10 11:10:12 +0000

    removed not used defines
    git-svn-id: http://svn.miranda-ng.org/main/trunk@3526 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/ClientChangeNotify/src/Version.h
+++ plugins/ClientChangeNotify/src/Version.h
@@ -4,16 +4,10 @@
 #define __BUILD_NUM					3
 
 #define __FILEVERSION_STRING		__MAJOR_VERSION,__MINOR_VERSION,__RELEASE_NUM,__BUILD_NUM
-#define __FILEVERSION_DOTS			__MAJOR_VERSION.__MINOR_VERSION.__RELEASE_NUM.__BUILD_NUM
 
-#define __STRINGIFY_IMPL(x)			#x
-#define __STRINGIFY(x)				__STRINGIFY_IMPL(x)
-#define __VERSION_STRING			__STRINGIFY(__FILEVERSION_DOTS)
-
-#define __PLUGIN_NAME				"Client Change Notify"
-#define __INTERNAL_NAME				"ClientChangeNotify"
+#define __PLUGIN_NAME				"Client change notify"
 #define __FILENAME					"ClientChangeNotify.dll"
-#define __DESCRIPTION 				"ClientChangeNotify plugin for Miranda NG."
+#define __DESCRIPTION 				"Shows a notification when someone in your contact list changes his client."
 #define __AUTHOR					"Deathdemon"
 #define __AUTHOREMAIL				"dchervov@yahoo.com"
 #define __AUTHORWEB					"http://miranda-ng.org/"
