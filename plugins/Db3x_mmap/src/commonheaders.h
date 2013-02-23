19074442841b7d7a67d402262bc9d80a86d3c8e5
Kirill Volinsky <mataes2007@gmail.com>
2013-02-23 15:04:33 +0000

    removed not used headers
    git-svn-id: http://svn.miranda-ng.org/main/trunk@3730 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Db3x_mmap/src/commonheaders.h
+++ plugins/Db3x_mmap/src/commonheaders.h
@@ -24,30 +24,16 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define _CRT_SECURE_NO_WARNINGS
 #define _WIN32_WINNT 0x0501
 
-#include "m_stdhdr.h"
-
 #include <windows.h>
-#include <commctrl.h>
-#include <stdio.h>
-#include <string.h>
 #include <time.h>
-#include <stddef.h>
 #include <process.h>
-#include <io.h>
-#include <string.h>
-#include <direct.h>
 #include <memory>
-#ifndef __GNUC__
-#include <crtdbg.h>
-#endif
 
 #include <newpluginapi.h>
 #include <win2k.h>
-#include <m_system.h>
 #include <m_system_cpp.h>
 #include <m_database.h>
 #include <m_langpack.h>
-#include <m_utils.h>
 
 #include "database.h"
 #include "dbintf.h"
