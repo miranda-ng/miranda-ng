7e94dc56189f6a0fe46a482c05e7c28d0182a661
Kirill Volinsky <mataes2007@gmail.com>
2013-02-24 14:31:12 +0000

    added precompiled header added version info
    git-svn-id: http://svn.miranda-ng.org/main/trunk@3754 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/FirstRun/src/commonheaders.h
+++ plugins/FirstRun/src/commonheaders.h
@@ -1,9 +1,10 @@
-#define MIRANDA_VER 0x0A00
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <windows.h>
+
 #include <newpluginapi.h>
 #include <m_langpack.h>
-#include <m_system.h>
 #include <m_protocols.h>
 #include <m_database.h>
+
+#include "Version.h"
--- plugins/FirstRun/src/main.cpp
+++ plugins/FirstRun/src/main.cpp
@@ -7,19 +7,19 @@ int hLangpack;
 
 PLUGININFOEX pluginInfo={
 	sizeof(PLUGININFOEX),
-	"First Run Plugin",
-	PLUGIN_MAKE_VERSION(0,0,0,2),
-	"Displays the Accounts window at the first start.",
-	"Yasnovidyashii",
-	"yasnovidyashii@gmail.com",
-	"© 2008 Mikhail Yuriev",
-	"http://miranda-ng.org/",
+	__PLUGIN_NAME,
+	PLUGIN_MAKE_VERSION(__MAJOR_VERSION, __MINOR_VERSION, __RELEASE_NUM, __BUILD_NUM),
+	__DESCRIPTION,
+	__AUTHOR,
+	__AUTHOREMAIL,
+	__COPYRIGHT,
+	__AUTHORWEB,
 	UNICODE_AWARE,
-	//{49c2cf54-7898-44de-be3a-6d2e4ef90079}
-	{0x49c2cf54, 0x7898, 0x44de, { 0xbe, 0x3a, 0x6d, 0x2e, 0x4e, 0xf9, 0x0, 0x79 }} 
+	// {49C2CF54-7898-44DE-BE3A-6D2E4EF90079}
+	{0x49c2cf54, 0x7898, 0x44de, {0xbe, 0x3a, 0x6d, 0x2e, 0x4e, 0xf9, 0x0, 0x79}}
 };
 
-int ModulesLoaded(WPARAM wParam,LPARAM lParam)
+int ModulesLoaded(WPARAM wParam, LPARAM lParam)
 {
 	TCHAR* ptszDllName;
     TCHAR ptszDllPath[MAX_PATH];
@@ -27,19 +27,19 @@ int ModulesLoaded(WPARAM wParam,LPARAM lParam)
     GetModuleFileName(hInst, ptszDllPath, MAX_PATH);
     ptszDllName = _tcslwr(_tcsrchr(ptszDllPath, '\\'));
 	
-	if (ptszDllName!=NULL)
-		ptszDllName=ptszDllName + 1;
+	if (ptszDllName != NULL)
+		ptszDllName = ptszDllName + 1;
 	
-	CallService(MS_PROTO_SHOWACCMGR, (WPARAM) NULL, 0);
+	CallService(MS_PROTO_SHOWACCMGR, 0, 0);
 
-	DBWriteContactSettingByte(NULL, "PluginDisable", _T2A(ptszDllName), 1);
+	db_set_b(NULL, "PluginDisable", _T2A(ptszDllName), 1);
 	
 	return 0;
 }
 
-BOOL WINAPI DllMain(HINSTANCE hinstDLL,DWORD fdwReason,LPVOID lpvReserved)
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
-	hInst=hinstDLL;
+	hInst = hinstDLL;
 	return TRUE;
 }
 
@@ -52,7 +52,7 @@ extern "C" __declspec(dllexport) int Load(void)
 {
 
 	mir_getLP(&pluginInfo);
-	hModulesLoaded = HookEvent(ME_SYSTEM_MODULESLOADED,ModulesLoaded);
+	hModulesLoaded = HookEvent(ME_SYSTEM_MODULESLOADED, ModulesLoaded);
 	return 0;
 }
 
