f87f1ed6f1399674f92bd4e7ddc4571635f71980
Kirill Volinsky <mataes2007@gmail.com>
2013-02-25 06:07:59 +0000

    typo fixed
    git-svn-id: http://svn.miranda-ng.org/main/trunk@3760 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Dbx_mmap_SA/src/version.h
+++ plugins/Dbx_mmap_SA/src/version.h
@@ -6,7 +6,7 @@
 #define __FILEVERSION_STRING		__MAJOR_VERSION,__MINOR_VERSION,__RELEASE_NUM,__BUILD_NUM
 
 #define __PLUGIN_NAME				"Miranda NG mmap secure database driver"
-#define __FILENAME					"ClientChangeNotify.dll"
+#define __FILENAME					"Dbx_mmap_SA.dll"
 #define __DESCRIPTION 				"Provides Miranda database support: global settings, contacts, history, settings per contact. Enhanced modification with Encryption support."
 #define __AUTHOR					"Miranda-IM project, modification by FYR and chaos.persei, nullbie, Billy_Bons"
 #define __AUTHOREMAIL				"chaos.persei@gmail.com; ashpynov@gmail.com; bio@msx.ru; ghazan@miranda.im"
