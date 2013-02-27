873c6cdd0f7155926f3b8b5fa5db9bf9929c303b
Vadim Dashevskiy <watcherhd@gmail.com>
2013-02-27 08:56:54 +0000

    - boost build script
    git-svn-id: http://svn.miranda-ng.org/main/trunk@3792 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

---
+++ tools/boost/build_boost.bat
@@ -0,0 +1,13 @@
+@echo off
+set p1=%1
+set p2=%2
+if  "%p1%" == "" (echo "please specify target platform by adding '86' or '64' parameter to command line!'"&&pause&&goto :EOF)
+if  "%p2%" == "" (echo "please specify target msvc compiler by adding '10' for MSVC2010 or '11' for MSVC2012 to command line!'"&&pause&&goto :EOF)
+
+call bootstrap.bat
+
+if /i '%1' == '86' (
+	b2 --toolset=msvc-%2.0 runtime-link=shared threading=multi link=static architecture=x86 address-model=32
+) else if /i '%1' == '64' (
+	b2 --toolset=msvc-%2.0 runtime-link=shared threading=multi link=static architecture=x86 address-model=64 --stagedir=stage64
+)
