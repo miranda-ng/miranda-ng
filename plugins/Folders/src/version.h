87af1c64a8f5ecb4bbae81aa7a6f6c82f91c16b1
George Hazan <george.hazan@gmail.com>
2013-03-02 13:41:14 +0000

    version bump
    git-svn-id: http://svn.miranda-ng.org/main/trunk@3836 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Folders/src/version.h
+++ plugins/Folders/src/version.h
@@ -1,14 +1,14 @@
-#define __MAJOR_VERSION				0
-#define __MINOR_VERSION				1
-#define __RELEASE_NUM				6
-#define __BUILD_NUM					1
+#define __MAJOR_VERSION          0
+#define __MINOR_VERSION          2
+#define __RELEASE_NUM            0
+#define __BUILD_NUM              0
 
-#define __FILEVERSION_STRING		__MAJOR_VERSION,__MINOR_VERSION,__RELEASE_NUM,__BUILD_NUM
+#define __FILEVERSION_STRING     __MAJOR_VERSION,__MINOR_VERSION,__RELEASE_NUM,__BUILD_NUM
 
-#define __PLUGIN_NAME				"Custom profile folders"
-#define __FILENAME					"Folders.dll"
-#define __DESCRIPTION 				"Allows plugins to save their data to user selected folders; supports variables."
-#define __AUTHOR					"Cristian Libotean"
-#define __AUTHOREMAIL				"eblis102@yahoo.com"
-#define __AUTHORWEB					"http://miranda-ng.org/"
-#define __COPYRIGHT					"© 2005-2012 Cristian Libotean"
+#define __PLUGIN_NAME            "Custom profile folders"
+#define __FILENAME               "Folders.dll"
+#define __DESCRIPTION            "Allows plugins to save their data to user selected folders; supports variables."
+#define __AUTHOR                 "Cristian Libotean, Miranda NG Team"
+#define __AUTHOREMAIL            "eblis102@yahoo.com"
+#define __AUTHORWEB              "http://miranda-ng.org/"
+#define __COPYRIGHT              "© 2005-2012 Cristian Libotean, 2012-13 Miranda NG Team"
