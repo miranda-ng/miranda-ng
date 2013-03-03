b7aa7ea29a5fab12adc0e4ca105e437d16fee555
Vadim Dashevskiy <watcherhd@gmail.com>
2013-03-03 05:24:57 +0000

    - restored files, deleted by mistake
    git-svn-id: http://svn.miranda-ng.org/main/trunk@3857 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

---
+++ plugins/NotifyAnything/sendlog/SendLogWin.cpp
@@ -0,0 +1,30 @@
+#include <windows.h>
+#include <winsock.h>
+
+#include <iostream>
+#include <string>
+
+void implementation(int argc, char **argv, bool console);
+
+int main(int argc, char **argv)
+try
+{
+	if (argc == 1 || argc == 2 && !strcmp("/?", argv[1])) {
+		MessageBox(NULL, "Syntax: sendlog32 [-T] [-H host] [-P port] text\n"
+			"\tSends single text message.", "Sendlog32", MB_OK);
+		return 0;
+	}
+
+	implementation(argc, argv, false);
+}
+catch (DWORD err) {
+	LPVOID lpMsgBuf;
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_FROM_SYSTEM|FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL, err, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR) &lpMsgBuf, 0, NULL);
+
+	MessageBox(NULL, (LPCTSTR)lpMsgBuf, "sendlog32", MB_OK);
+
+	LocalFree(lpMsgBuf);
+
+	return 1;
+}
