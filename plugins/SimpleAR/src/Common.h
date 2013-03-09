e1814c1359bbf11a53203bb9cebbba22f87150fd
Kirill Volinsky <mataes2007@gmail.com>
2013-03-09 12:14:52 +0000

    removed not used headers
    git-svn-id: http://svn.miranda-ng.org/main/trunk@3937 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/SimpleAR/src/Common.h
+++ plugins/SimpleAR/src/Common.h
@@ -1,34 +1,15 @@
-#define MIRANDA_VER    0x0A00
 #define _CRT_SECURE_NO_WARNINGS
 
-// Windows Header Files
 #include <windows.h>
-#include <commctrl.h>
-#include <stdio.h>
 #include <time.h>
-#include <stddef.h>
-#include <process.h>
-#include <string.h>
-#include <winsock.h>
-#include <string>
-#include <winbase.h>
 
-//Miranda SDK headers
-#include "newpluginapi.h"
-#include "m_clist.h"
-#include "m_clui.h"
-#include "m_skin.h"
-#include "m_langpack.h"
-#include "m_protomod.h"
-#include "m_database.h"
-#include "m_system.h"
-#include "m_protocols.h"
-#include "m_userinfo.h"
-#include "m_options.h"
-#include "m_protosvc.h"
-#include "m_utils.h"
-#include "m_ignore.h"
-#include "m_clc.h"
+#include <newpluginapi.h>
+#include <m_clist.h>
+#include <m_langpack.h>
+#include <m_database.h>
+#include <m_options.h>
+#include <m_protosvc.h>
+
 #include <m_variables.h>
 
 #include "Resource.h"
--- plugins/SimpleAR/src/Version.h
+++ plugins/SimpleAR/src/Version.h
@@ -4,15 +4,8 @@
 #define __BUILD_NUM					6
 
 #define __FILEVERSION_STRING		__MAJOR_VERSION,__MINOR_VERSION,__RELEASE_NUM,__BUILD_NUM
-#define __FILEVERSION_DOTS			__MAJOR_VERSION.__MINOR_VERSION.__RELEASE_NUM.__BUILD_NUM
 
-#define __STRINGIFY_IMPL(x)			#x
-#define __STRINGIFY(x)				__STRINGIFY_IMPL(x)
-#define __VERSION_STRING			__STRINGIFY(__FILEVERSION_DOTS)
-
-
-#define __PLUGIN_NAME				"SimpleAR"
-#define __INTERNAL_NAME				"Simple Auto Replier"
+#define __PLUGIN_NAME				"Simple auto replier"
 #define __FILENAME					"SimpleAR.dll"
 #define __DESCRIPTION 				"Simple Auto Replier."
 #define __AUTHOR					"Stark Wong, Mataes, Mikel-Ard-Ri"
