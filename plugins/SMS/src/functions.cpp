4cbdf4bd6d261e9a6a4f3b86411983ba1a89ef48
George Hazan <george.hazan@gmail.com>
2013-03-11 20:36:55 +0000

    misprint
    git-svn-id: http://svn.miranda-ng.org/main/trunk@3978 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/SMS/src/functions.cpp
+++ plugins/SMS/src/functions.cpp
@@ -609,13 +609,12 @@ void LoadMsgDlgFont(int i,LOGFONT *lf,COLORREF *colour)
 
 LRESULT CALLBACK MessageSubclassProc(HWND hwnd,UINT message,WPARAM wParam,LPARAM lParam)
 {
-	LRESULT lrRet=0;
-
-	switch(message){
-	case WM_CHAR:
-		if (wParam=='\n' && GetKeyState(VK_CONTROL)&0x8000)
-			PostMessage(GetParent(hwnd),WM_COMMAND,IDOK,0);
-		return 0;
+	switch(message) {
+	case WM_CHAR: 
+		if (wParam == '\n' && GetKeyState(VK_CONTROL) & 0x8000) {
+			PostMessage(GetParent(hwnd), WM_COMMAND, IDOK, 0);
+			return 0;
+		}
 	}
 
 	return mir_callNextSubclass(hwnd, MessageSubclassProc, message, wParam, lParam);
