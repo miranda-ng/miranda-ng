156911d2bc7900048e6ed7a574e5d34f1d9ed743
Robert Pösel <robyer@seznam.cz>
2013-03-20 13:23:38 +0000

    Facebook: Fixed size of avatars due to facebook change
    git-svn-id: http://svn.miranda-ng.org/main/trunk@4124 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/FacebookRM/src/avatars.cpp
+++ protocols/FacebookRM/src/avatars.cpp
@@ -57,9 +57,16 @@ void FacebookProto::CheckAvatarChange(HANDLE hContact, std::string image_url)
 	if (image_url.empty())
 		return;
 	
-	if (DBGetContactSettingByte(NULL, m_szModuleName, FACEBOOK_KEY_BIG_AVATARS, DEFAULT_BIG_AVATARS)) 
+	bool big_avatars = (bool)DBGetContactSettingByte(NULL, m_szModuleName, FACEBOOK_KEY_BIG_AVATARS, DEFAULT_BIG_AVATARS);
+	
+	// We've got url to avatar of default size 32x32px, let's change it to slightly bigger (50x50px) - looks like maximum size for square format
+	std::tstring::size_type pos = image_url.rfind( "/s32x32/" );
+	if (pos != std::wstring::npos)
+		image_url = image_url.replace( pos, 8, big_avatars ? "/s200x200/" : "/s50x50/" );
+	
+	if (big_avatars)
 	{
-		std::tstring::size_type pos = image_url.rfind( "_q." );
+		pos = image_url.rfind( "_q." );
 		if (pos != std::wstring::npos)
 			image_url = image_url.replace( pos, 3, "_s." );
 	}
