3d411171eba820bffa2998f2442c197a553153f4
Vadim Dashevskiy <watcherhd@gmail.com>
2013-03-25 16:44:51 +0000

    - resource cosmetics (patch from person)
    git-svn-id: http://svn.miranda-ng.org/main/trunk@4189 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/StatusChange/src/resource.h
+++ plugins/StatusChange/src/resource.h
@@ -1,5 +1,8 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ..\res\resource.rc
+//
 #define IDD_OPTIONS                     10
-
 #define IDC_CHK_OFFLINE                 1001
 #define IDC_CHK_ONLINE                  1002
 #define IDC_CHK_AWAY                    1003
@@ -10,7 +13,6 @@
 #define IDC_CHK_INVISIBLE               1008
 #define IDC_CHK_ONTHEPHONE              1009
 #define IDC_CHK_OUTTOLUNCH              1010
-
 #define IDC_RAD_OFFLINE                 2001
 #define IDC_RAD_ONLINE                  2002
 #define IDC_RAD_AWAY                    2003
@@ -21,14 +23,23 @@
 #define IDC_RAD_INVISIBLE               2008
 #define IDC_RAD_ONTHEPHONE              2009
 #define IDC_RAD_OUTTOLUNCH              2010
-
 #define IDC_CHK_MESSAGEREAD             3001
 #define IDC_CHK_MESSAGESEND             3002
 #define IDC_CHK_URLREAD                 3003
 #define IDC_CHK_URLSEND                 3004
 #define IDC_CHK_FILEREAD                3005
 #define IDC_CHK_FILESEND                3006
-
 #define IDC_MSGICON                     4001
 #define IDC_URLICON                     4002
 #define IDC_FILEICON                    4003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
