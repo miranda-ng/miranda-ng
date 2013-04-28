732783eb0ab6e5528ace0f3ef3caafc3096bb237
Kirill Volinsky <mataes2007@gmail.com>
2013-04-28 14:19:50 +0000

    code cleanup
    git-svn-id: http://svn.miranda-ng.org/main/trunk@4560 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/Skype/src/aes/aes.h
+++ protocols/Skype/src/aes/aes.h
@@ -1,24 +1,15 @@
 #ifndef _AES_H
 #define _AES_H
 
-#ifndef uint8
-#define uint8  unsigned char
-#endif
-
-#ifndef uint32
-#define uint32 unsigned long int
-#endif
-
 typedef struct
 {
-    uint32 erk[64];     /* encryption round keys */
-    uint32 drk[64];     /* decryption round keys */
-    int nr;             /* number of rounds */
-}
-aes_context;
+	unsigned long int erk[64]; /* encryption round keys */
+	unsigned long int drk[64]; /* decryption round keys */
+	int nr; /* number of rounds */
+} aes_context;
 
-int  aes_set_key( aes_context *ctx, uint8 *key, int nbits );
-void aes_encrypt( aes_context *ctx, uint8 input[16], uint8 output[16] );
-void aes_decrypt( aes_context *ctx, uint8 input[16], uint8 output[16] );
+int aes_set_key(aes_context *ctx, unsigned char *key, int nbits);
+void aes_encrypt(aes_context *ctx, unsigned char input[16], unsigned char output[16]);
+void aes_decrypt(aes_context *ctx, unsigned char input[16], unsigned char output[16]);
 
 #endif /* aes.h */
