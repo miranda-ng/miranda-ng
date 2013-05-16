016efa8dd058f9af5a57a33a6541736ce710a5a7
George Hazan <george.hazan@gmail.com>
2013-05-16 14:25:28 +0000

    unused m_ignore.h
    git-svn-id: http://svn.miranda-ng.org/main/trunk@4676 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- src/core/stduihist/commonheaders.h
+++ src/core/stduihist/commonheaders.h
@@ -70,7 +70,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <m_userinfo.h>
 #include <m_history.h>
 #include <m_findadd.h>
-#include <m_ignore.h>
 #include <m_icolib.h>
 #include <m_modernopt.h>
 #include <m_timezones.h>
--- src/core/stdurl/commonheaders.h
+++ src/core/stdurl/commonheaders.h
@@ -71,7 +71,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <m_history.h>
 #include <m_addcontact.h>
 #include <m_findadd.h>
-#include <m_ignore.h>
 #include <m_icolib.h>
 #include <m_modernopt.h>
 #include <m_timezones.h>
