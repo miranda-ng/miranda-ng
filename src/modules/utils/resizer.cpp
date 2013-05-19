bfc463a52dff123910f1ef4a4e1e92e79259c5e5
George Hazan <george.hazan@gmail.com>
2013-05-19 18:21:01 +0000

    fix for windows resizer in 64-bit mode
    git-svn-id: http://svn.miranda-ng.org/main/trunk@4746 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- src/modules/utils/resizer.cpp
+++ src/modules/utils/resizer.cpp
@@ -23,6 +23,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "..\..\core\commonheaders.h"
 
+#pragma pack(2)
+
 typedef struct {
 	DWORD  helpID;
 	DWORD  exStyle;
