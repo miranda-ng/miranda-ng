77627d30234c5f94d8a59a837fabeb969209f6b2
Vadim Dashevskiy <watcherhd@gmail.com>
2013-05-22 11:03:26 +0000

    - lpgen.js.bat updated
    git-svn-id: http://svn.miranda-ng.org/main/trunk@4796 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- tools/lpgen/lpgen.js.bat
+++ tools/lpgen/lpgen.js.bat
@@ -1,3 +1,3 @@
 rem echo off
 cscript /nologo lpgen.js
-copy ..\..\langpacks\english\"=CORE=.txt" +..\..\langpacks\english\Plugins\*.txt ..\..\langpacks\english\Langpack_english.txt
+copy ..\..\langpacks\english\"=HEAD=.txt"+..\..\langpacks\english\"=CORE=.txt" +..\..\langpacks\english\Plugins\*.txt ..\..\langpacks\english\Langpack_english.txt
