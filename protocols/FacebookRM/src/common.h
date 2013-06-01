b658ef905291fb9c3f128a2727e507c78d3fb438
Vadim Dashevskiy <watcherhd@gmail.com>
2013-06-01 20:32:33 +0000

    - compilation fix
    git-svn-id: http://svn.miranda-ng.org/main/trunk@4856 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/FacebookRM/src/common.h
+++ protocols/FacebookRM/src/common.h
@@ -32,6 +32,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <map>
 #include <vector>
 #include <set>
+#include <algorithm>
 
 #include <windows.h>
 #include <time.h>
