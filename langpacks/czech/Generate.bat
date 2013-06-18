86b46853d8fa3d0447534e991bbe33af1d52b8a2
Robert Pösel <robyer@seznam.cz>
2013-06-18 15:35:57 +0000

    Czech generation script is now more awesome.
    git-svn-id: http://svn.miranda-ng.org/main/trunk@5018 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- langpacks/czech/Generate.bat
+++ langpacks/czech/Generate.bat
@@ -1,11 +1,20 @@
-rem echo off
+@echo off
+
+set phase=0
+
+:start
+
 cd "..\..\tools\lpgen\"
 
 mkdir "Plugins"
 mkdir "Untranslated"
 
-cscript /nologo translate.js /log:"yes" /out:"Plugins" /untranslated:"Untranslated" /outfile:"langpack_czech.txt" /path:"..\..\langpacks\czech\Plugins" /core:"..\..\langpacks\czech\=CORE=.txt" /langpack:"..\..\langpacks\czech\Deprecated\old langpack\langpack_czech2.txt"
-
+if "%phase%" == "0" (
+	cscript /nologo translate.js /log:"yes" /out:"Plugins" /untranslated:"Untranslated" /outfile:"langpack_czech.txt" /path:"..\..\langpacks\czech\Plugins" /core:"..\..\langpacks\czech\=CORE=.txt" /langpack:"..\..\langpacks\czech\Deprecated\old langpack\langpack_czech2.txt"
+) else (
+	cscript /nologo translate.js /log:"yes" /out:"Plugins" /untranslated:"Untranslated" /outfile:"langpack_czech.txt" /path:"..\..\langpacks\czech\Plugins" /core:"..\..\langpacks\czech\=CORE=.txt" /langpack:"..\..\langpacks\czech\Langpack_czech.txt"
+)
+	
 rm -r "../../langpacks/czech/Plugins"
 rm -r "../../langpacks/czech/Untranslated"
 rm -r "../../langpacks/czech/=CORE=.txt"
@@ -20,7 +29,16 @@ copy "=CORE=.txt" "..\..\langpacks\czech\=CORE=.txt"
 rm -r "Plugins"
 rm -r "Untranslated"
 rm "=CORE=.txt"
-rm "langpack_czech.txt"
+rm "Langpack_czech.txt"
 
 cd "..\..\langpacks\tool\"
-LangpackSuite.exe
+LangpackSuite.exe \q \pczech
+
+if "%phase%" == "1" (
+	goto end
+) else (
+	set phase=1
+	goto start
+)
+
+:end
