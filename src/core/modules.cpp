b4873d4448cffe1269f7e004da80351c583b1228
George Hazan <george.hazan@gmail.com>
2013-06-23 15:19:53 +0000

    old unused variables removed
    git-svn-id: http://svn.miranda-ng.org/main/trunk@5097 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- src/core/modules.cpp
+++ src/core/modules.cpp
@@ -24,11 +24,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "../modules/plugins/plugins.h"
 
-// other static variables
-static DWORD  mainThreadId;
-static HANDLE hMainThread;
-static HANDLE hMissingService;
-
 int CheckRestart();		// core: IDD_WAITRESTART
 
 int LoadSystemModule(void);		// core: m_system.h services
