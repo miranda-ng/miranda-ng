c93ec058116d2c607146d8c2c535d6a706144ec9
Robert Pösel <robyer@seznam.cz>
2013-07-10 18:50:35 +0000

    Windows line-endings for refresher scripts.
    git-svn-id: http://svn.miranda-ng.org/main/trunk@5311 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- langpacks/slovak/Refresh.bat
+++ langpacks/slovak/Refresh.bat
@@ -1,4 +1,18 @@
 @echo off
 
+REM ====== VARIABLES ========
+
+set "language=slovak"
+set "oldLangpackPath=Deprecated\Langpack_slovak_old.txt"
+
+REM =========================
+
 cd "..\..\tools\lpgen\"
-refresher.bat slovak "Deprecated\Langpack_slovak_old.txt"
+refresher.bat %language% "%oldLangpackPath%"
+
+REM copy and reload langpack in your Miranda (uncoment next lines + enable cmdline.dll plugin to use it)
+ REM echo Copy and refresh Miranda NG langpack
+ REM set "mirandaPath=D:\Programy\Miranda NG"
+ REM copy "..\..\langpacks\%language%\Langpack_%language%.txt" "%mirandaPath%\Langpack_%language%.txt"
+ REM cd "%mirandaPath%"
+ REM mimcmd.exe callservice LangPack/Reload 0 0
