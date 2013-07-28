98a25556382d67f551975b5d642f139e46239ea7
George Hazan <george.hazan@gmail.com>
2013-07-28 11:20:37 +0000

    unused headers removed
    git-svn-id: http://svn.miranda-ng.org/main/trunk@5505 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- src/core/stdcrypt/commonheaders.h
+++ src/core/stdcrypt/commonheaders.h
@@ -52,12 +52,10 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <m_database.h>
 #include <m_netlib.h>
 #include <m_langpack.h>
-#include <m_avatars.h>
 #include <m_button.h>
 #include <m_protosvc.h>
 #include <m_protomod.h>
 #include <m_protocols.h>
-#include <m_protoint.h>
 #include <m_options.h>
 #include <m_skin.h>
 #include <m_contacts.h>
