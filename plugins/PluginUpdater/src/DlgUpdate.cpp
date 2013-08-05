4ac0b4b54f671a758fc9fa31b2912b7541ce2de5
Vadim Dashevskiy <watcherhd@gmail.com>
2013-08-05 06:51:54 +0000

    - Pluginupdater: RSS <> NewsAggregator mask
    git-svn-id: http://svn.miranda-ng.org/main/trunk@5583 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/PluginUpdater/src/DlgUpdate.cpp
+++ plugins/PluginUpdater/src/DlgUpdate.cpp
@@ -345,9 +345,10 @@ static renameTable[] =
 	{ _T("dbtool.exe"),                     _T("Plugins\\dbchecker.dll") },
 	{ _T("dbtool_sa.exe"),                  _T("Plugins\\dbchecker.dll") },
 	{ _T("bclist.dll"),                     _T("Plugins\\clist_blind.dll") },
-	{ _T("otr.dll"),                        _T("Plugins\\MirOTR.dll") },
-	{ _T("ttnotify.dll"),                   _T("Plugins\\TooltipNotify.dll") },
-	{ _T("NewStatusNotify.dll"),            _T("Plugins\\NewXStatusNotify.dll") },
+	{ _T("otr.dll"),                        _T("Plugins\\mirotr.dll") },
+	{ _T("ttnotify.dll"),                   _T("Plugins\\tooltipnotify.dll") },
+	{ _T("newstatusnotify.dll"),            _T("Plugins\\newxstatusnotify.dll") },
+	{ _T("rss.dll"),                        _T("Plugins\\newsaggregator.dll") },
 
 	{ _T("proto_newsaggr.dll"),             _T("Icons\\proto_newsaggregator.dll") },
 	{ _T("clienticons_general.dll"),        _T("Icons\\fp_icons.dll") },
