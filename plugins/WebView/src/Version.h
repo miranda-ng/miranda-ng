f7ad58e2507c6e70cf3293c7d23ed4d18f454751
Vadim Dashevskiy <watcherhd@gmail.com>
2013-08-07 04:28:11 +0000

    renamed to WebView
    git-svn-id: http://svn.miranda-ng.org/main/trunk@5608 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/WebView/src/Version.h
+++ plugins/WebView/src/Version.h
@@ -5,7 +5,7 @@
 
 #define __FILEVERSION_STRING __MAJOR_VERSION,__MINOR_VERSION,__RELEASE_NUM,__BUILD_NUM
 
-#define __PLUGIN_NAME        "Webview Protocol"
+#define __PLUGIN_NAME        "Webview"
 #define __FILENAME           "Webview.dll"
 #define __DESCRIPTION        "Adds web pages as contacts to your contact list and can display text and/or issue change alerts from those pages in a window."
 #define __AUTHOR             "Vincent Joyce"
--- plugins/WebView/src/main.cpp
+++ plugins/WebView/src/main.cpp
@@ -193,7 +193,7 @@ extern "C" int __declspec(dllexport) Load()
 	SkinAddNewSoundEx("webviewalert", optionsname, Translate("Alert Event"));
 
 	//add module to known list
-	db_set_s(NULL, "KnownModules", "Webview Protocol", MODULENAME);
+	db_set_s(NULL, "KnownModules", "Webview", MODULENAME);
 
 	//value is 1 if menu is disabled
 	db_set_b(NULL, MODULENAME, MENU_IS_DISABLED_KEY, 1);
