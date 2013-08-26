4906fb47f340bea7d2ba551364d1a5d8d0473861
Kirill Volinsky <mataes2007@gmail.com>
2013-08-26 14:14:54 +0000

    libjpeg update
    git-svn-id: http://svn.miranda-ng.org/main/trunk@5843 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/AdvaImg/src/LibJPEG/jconfig.h
+++ plugins/AdvaImg/src/LibJPEG/jconfig.h
@@ -1,5 +1,5 @@
 /* jconfig.vc --- jconfig.h for Microsoft Visual C++ on Windows 95 or NT. */
-/* see jconfig.doc for explanations */
+/* see jconfig.txt for explanations */
 
 #define HAVE_PROTOTYPES
 #define HAVE_UNSIGNED_CHAR
@@ -8,16 +8,14 @@
 /* #define const */
 #undef CHAR_IS_UNSIGNED
 #define HAVE_STDDEF_H
-#ifndef HAVE_STDLIB_H
 #define HAVE_STDLIB_H
-#endif
 #undef NEED_BSD_STRINGS
 #undef NEED_SYS_TYPES_H
 #undef NEED_FAR_POINTERS	/* we presume a 32-bit flat memory model */
 #undef NEED_SHORT_EXTERNAL_NAMES
 #undef INCOMPLETE_TYPES_BROKEN
 
-/* Define "boolean" as unsigned char, not int, per Windows custom */
+/* Define "boolean" as unsigned char, not enum, per Windows custom */
 #ifndef __RPCNDR_H__		/* don't conflict if rpcndr.h already read */
 typedef unsigned char boolean;
 #endif
