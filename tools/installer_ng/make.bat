d34ee7f78c43aef81da67b56f7a856a5ad1bcb32
Vadim Dashevskiy <watcherhd@gmail.com>
2013-09-02 22:07:50 +0000

    - win64 compatible
    git-svn-id: http://svn.miranda-ng.org/main/trunk@5909 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- tools/installer_ng/make.bat
+++ tools/installer_ng/make.bat
@@ -16,19 +16,24 @@ wget -O InnoNG_64\Installer\vcredist_x64.exe http://download.microsoft.com/downl
 rem end
 
 rem Extract
-"%PROGRAMFILES%\7-zip\7z.exe" x tmp\miranda-ng-alpha-latest.7z -y -oInnoNG_32\Files
-"%PROGRAMFILES%\7-zip\7z.exe" x tmp\clist_blind_x32.zip -y -oInnoNG_32\Files
-"%PROGRAMFILES%\7-zip\7z.exe" x tmp\scriver_x32.zip -y -oInnoNG_32\Files
-"%PROGRAMFILES%\7-zip\7z.exe" x tmp\miranda-ng-alpha-latest_x64.7z -y -oInnoNG_64\Files
-"%PROGRAMFILES%\7-zip\7z.exe" x tmp\clist_blind_x64.zip -y -oInnoNG_64\Files
-"%PROGRAMFILES%\7-zip\7z.exe" x tmp\scriver_x64.zip -y -oInnoNG_64\Files
-"%PROGRAMFILES%\7-zip\7z.exe" x tmp\lang*.zip -y -oInnoNG_32\Files
-"%PROGRAMFILES%\7-zip\7z.exe" x tmp\lang*.zip -y -oInnoNG_64\Files
+"%ProgramFiles%\7-zip\7z.exe" x tmp\miranda-ng-alpha-latest.7z -y -oInnoNG_32\Files
+"%ProgramFiles%\7-zip\7z.exe" x tmp\clist_blind_x32.zip -y -oInnoNG_32\Files
+"%ProgramFiles%\7-zip\7z.exe" x tmp\scriver_x32.zip -y -oInnoNG_32\Files
+"%ProgramFiles%\7-zip\7z.exe" x tmp\miranda-ng-alpha-latest_x64.7z -y -oInnoNG_64\Files
+"%ProgramFiles%\7-zip\7z.exe" x tmp\clist_blind_x64.zip -y -oInnoNG_64\Files
+"%ProgramFiles%\7-zip\7z.exe" x tmp\scriver_x64.zip -y -oInnoNG_64\Files
+"%ProgramFiles%\7-zip\7z.exe" x tmp\lang*.zip -y -oInnoNG_32\Files
+"%ProgramFiles%\7-zip\7z.exe" x tmp\lang*.zip -y -oInnoNG_64\Files
 rem end
 
 rem Make
-"%PROGRAMFILES%\Inno Setup 5\Compil32.exe" /cc "InnoNG_32\MirandaNG.iss"
-"%PROGRAMFILES%\Inno Setup 5\Compil32.exe" /cc "InnoNG_64\MirandaNG.iss"
+if defined ProgramFiles(x86) (
+	"%ProgramFiles(x86)%\Inno Setup 5\Compil32.exe" /cc "InnoNG_32\MirandaNG.iss"
+	"%ProgramFiles(x86)%\Inno Setup 5\Compil32.exe" /cc "InnoNG_64\MirandaNG.iss"
+) else (
+	"%ProgramFiles%\Inno Setup 5\Compil32.exe" /cc "InnoNG_32\MirandaNG.iss"
+	"%ProgramFiles%\Inno Setup 5\Compil32.exe" /cc "InnoNG_64\MirandaNG.iss"
+)	
 rem end
 
 rem Cleanup
