827cc6051f4df89e08e65b9bd85b0e06e4a9cde9
Vadim Dashevskiy <watcherhd@gmail.com>
2013-09-06 11:33:05 +0000

    Installer: version variable fixed
    git-svn-id: http://svn.miranda-ng.org/main/trunk@5964 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- tools/installer_ng/compile.bat
+++ tools/installer_ng/compile.bat
@@ -1,7 +1,5 @@
 @echo off
 set GetVer=for /F "tokens=1,2,3 delims= " %%i in (build.no) do set MirVer=%%i.%%j.%%k
-set Compile32=Inno Setup 5\ISCC.exe" /Dptx86 /DAppVer=%MirVer% /O"Output" "InnoNG_32\MirandaNG.iss"
-set Compile64=Inno Setup 5\ISCC.exe" /DAppVer=%MirVer% /O"Output" "InnoNG_64\MirandaNG.iss"
 rem Get version
 if exist ..\..\build\build.no goto localgetver
 if not exist tmp mkdir tmp
@@ -15,6 +13,11 @@ pushd ..\..\build
 %GetVer%
 popd
 :esclocal
+rem end
+
+rem Set compiler variables
+set Compile32=Inno Setup 5\ISCC.exe" /Dptx86 /DAppVer=%MirVer% /O"Output" "InnoNG_32\MirandaNG.iss"
+set Compile64=Inno Setup 5\ISCC.exe" /DAppVer=%MirVer% /O"Output" "InnoNG_64\MirandaNG.iss"
 rem end
 
 rem Make
