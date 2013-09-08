e4eeb9b16f42de4ece946f733df64cdcf86971a7
Vadim Dashevskiy <watcherhd@gmail.com>
2013-09-08 09:44:36 +0000

    - Installer: GenerateInnoGuiCompatScript.bat optimization - better version
    git-svn-id: http://svn.miranda-ng.org/main/trunk@6018 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- tools/installer_ng/Common/GenerateInnoGuiCompatScript.bat
+++ tools/installer_ng/Common/GenerateInnoGuiCompatScript.bat
@@ -1,8 +1,6 @@
 @echo off
 if not exist MirandaNG.iss goto end
 set GetVer=for /F "tokens=1,2,3 delims= " %%i in (build.no) do set MirVer=%%i.%%j.%%k
-set BomRem=http://miranda-ng.org/distr/installer/bom_remove.exe
-set BomAdd=http://miranda-ng.org/distr/installer/bom_add.exe
 for %%* in (.) do set InnoX=%%~n*
 rem Get version
 if exist ..\..\..\build\build.no goto localgetver
@@ -20,29 +18,21 @@ rem end
 rem Create Inno setup compatible script that compiles without passing parameters to compiler
 if %InnoX% == InnoNG_32 (
   if exist MirandaNG32.iss del /F /Q MirandaNG32.iss
-  ..\Tools\wget.exe -O bom_remove.exe %BomRem%
-  ..\Tools\wget.exe -O bom_add.exe %BomAdd%
-  bom_remove.exe MirandaNG.iss
+  ..\Tools\bom_remove.exe MirandaNG.iss
   echo #define ptx86 > MirandaNG32.iss
   echo #define AppVer "%MirVer%" >> MirandaNG32.iss
   echo.>> MirandaNG32.iss
   type MirandaNG.iss >>  MirandaNG32.iss
-  bom_add.exe MirandaNG32.iss
-  bom_add.exe MirandaNG.iss
-  del /f /q bom_remove.exe
-  del /f /q bom_add.exe
+  ..\Tools\bom_add.exe MirandaNG32.iss
+  ..\Tools\bom_add.exe MirandaNG.iss
 ) else if %InnoX% == InnoNG_64 (
   if exist MirandaNG64.iss del /F /Q MirandaNG64.iss
-  ..\Tools\wget.exe -O bom_remove.exe %BomRem%
-  ..\Tools\wget.exe -O bom_add.exe %BomAdd%
-  bom_remove.exe MirandaNG.iss
+  ..\Tools\bom_remove.exe MirandaNG.iss
   echo #define AppVer "%MirVer%" > MirandaNG64.iss
   echo.>> MirandaNG64.iss
   type MirandaNG.iss >> MirandaNG64.iss
-  bom_add.exe MirandaNG64.iss
-  bom_add.exe MirandaNG.iss
-  del /f /q bom_remove.exe
-  del /f /q bom_add.exe
+  ..\Tools\bom_add.exe MirandaNG64.iss
+  ..\Tools\bom_add.exe MirandaNG.iss
 )
 rem end
 
