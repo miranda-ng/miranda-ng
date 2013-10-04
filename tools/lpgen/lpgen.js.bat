9b5be51e0aa2e7db77884c327ebb73e1ea1dca3f
Vadim Dashevskiy <watcherhd@gmail.com>
2013-10-04 17:33:10 +0000

    Langpack_english.txt eliminated, langpacks/english: updated
    git-svn-id: http://svn.miranda-ng.org/main/trunk@6327 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- tools/lpgen/lpgen.js.bat
+++ tools/lpgen/lpgen.js.bat
@@ -1,3 +1,2 @@
 rem echo off
 cscript /nologo lpgen.js
-copy ..\..\langpacks\english\"=HEAD=.txt"+..\..\langpacks\english\"=CORE=.txt" +..\..\langpacks\english\Plugins\*.txt ..\..\langpacks\english\Langpack_english.txt
