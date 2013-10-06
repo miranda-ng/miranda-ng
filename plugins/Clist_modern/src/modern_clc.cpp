e732d8db600020536fabf5134fe25facf868cdb0
Dart Raiden <wowemuh@gmail.com>
2013-10-06 22:17:01 +0000

    unnecessary space
    git-svn-id: http://svn.miranda-ng.org/main/trunk@6382 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Clist_modern/src/modern_clc.cpp
+++ plugins/Clist_modern/src/modern_clc.cpp
@@ -1316,7 +1316,7 @@ static LRESULT clcOnLButtonUp(ClcData *dat, HWND hwnd, UINT msg, WPARAM wParam, 
 							if (contSour->subcontacts == contDest) {
 								HANDLE hsour;
 								hsour = contSour->hContact;
-								mir_sntprintf(Wording,SIZEOF(Wording),TranslateT("Do you want contact '%s' to be default ?"),contSour->szText);
+								mir_sntprintf(Wording,SIZEOF(Wording),TranslateT("Do you want contact '%s' to be default?"),contSour->szText);
 								res = MessageBox(hwnd,Wording,TranslateT("Set default contact"),MB_OKCANCEL|MB_ICONQUESTION);
 								if (res == 1)
 									CallService(MS_MC_SETDEFAULTCONTACT,(WPARAM)contDest->hContact,(LPARAM)hsour);
