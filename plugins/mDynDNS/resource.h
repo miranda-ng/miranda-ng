4a29682abc87c0d651d61c847dac741f85e9a30e
Robert Pösel <robyer@seznam.cz>
2013-10-29 20:13:29 +0000

    Adopted mDynDNS plugin
    First compilable version.
    
    git-svn-id: http://svn.miranda-ng.org/main/trunk@6686 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

---
+++ plugins/mDynDNS/resource.h
@@ -0,0 +1,44 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by resource.rc
+//
+#define IDOK                            1
+#define IDCANCEL                        2
+#define IDCLOSE                         8
+#define IDCONTINUE                      11
+#define IDB_DynDNS                      103
+#define IDI_ONLINE                      104
+#define IDI_OFFLINE                     105
+#define IDI_NA                          131
+#define IDI_OCCUPIED                    159
+#define IDD_OPT_MDYNDNS                 301
+#define IDC_USERNAME                    302
+#define IDC_PASSWORD                    303
+#define IDC_DOMAIN                      304
+#define IDC_CREATEACCOUNT               305
+#define IDD_OPT_POPUPS                  306
+#define IDC_DELAY_POPUP                 307
+#define IDC_DELAY_CUSTOM                308
+#define IDC_DELAY_PERMANENT             309
+#define IDC_DELAY                       310
+#define IDC_PREVIEW                     313
+#define IDC_ENABLEPOPUP                 314
+#define IDC_ENABLEUPDATEPOPUP           315
+#define IDC_DynDNS                      316
+#define IDC_COLOR_CUSTOM                321
+#define IDC_COLOR_BKG                   322
+#define IDC_COLOR_TXT                   323
+#define IDC_COLOR_OS                    324
+#define IDC_COLOR_POPUP                 325
+#define IDI_ICON1                       1000
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           111
+#endif
+#endif
