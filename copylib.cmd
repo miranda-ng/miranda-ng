65ac461db6c7b95aabbd307231553f0cd11e379a
Vadim Dashevskiy <watcherhd@gmail.com>
2013-11-02 10:16:59 +0000

    test
    git-svn-id: http://svn.miranda-ng.org/main/trunk@6740 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- copylib.cmd
+++ copylib.cmd
@@ -3,3 +3,4 @@ copy /Y .\Bin10\Debug\Obj\mir_core\mir_core.lib .\Bin10\lib
 copy /Y .\Bin10\Debug64\Obj\mir_core\mir_core64.lib .\Bin10\lib
 copy /Y .\Bin11\Debug\Obj\mir_core\mir_core.lib .\Bin11\lib
 copy /Y .\Bin11\Debug64\Obj\mir_core\mir_core64.lib .\Bin11\lib
+test
