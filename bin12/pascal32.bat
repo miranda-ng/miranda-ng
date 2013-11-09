87d086ae4119579025a3ab6ea6762a6d7baab105
Vadim Dashevskiy <watcherhd@gmail.com>
2013-11-09 19:41:18 +0000

    Pascal compilation 2013 adjustment
    git-svn-id: http://svn.miranda-ng.org/main/trunk@6845 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- bin12/pascal32.bat
+++ bin12/pascal32.bat
@@ -3,40 +3,40 @@
 pushd ..\plugins
 
 pushd Actman
-call make.bat fpc 11
+call make.bat fpc 12
 if errorlevel 1 goto :Error
 popd
 
 pushd Actman30
-call make.bat fpc 11
+call make.bat fpc 12
 if errorlevel 1 goto :Error
 popd
 
 pushd ImportTXT
-call make.bat fpc 11
+call make.bat fpc 12
 if errorlevel 1 goto :Error
 popd
 
 pushd mRadio
-call make.bat fpc 11
+call make.bat fpc 12
 if errorlevel 1 goto :Error
 popd
 
 pushd QuickSearch
-call make.bat fpc 11
+call make.bat fpc 12
 if errorlevel 1 goto :Error
 popd
 
 pushd Watrack
-call make.bat fpc 11
+call make.bat fpc 12
 if errorlevel 1 goto :Error
 cd icons
-call makeicons.bat fpc 11
+call makeicons.bat fpc 12
 if errorlevel 1 goto :Error
 popd
 
 pushd Dbx_mmap_SA\Cryptors\Athena
-call make.bat fpc 11
+call make.bat fpc 12
 if errorlevel 1 goto :Error
 popd
 
