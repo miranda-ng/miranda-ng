911aa265efca7b33045405f3ac0f0a3c65688edf
Kirill Volinsky <mataes2007@gmail.com>
2013-11-09 13:43:42 +0000

    added projects and solutions for vc2013
    git-svn-id: http://svn.miranda-ng.org/main/trunk@6831 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

---
+++ bin12/pascal64.bat
@@ -0,0 +1,43 @@
+rem @echo off
+
+pushd ..\plugins
+
+pushd Actman
+call make.bat fpc64 11
+if errorlevel 1 goto :Error
+popd
+
+pushd Actman30
+call make.bat fpc64 11
+if errorlevel 1 goto :Error
+popd
+
+pushd mRadio
+call make.bat fpc64 11
+if errorlevel 1 goto :Error
+popd
+
+pushd QuickSearch
+call make.bat fpc64 11
+if errorlevel 1 goto :Error
+popd
+
+pushd Watrack
+call make.bat fpc64 11
+if errorlevel 1 goto :Error
+cd icons
+call makeicons.bat fpc64 11
+if errorlevel 1 goto :Error
+popd
+
+pushd Dbx_mmap_SA\Cryptors\Athena
+call make.bat fpc64 11
+if errorlevel 1 goto :Error
+popd
+
+popd
+goto :eof
+
+:Error
+echo ============================= FAIL! =============================
+exit
