22e77c2e8b503124498d414bc217fa4e66c1432e
George Hazan <george.hazan@gmail.com>
2013-11-28 20:18:40 +0000

    updated mmap resources
    git-svn-id: http://svn.miranda-ng.org/main/trunk@7007 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Db3x_mmap/src/resource.h
+++ plugins/Db3x_mmap/src/resource.h
@@ -1,30 +1,23 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
-// Used by resource.rc
+// Microsoft Visual C++ generated include file.
+// Used by D:\Myranda\plugins\Db3x_mmap\res\db3x_mmap.rc
 //
-#define IDC_NOTOALL                     3
-#define IDD_INSTALLINI                  235
-#define IDD_WARNINICHANGE               236
-#define IDD_INIIMPORTDONE               237
-#define IDC_ININAME                     1333
-#define IDC_VIEWINI                     1334
-#define IDC_SECURITYINFO                1335
-#define IDC_SETTINGNAME                 1336
-#define IDC_NEWVALUE                    1337
-#define IDC_WARNNOMORE                  1338
-#define IDC_DELETE                      1339
-#define IDC_RECYCLE                     1340
-#define IDC_NEWNAME                     1341
-#define IDC_MOVE                        1342
-#define IDC_LEAVE                       1343
+#define IDI_ICONPASS                    100
+#define IDD_LOGIN                       101
+#define IDD_CHANGEPASS                  102
+#define IDC_HEADERBAR                   1001
+#define IDC_LANG                        1002
+#define IDC_USERPASS                    1003
+#define IDC_USERPASS1                   1004
+#define IDC_USERPASS2                   1005
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        269
-#define _APS_NEXT_COMMAND_VALUE         40018
-#define _APS_NEXT_CONTROL_VALUE         1657
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
