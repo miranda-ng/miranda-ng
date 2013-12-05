5800b6ac596faae2c6849df75011d40d25c1aadc
George Hazan <george.hazan@gmail.com>
2013-12-05 22:37:43 +0000

    version bump
    git-svn-id: http://svn.miranda-ng.org/main/trunk@7063 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Db3x_mmap/src/version.h
+++ plugins/Db3x_mmap/src/version.h
@@ -1,14 +1,14 @@
-#define __MAJOR_VERSION				0
-#define __MINOR_VERSION				11
-#define __RELEASE_NUM				0
-#define __BUILD_NUM					1
+#define __MAJOR_VERSION          0
+#define __MINOR_VERSION         11
+#define __RELEASE_NUM            0
+#define __BUILD_NUM              2
 
-#define __FILEVERSION_STRING		__MAJOR_VERSION,__MINOR_VERSION,__RELEASE_NUM,__BUILD_NUM
+#define __FILEVERSION_STRING     __MAJOR_VERSION,__MINOR_VERSION,__RELEASE_NUM,__BUILD_NUM
 
-#define __PLUGIN_NAME				"Miranda NG mmap database driver"
-#define __FILENAME					"Dbx_mmap.dll"
-#define __DESCRIPTION 				"Provides Miranda database support: global settings, contacts, history, settings per contact."
-#define __AUTHOR					"Miranda-NG project"
-#define __AUTHOREMAIL				"bio@msx.ru; ghazan@miranda.im"
-#define __AUTHORWEB					"http://miranda-ng.org/p/Dbx_mmap/"
-#define __COPYRIGHT					"© 2012-13 Miranda NG project"
+#define __PLUGIN_NAME            "Miranda NG mmap database driver"
+#define __FILENAME               "Dbx_mmap.dll"
+#define __DESCRIPTION            "Provides Miranda database support: global settings, contacts, history, settings per contact."
+#define __AUTHOR                 "Miranda-NG project"
+#define __AUTHOREMAIL            "bio@msx.ru; ghazan@miranda.im"
+#define __AUTHORWEB              "http://miranda-ng.org/p/Dbx_mmap/"
+#define __COPYRIGHT              "© 2012-13 Miranda NG project"
