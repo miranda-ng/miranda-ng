cf77bdeca036ce292aa334d352a194209a45cf3d
Tobias Weimer <wishmaster51@googlemail.com>
2014-01-17 19:58:13 +0000

    minor options redesign
    git-svn-id: http://svn.miranda-ng.org/main/trunk@7697 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/MSN/src/resource.h
+++ protocols/MSN/src/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by msn.rc
+// Used by ..\res\msn.rc
 //
 #define IDD_CHATROOM_INVITE             40
 #define IDI_MSN                         101
