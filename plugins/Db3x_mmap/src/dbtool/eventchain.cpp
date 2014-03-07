bf1057988f5e011f6b4d4b5dea3a45fbc4e5b9f2
George Hazan <george.hazan@gmail.com>
2014-03-07 14:57:23 +0000

    special message for fixing zero contact ids
    git-svn-id: http://svn.miranda-ng.org/main/trunk@8450 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Db3x_mmap/src/dbtool/eventchain.cpp
+++ plugins/Db3x_mmap/src/dbtool/eventchain.cpp
@@ -186,8 +186,10 @@ int CDb3Mmap::WorkEventChain(DWORD ofsContact, DBContact *dbc, int firstTime)
 		dbeOld.flags &= ~DBEF_FIRST;
 	}
 
-	if (dbeOld.contactID == 0)
+	if (dbeOld.contactID == 0) {
+		cb->pfnAddLogMessage(STATUS_WARNING, TranslateT("Zero event's contact id: correcting"));
 		dbeOld.contactID = dbc->dwContactID;
+	}
 
 	if (dbeOld.flags & ~DBEF_ALL) {
 		cb->pfnAddLogMessage(STATUS_WARNING, TranslateT("Extra flags found in event: removing"));
