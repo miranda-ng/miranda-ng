2b72ef9487d1487cb4096b359c3dec675ae2703e
George Hazan <george.hazan@gmail.com>
2014-03-22 09:36:17 +0000

    crash fix
    git-svn-id: http://svn.miranda-ng.org/main/trunk@8678 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/DbEditorPP/src/settinglist.cpp
+++ plugins/DbEditorPP/src/settinglist.cpp
@@ -350,12 +350,12 @@ static LRESULT CALLBACK SettingLabelEditSubClassProc(HWND hwnd,UINT msg,WPARAM w
 		switch (LOWORD(wParam)) {
 		case IDOK:
 			{
-				int len = GetWindowTextLength(hwnd) + 1;
+				int len = GetWindowTextLengthA(hwnd) + 1;
 				char *value = (char*)_alloca(len);
 				WCHAR *wc = NULL;
 				DBVARIANT dbv = { 0 };
 
-				GetWindowText(hwnd, value, len);
+				GetWindowTextA(hwnd, value, len);
 
 				if (info->unicode)
 					wc = mir_a2u(value);
