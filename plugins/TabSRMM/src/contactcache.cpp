20f4a5534324ba89fa08f2a6948b1400547c9362
George Hazan <george.hazan@gmail.com>
2014-03-27 20:56:02 +0000

    crash fix
    git-svn-id: http://svn.miranda-ng.org/main/trunk@8779 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/TabSRMM/src/contactcache.cpp
+++ plugins/TabSRMM/src/contactcache.cpp
@@ -150,7 +150,7 @@ bool CContactCache::updateStatus()
  */
 void CContactCache::updateMeta(bool fForce)
 {
-	m_szMetaProto = (m_Valid) ? GetContactProto(cc->pSubs[cc->nDefault]) : NULL;
+	m_szMetaProto = (m_Valid) ? GetContactProto(db_mc_getDefault(cc->contactID)) : NULL;
 }
 
 /**
