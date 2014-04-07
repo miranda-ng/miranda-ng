efd6bf672b9aa5c17586583936e02f9ca3ee2786
Dart Raiden <wowemuh@gmail.com>
2014-04-07 21:08:47 +0000

    IRC: more correct name
    git-svn-id: http://svn.miranda-ng.org/main/trunk@8881 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/IRCG/src/options.cpp
+++ protocols/IRCG/src/options.cpp
@@ -1671,7 +1671,7 @@ int CIrcProto::OnInitOptionsPages(WPARAM wParam, LPARAM)
 	Options_AddPage(wParam, &odp);
 
 	odp.pszTemplate = MAKEINTRESOURCEA(IDD_PREFS_CTCP);
-	odp.ptszTab = LPGENT("CTCP");
+	odp.ptszTab = LPGENT("DCC and CTCP");
 	odp.dwInitParam = (LPARAM)&OptCreateConn;
 	OptCreateConn.create = CCtcpPrefsDlg::Create;
 	OptCreateConn.param = this;
