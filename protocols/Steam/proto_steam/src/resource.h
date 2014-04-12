af1927ce05cd07c036c9fb73c363c3b489fc6bc7
Kirill Volinsky <mataes2007@gmail.com>
2014-04-12 14:25:56 +0000

    added proto_steam
    git-svn-id: http://svn.miranda-ng.org/main/trunk@8963 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

---
+++ protocols/Steam/proto_steam/src/resource.h
@@ -0,0 +1,20 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Proto_IRC.rc
+//
+#define IDI_ICON1                       105
+#define IDI_ICON2                       104
+#define IDI_ICON3                       128
+#define IDI_ICON4                       158
+#define IDI_ICON5                       1003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
