3eafcb2dca7f477fc1e447400065faf5a7c3d17d
Kirill Volinsky <mataes2007@gmail.com>
2014-04-18 06:03:22 +0000

    rename options menu item
    git-svn-id: http://svn.miranda-ng.org/main/trunk@8989 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/WhoUsesMyFiles/src/wumfplug.cpp
+++ plugins/WhoUsesMyFiles/src/wumfplug.cpp
@@ -479,7 +479,7 @@ int OptionsInit(WPARAM wparam, LPARAM)
 	odp.position = 945000000;
 	odp.hInstance = hInst;
 	odp.pszTemplate = MAKEINTRESOURCEA(IDD_OPTIONS);
-	odp.pszTitle = LPGEN("Wumf");
+	odp.pszTitle = LPGEN("Who uses my files");
 	odp.pfnDlgProc = OptionsDlgProc;
 	odp.pszGroup = LPGEN("Services");
 	odp.flags = ODPF_BOLDGROUPS;
