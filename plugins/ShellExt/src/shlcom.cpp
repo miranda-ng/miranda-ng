69f2ca602c2ee8871abb591093a39209fc920931
Kirill Volinsky <mataes2007@gmail.com>
2014-04-22 10:55:09 +0000

    Translation fix
    git-svn-id: http://svn.miranda-ng.org/main/trunk@9038 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/ShellExt/src/shlcom.cpp
+++ plugins/ShellExt/src/shlcom.cpp
@@ -414,7 +414,7 @@ void __stdcall ipcService(ULONG_PTR dwParam)
 			pMMT->dwFlags = HIPC_NOICONS;
 
 		// see if we have a custom string for 'Miranda'
-		szMiranda = Translate("Miranda");
+		szMiranda = _T("Miranda");
 		lstrcpynA(pMMT->MirandaName, szMiranda, sizeof(pMMT->MirandaName) - 1);
 
 		// for the MRU menu
