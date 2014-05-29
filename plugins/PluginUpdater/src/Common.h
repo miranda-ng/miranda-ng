672727906d73b5ad2fe5efe497586db655709a90
Tobias Weimer <wishmaster51@googlemail.com>
2014-05-29 16:09:27 +0000

    compilation fix
    git-svn-id: http://svn.miranda-ng.org/main/trunk@9352 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/PluginUpdater/src/Common.h
+++ plugins/PluginUpdater/src/Common.h
@@ -193,7 +193,7 @@ bool  ParseHashes(const TCHAR *ptszUrl, ptrT &baseUrl, SERVLIST &arHashes);
 int   CompareHashes(const ServListEntry *p1, const ServListEntry *p2);
 
 TCHAR* GetDefaultUrl();
-BOOL   DownloadFile(FILEURL *pFileURL, HANDLE &nlc);
+bool   DownloadFile(FILEURL *pFileURL, HANDLE &nlc);
 
 void  ShowPopup(HWND hDlg, LPCTSTR Title, LPCTSTR Text, int Number, int ActType, bool NoMessageBox = false);
 void  __stdcall RestartMe(void*);
