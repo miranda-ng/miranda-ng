f155b10a746a08532b83c3e7fc78e054e3559059
René Schümann <white06tiger@gmail.com>
2014-07-21 00:46:53 +0000

    Plugin Updater: ! fixed dialog destruction
    @robyer, EndDialog is only to be used with DialogBox*() and not CreateDialog*() we use here... that's why WM_DESTROY "was" needed.
    
    git-svn-id: http://svn.miranda-ng.org/main/trunk@9894 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/PluginUpdater/src/DlgUpdate.cpp
+++ plugins/PluginUpdater/src/DlgUpdate.cpp
@@ -51,7 +51,7 @@ static void ApplyUpdates(void *param)
 
 	// 1) If we need to escalate priviledges, launch a stub
 	if (!PrepareEscalation()) {
-		EndDialog(hDlg, 0);
+		DestroyWindow(hDlg);
 		return;
 	}
 
@@ -137,8 +137,7 @@ static void ApplyUpdates(void *param)
 
 	// 5) Prepare Restart
 	int rc = MessageBox(hDlg, TranslateT("Update complete. Press Yes to restart Miranda now or No to postpone a restart until the exit."), TranslateT("Plugin Updater"), MB_YESNO | MB_ICONQUESTION);
-	EndDialog(hDlg, 0);
-	PostMessage(hDlg, WM_DESTROY, 0, 0); // why do we have to call this manually?
+	DestroyWindow(hDlg);
 	if (rc == IDYES)
 		CallFunctionAsync(RestartMe, 0);
 }
