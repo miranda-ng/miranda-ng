24e2a8bb9844345d58c2cf9317d7dec73c332cba
Robert Pösel <robyer@seznam.cz>
2014-07-21 13:34:28 +0000

    TabSRMM: Fix #741 (Browsing last messages sent (using Ctrl + up) breaks particular Unicode characters)
    git-svn-id: http://svn.miranda-ng.org/main/trunk@9902 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/TabSRMM/src/contactcache.cpp
+++ plugins/TabSRMM/src/contactcache.cpp
@@ -281,7 +281,7 @@ void CContactCache::saveHistory(WPARAM wParam, LPARAM lParam)
 		m_iHistoryTop = (int)wParam;
 	}
 
-	szFromStream = ::Message_GetFromStream(GetDlgItem(m_hwnd, IDC_MESSAGE), m_dat, (CP_UTF8 << 16) | (SF_RTFNOOBJS | SFF_PLAINRTF | SF_USECODEPAGE));
+	szFromStream = ::Message_GetFromStream(GetDlgItem(m_hwnd, IDC_MESSAGE), m_dat, (CP_UTF8 << 16) | (SF_RTFNOOBJS | SFF_PLAINRTF | SF_USECODEPAGE | SF_NCRFORNONASCII));
 
 	iLength = iStreamLength = (strlen(szFromStream) + 1);
 
