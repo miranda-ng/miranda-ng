7ae400e128d88851138f1d4dbcf4c3ec508d9163
George Hazan <george.hazan@gmail.com>
2014-07-31 20:13:33 +0000

    hide avatar in the info panel if there's nothing to display
    git-svn-id: http://svn.miranda-ng.org/main/trunk@10011 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/TabSRMM/src/msgdialog.cpp
+++ plugins/TabSRMM/src/msgdialog.cpp
@@ -1643,6 +1643,7 @@ INT_PTR CALLBACK DlgProcMessage(HWND hwndDlg, UINT msg, WPARAM wParam, LPARAM lP
 				SetWindowPos(dat->hwndPanelPicParent, HWND_TOP, rc.left - 2, rc.top, rc.right - rc.left, (rc.bottom - rc.top) + 1, 0);
 				ShowWindow(dat->hwndPanelPicParent, (dat->panelWidth == -1) || !dat->Panel->isActive() ? SW_HIDE : SW_SHOW);
 			}
+			else ShowWindow(dat->hwndPanelPicParent, SW_HIDE);
 
 			dat->rcPic = rc;
 
