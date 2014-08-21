d33b48c34e2ce98bd5a73698006be3859a7dfbc4
René Schümann <white06tiger@gmail.com>
2014-08-21 11:42:10 +0000

    IEHistory: prepared file structure (project file coming with NG compatibility update)
    git-svn-id: http://svn.miranda-ng.org/main/trunk@10252 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/IEHistory/pack source.bat
+++
@@ -1,8 +0,0 @@
-for /F "tokens=5-9* delims=. " %%i in (docs\IEHistory_readme.txt) do (call :Pack %%i %%j %%k %%l; exit)
-
-:Pack
-d:\usr\PowerArchiver\pacl\pacomp.exe -p -a -c2 "IEHistory src %1.%2.%3.%4.zip" @files_source.txt -x*.zip -x*.ncb -x*.user
-exit
-
-error:
-echo "Error packing IEHistory"
