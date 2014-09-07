13397e846cccf55c9d1dfdd1cc91a3c59b153454
RMN <rmn@miranda-ng.org>
2014-09-07 20:03:59 +0000

    - langpacks/russian: typo - langpacks/ukrainian: update according to [10395] - langpacks/ukrainian: UpdateLang.bat to empty "Untranslated" folder before running the script
    git-svn-id: http://svn.miranda-ng.org/main/trunk@10401 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- langpacks/ukrainian/UpdateLang.bat
+++ langpacks/ukrainian/UpdateLang.bat
@@ -1,6 +1,7 @@
 for %%* in (.) do set CurrDirName=%%~n*
 set langdir=%CD%
 set lang=%CurrDirName%
+del /f /q %langdir%\Untranslated\*.txt
 
 pushd ..\..\tools\lpgen
 cscript /nologo translate.js /sourcelang:"%lang%" /noref:"yes" /popuntranslated:"yes" /out:"%langdir%" /untranslated:"%langdir%\Untranslated"
