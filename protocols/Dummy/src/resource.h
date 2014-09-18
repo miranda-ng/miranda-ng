a4861f09eb12744e28718da2e38dda9482b5dc2a
Robert Pösel <robyer@seznam.cz>
2014-09-18 09:39:45 +0000

    Implement basic Dummy protocol
    git-svn-id: http://svn.miranda-ng.org/main/trunk@10506 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

---
+++ protocols/Dummy/src/resource.h
@@ -0,0 +1,19 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by D:\Development\Miranda NG\protocols\Dummy\res\resource.rc
+//
+#define IDD_ACCMGRUI                    101
+#define IDC_ID_TEXT                     1030
+#define IDC_ID_SETTING                  1031
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1032
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
