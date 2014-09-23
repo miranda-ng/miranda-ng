2be198e70035b758e0cfbd468d551051f9b5d939
Sergey Bolhovskoy <elzorfox@ya.ru>
2014-09-23 10:27:30 +0000

    VKontakte: support my avatar
    git-svn-id: http://svn.miranda-ng.org/main/trunk@10563 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/VKontakte/src/vk_proto.cpp
+++ protocols/VKontakte/src/vk_proto.cpp
@@ -39,6 +39,7 @@ CVkProto::CVkProto(const char *szModuleName, const TCHAR *ptszUserName) :
 	CreateProtoService(PS_CREATEACCMGRUI, &CVkProto::SvcCreateAccMgrUI);
 	CreateProtoService(PS_GETAVATARINFOT, &CVkProto::SvcGetAvatarInfo);
 	CreateProtoService(PS_GETAVATARCAPS, &CVkProto::SvcGetAvatarCaps);
+	CreateProtoService(PS_GETMYAVATART, &CVkProto::SvcGetMyAvatar);
 	CreateProtoService(PS_SET_LISTENINGTO, &CVkProto::SvcSetListeningTo);
 
 	HookProtoEvent(ME_OPT_INITIALISE, &CVkProto::OnOptionsInit);
