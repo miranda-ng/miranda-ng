d23bd66468ce325617f5fe88ef1549c07738181b
Vadim Dashevskiy <watcherhd@gmail.com>
2014-10-05 21:00:37 +0000

    bump (for msn ini update)
    git-svn-id: http://svn.miranda-ng.org/main/trunk@10703 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Weather/src/version.h
+++ plugins/Weather/src/version.h
@@ -1,7 +1,7 @@
 #define __MAJOR_VERSION           0
 #define __MINOR_VERSION           4
 #define __RELEASE_NUM             0
-#define __BUILD_NUM               1
+#define __BUILD_NUM               2
 
 #include <stdver.h>
 
