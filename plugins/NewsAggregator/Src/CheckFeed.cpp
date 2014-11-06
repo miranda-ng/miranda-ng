6f501f02482213189550111e434e48dc192d3683
George Hazan <george.hazan@gmail.com>
2014-11-06 21:40:38 +0000

    doubles search algorithm optimized
    git-svn-id: http://svn.miranda-ng.org/main/trunk@10920 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/NewsAggregator/Src/CheckFeed.cpp
+++ plugins/NewsAggregator/Src/CheckFeed.cpp
@@ -136,6 +136,10 @@ static void XmlToMsg(MCONTACT hContact, CMString &title, CMString &link, CMStrin
 		olddbei.pBlob = pbBuffer;
 		db_event_get(hDbEvent, &olddbei);
 
+		// there's no need to look for the elder events
+		if (olddbei.timestamp < stamp)
+			break;
+
 		if (olddbei.cbBlob == cbOrigLen + 1 && !lstrcmpA((char*)olddbei.pBlob, pszTemp)) {
 			MesExist = true;
 			break;
