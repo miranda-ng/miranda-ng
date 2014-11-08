27d886fcbfe87713be0fd970a2c8ebc1056b60ec
Kirill Volinsky <mataes2007@gmail.com>
2014-11-08 14:24:55 +0000

    removed not needed defines
    git-svn-id: http://svn.miranda-ng.org/main/trunk@10926 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/CrashDumper/src/sdkstuff.h
+++ plugins/CrashDumper/src/sdkstuff.h
@@ -16,64 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef __in_bcount_opt
-#define __in_bcount_opt(x)
-#endif
-
-#ifndef __out_bcount_opt
-#define __out_bcount_opt(x)
-#endif
-
 #include "dbghelp.h"
-
-
-#ifndef PRODUCT_ULTIMATE
-
-#define PRODUCT_UNDEFINED                       0x00000000
-
-#define PRODUCT_ULTIMATE                        0x00000001
-#define PRODUCT_HOME_BASIC                      0x00000002
-#define PRODUCT_HOME_PREMIUM                    0x00000003
-#define PRODUCT_ENTERPRISE                      0x00000004
-#define PRODUCT_HOME_BASIC_N                    0x00000005
-#define PRODUCT_BUSINESS                        0x00000006
-#define PRODUCT_STANDARD_SERVER                 0x00000007
-#define PRODUCT_DATACENTER_SERVER               0x00000008
-#define PRODUCT_SMALLBUSINESS_SERVER            0x00000009
-#define PRODUCT_ENTERPRISE_SERVER               0x0000000A
-#define PRODUCT_STARTER                         0x0000000B
-#define PRODUCT_DATACENTER_SERVER_CORE          0x0000000C
-#define PRODUCT_STANDARD_SERVER_CORE            0x0000000D
-#define PRODUCT_ENTERPRISE_SERVER_CORE          0x0000000E
-#define PRODUCT_ENTERPRISE_SERVER_IA64          0x0000000F
-#define PRODUCT_BUSINESS_N                      0x00000010
-#define PRODUCT_WEB_SERVER                      0x00000011
-#define PRODUCT_CLUSTER_SERVER                  0x00000012
-#define PRODUCT_HOME_SERVER                     0x00000013
-#define PRODUCT_STORAGE_EXPRESS_SERVER          0x00000014
-#define PRODUCT_STORAGE_STANDARD_SERVER         0x00000015
-#define PRODUCT_STORAGE_WORKGROUP_SERVER        0x00000016
-#define PRODUCT_STORAGE_ENTERPRISE_SERVER       0x00000017
-#define PRODUCT_SERVER_FOR_SMALLBUSINESS        0x00000018
-#define PRODUCT_SMALLBUSINESS_SERVER_PREMIUM    0x00000019
-
-#define PRODUCT_UNLICENSED                      0xABCDABCD
-
-#define SM_TABLETPC             86
-#define SM_MEDIACENTER          87
-#define SM_STARTER              88
-#define SM_SERVERR2             89
-
-#endif
-
-#ifndef VER_SUITE_STORAGE_SERVER
-#define VER_SUITE_STORAGE_SERVER            0x00002000
-#define VER_SUITE_COMPUTE_SERVER            0x00004000
-#endif
-
-#ifndef PF_NX_ENABLED
-#define PF_NX_ENABLED 12
-#endif
 
 typedef struct _IMAGEHLP_MODULE64_V2 {
 	DWORD    SizeOfStruct;           // set to sizeof(IMAGEHLP_MODULE64)
