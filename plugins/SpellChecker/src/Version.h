44c8c7dc641f90b0272c877e31291d92bd2f9e5b
George Hazan <george.hazan@gmail.com>
2014-11-18 15:12:50 +0000

    fix for text underlining in SpellChecker
    git-svn-id: http://svn.miranda-ng.org/main/trunk@11012 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/SpellChecker/src/Version.h
+++ plugins/SpellChecker/src/Version.h
@@ -1,14 +1,14 @@
-#define __MAJOR_VERSION            0
-#define __MINOR_VERSION            2
-#define __RELEASE_NUM            6
-#define __BUILD_NUM               1
+#define __MAJOR_VERSION           0
+#define __MINOR_VERSION           2
+#define __RELEASE_NUM             6
+#define __BUILD_NUM               2
 
 #include <stdver.h>
 
 #define __PLUGIN_NAME             "Spell checker"
-#define __FILENAME               "SpellChecker.dll"
+#define __FILENAME                "SpellChecker.dll"
 #define __DESCRIPTION             "Spell checker for the message windows. Uses Hunspell to do the checking."
-#define __AUTHOR               "Ricardo Pescuma Domenecci, FREAK_THEMIGHTY"
-#define __AUTHOREMAIL            "pescuma@miranda-im.org"
+#define __AUTHOR                  "Ricardo Pescuma Domenecci, FREAK_THEMIGHTY"
+#define __AUTHOREMAIL             "pescuma@miranda-im.org"
 #define __AUTHORWEB               "http://miranda-ng.org/p/SpellChecker/"
 #define __COPYRIGHT               "© 2006-2010 Ricardo Pescuma Domenecci"
