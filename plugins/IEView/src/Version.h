faf02a11c53074dc85aa66e617d868a1901580d1
George Hazan <george.hazan@gmail.com>
2014-11-18 21:52:23 +0000

    crash fix
    git-svn-id: http://svn.miranda-ng.org/main/trunk@11018 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/IEView/src/Version.h
+++ plugins/IEView/src/Version.h
@@ -1,14 +1,14 @@
-#define __MAJOR_VERSION            1
-#define __MINOR_VERSION            4
+#define __MAJOR_VERSION          1
+#define __MINOR_VERSION          4
 #define __RELEASE_NUM            0
-#define __BUILD_NUM               1
+#define __BUILD_NUM              2
 
 #include <stdver.h>
 
 #define __PLUGIN_NAME            "IEView"
 #define __FILENAME               "IEView.dll"
-#define __DESCRIPTION             "IE Based Chat Log."
-#define __AUTHOR               "Piotr Piastucki, Francois Mean"
+#define __DESCRIPTION            "IE Based Chat Log."
+#define __AUTHOR                 "Piotr Piastucki, Francois Mean"
 #define __AUTHOREMAIL            "the_leech@users.berlios.de"
-#define __AUTHORWEB               "http://miranda-ng.org/p/IEView/"
-#define __COPYRIGHT               "© 2005-2011 Piotr Piastucki, Francois Mean"
+#define __AUTHORWEB              "http://miranda-ng.org/p/IEView/"
+#define __COPYRIGHT              "© 2005-2011 Piotr Piastucki, Francois Mean"
