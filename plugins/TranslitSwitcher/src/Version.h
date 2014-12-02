4a6753b9df99107d0e83e1ed10f0effe0d00d9db
George Hazan <george.hazan@gmail.com>
2014-12-02 23:25:04 +0000

    bugfix: last symbol got eaten
    git-svn-id: http://svn.miranda-ng.org/main/trunk@11225 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/TranslitSwitcher/src/Version.h
+++ plugins/TranslitSwitcher/src/Version.h
@@ -1,14 +1,14 @@
-#define __MAJOR_VERSION            0
-#define __MINOR_VERSION            0
-#define __RELEASE_NUM            1
-#define __BUILD_NUM               0
+#define __MAJOR_VERSION           0
+#define __MINOR_VERSION           1
+#define __RELEASE_NUM             0
+#define __BUILD_NUM               1
 
 #include <stdver.h>
 
 #define __PLUGIN_NAME            "Switch layout or transliterate"
 #define __FILENAME               "TranslitSwitcher.dll"
-#define __DESCRIPTION             "Allows to switch a layout or transliterate or invert case of the entered text in the message window with SmileyAdd support."
-#define __AUTHOR               "Mataes, tico-tico, Tim"
+#define __DESCRIPTION            "Allows to switch a layout or transliterate or invert case of the entered text in the message window with SmileyAdd support."
+#define __AUTHOR                 "Mataes, tico-tico, Tim"
 #define __AUTHOREMAIL            "mataes2007@gmail.com"
-#define __AUTHORWEB               "http://miranda-ng.org/p/TranslitSwitcher/"
-#define __COPYRIGHT               "© 2011-2010 Mataes, tico-tico © 2007 Dmitry Titkov"
+#define __AUTHORWEB              "http://miranda-ng.org/p/TranslitSwitcher/"
+#define __COPYRIGHT              "© 2011-2010 Mataes, tico-tico © 2007 Dmitry Titkov"
