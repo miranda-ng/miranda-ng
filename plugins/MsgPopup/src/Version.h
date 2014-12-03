5a8c0ced3f1fd3e523512696b615174e3a074d6e
George Hazan <george.hazan@gmail.com>
2014-12-03 13:42:46 +0000

    adaptation for 64 bits
    git-svn-id: http://svn.miranda-ng.org/main/trunk@11229 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/MsgPopup/src/Version.h
+++ plugins/MsgPopup/src/Version.h
@@ -1,14 +1,14 @@
-#define __MAJOR_VERSION            0
-#define __MINOR_VERSION            1
-#define __RELEASE_NUM            1
-#define __BUILD_NUM               3
+#define __MAJOR_VERSION          0
+#define __MINOR_VERSION          1
+#define __RELEASE_NUM            2
+#define __BUILD_NUM              1
 
 #include <stdver.h>
 
 #define __PLUGIN_NAME            "Message popup"
 #define __FILENAME               "MsgPopup.dll"
-#define __DESCRIPTION             "Plugin replaces message boxes (those having only OK button) with popups."
-#define __AUTHOR               "StDenis"
+#define __DESCRIPTION            "Plugin replaces message boxes (those having only OK button) with popups."
+#define __AUTHOR                 "StDenis"
 #define __AUTHOREMAIL            "stdenformiranda@fromru.com"
-#define __AUTHORWEB               "http://miranda-ng.org/p/MsgPopup/"
-#define __COPYRIGHT               "© 2004 Denis Stanishevskiy"
+#define __AUTHORWEB              "http://miranda-ng.org/p/MsgPopup/"
+#define __COPYRIGHT              "© 2004 Denis Stanishevskiy"
