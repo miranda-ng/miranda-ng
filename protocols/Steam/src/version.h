a5b32e32641217a9202c8af86f5d2f64934be690
Robert Pösel <robyer@seznam.cz>
2014-12-09 19:09:40 +0000

    Steam: That's enough from me, version bump
    git-svn-id: http://svn.miranda-ng.org/main/trunk@11308 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/Steam/src/version.h
+++ protocols/Steam/src/version.h
@@ -1,6 +1,6 @@
 #define __MAJOR_VERSION            0
 #define __MINOR_VERSION            11
-#define __RELEASE_NUM              1
+#define __RELEASE_NUM              2
 #define __BUILD_NUM                0
 
 #include <stdver.h>
