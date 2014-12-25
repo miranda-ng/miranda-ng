e70ffdf47fdcaf0df298db05372564277697a02d
Kirill Volinsky <mataes2007@gmail.com>
2014-12-25 19:23:29 +0000

    files structure changed
    git-svn-id: http://svn.miranda-ng.org/main/trunk@11637 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

---
+++ src/core/stduseronline/src/version.h
@@ -0,0 +1,12 @@
+#include <m_version.h>
+
+#define __FILEVERSION_STRING     MIRANDA_VERSION_FILEVERSION
+#define __VERSION_STRING         MIRANDA_VERSION_STRING
+
+#define __PLUGIN_NAME            "Standard User-Is-Online module"
+#define __FILENAME               "stduseronline.dll"
+#define __DESCRIPTION            "Core module for user-is-online event processing."
+#define __AUTHOR                 "Miranda NG Development Team"
+#define __AUTHOREMAIL            ""
+#define __AUTHORWEB              "http://miranda-ng.org/p/StdUserOnline/"
+#define __COPYRIGHT              "© 2012 Miranda NG Development Team"
