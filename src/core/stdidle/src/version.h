a12a4be8d750d5f6c44950a40159f175348ca5e7
Kirill Volinsky <mataes2007@gmail.com>
2014-12-25 19:00:41 +0000

    files structure changed
    git-svn-id: http://svn.miranda-ng.org/main/trunk@11634 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

---
+++ src/core/stdidle/src/version.h
@@ -0,0 +1,12 @@
+#include <m_version.h>
+
+#define __FILEVERSION_STRING     MIRANDA_VERSION_FILEVERSION
+#define __VERSION_STRING         MIRANDA_VERSION_STRING
+
+#define __PLUGIN_NAME            "Standard Idle module"
+#define __FILENAME               "stdidle.dll"
+#define __DESCRIPTION            "Core module for idle state processing."
+#define __AUTHOR                 "Miranda NG Development Team"
+#define __AUTHOREMAIL            ""
+#define __AUTHORWEB              "http://miranda-ng.org/p/StdIdle/"
+#define __COPYRIGHT              "© 2012 Miranda NG Development Team"
