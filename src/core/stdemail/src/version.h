f3db9af4cc81484ef1b6529934d30adb77292b7d
Kirill Volinsky <mataes2007@gmail.com>
2014-12-25 18:33:15 +0000

    files structure changed
    git-svn-id: http://svn.miranda-ng.org/main/trunk@11631 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

---
+++ src/core/stdemail/src/version.h
@@ -0,0 +1,12 @@
+#include <m_version.h>
+
+#define __FILEVERSION_STRING     MIRANDA_VERSION_FILEVERSION
+#define __VERSION_STRING         MIRANDA_VERSION_STRING
+
+#define __PLUGIN_NAME            "Standard Email module"
+#define __FILENAME               "stdemail.dll"
+#define __DESCRIPTION            "Core module for e-mail urls handling."
+#define __AUTHOR                 "Miranda NG Development Team"
+#define __AUTHOREMAIL            ""
+#define __AUTHORWEB              "http://miranda-ng.org/p/StdEmail/"
+#define __COPYRIGHT              "© 2012 Miranda NG Development Team"
