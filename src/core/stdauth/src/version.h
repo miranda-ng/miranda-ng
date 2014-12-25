cdaff59ae33b92e94209bb22fdc0e693f899f073
Kirill Volinsky <mataes2007@gmail.com>
2014-12-25 17:56:59 +0000

    files structure changed
    git-svn-id: http://svn.miranda-ng.org/main/trunk@11627 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

---
+++ src/core/stdauth/src/version.h
@@ -0,0 +1,12 @@
+#include <m_version.h>
+
+#define __FILEVERSION_STRING			MIRANDA_VERSION_FILEVERSION
+#define __VERSION_STRING			MIRANDA_VERSION_STRING
+
+#define __PLUGIN_NAME				"Standard Auth module"
+#define __FILENAME				"stdauth.dll"
+#define __DESCRIPTION 				"Core module for sending/receiving auth requests."
+#define __AUTHOR                		 "Miranda NG Development Team"
+#define __AUTHOREMAIL				""
+#define __AUTHORWEB				"http://miranda-ng.org/p/StdAuth/"
+#define __COPYRIGHT				"© 2012 Miranda NG Development Team"
