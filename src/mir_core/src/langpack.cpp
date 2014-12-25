12e3e7f057bdb3d965a944d6c97fe7f222158eee
Kirill Volinsky <mataes2007@gmail.com>
2014-12-25 19:42:00 +0000

    files structure changed
    git-svn-id: http://svn.miranda-ng.org/main/trunk@11638 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- src/mir_core/src/langpack.cpp
+++ src/mir_core/src/langpack.cpp
@@ -23,7 +23,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #include "commonheaders.h"
-#include "../modules/langpack/langpack.h"
+#include "../../modules/langpack/langpack.h"
 
 #define LANGPACK_BUF_SIZE 4000
 
