8d86e3673af54d97b8369fdffba2200268ad3fac
Robert Pösel <robyer@seznam.cz>
2014-12-28 17:25:24 +0000

    lpgen/refresher.bat: Add some error handling
    git-svn-id: http://svn.miranda-ng.org/main/trunk@11664 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- langpacks/czech/Refresh.bat
+++ langpacks/czech/Refresh.bat
@@ -11,5 +11,8 @@ REM =========================
 cd "..\..\tools\lpgen\"
 call refresher.bat %language% "%oldLangpackPath%"
 
+REM if previous command failed, exit
+if %ERRORLEVEL% NEQ 0 exit /B 1
+
 REM copy and reload langpack in your Miranda (uncoment next line + enable cmdline.dll plugin to use it)
 call installer.bat %language% "%mirandaPath%"
