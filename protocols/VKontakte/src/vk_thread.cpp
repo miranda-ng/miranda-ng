6554e92c3b73386100f6143424f51933824aa8cd
Sergey Bolhovskoy <elzorfox@ya.ru>
2014-12-31 20:19:20 +0000

    VKontakte: Happy new year!
    git-svn-id: http://svn.miranda-ng.org/main/trunk@11701 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/VKontakte/src/vk_thread.cpp
+++ protocols/VKontakte/src/vk_thread.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2013-14 Miranda NG project (http://miranda-ng.org)
+Copyright (c) 2013-15 Miranda NG project (http://miranda-ng.org)
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
