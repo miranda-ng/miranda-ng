351d379775862b1a22c0014eadc439f6b4f2cedf
Kirill Volinsky <mataes2007@gmail.com>
2015-01-02 16:37:03 +0000

    fix for status icon in tray tooltip
    git-svn-id: http://svn.miranda-ng.org/main/trunk@11723 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Quotes/src/ImportExport.h
+++ plugins/Quotes/src/ImportExport.h
@@ -4,8 +4,6 @@
 INT_PTR Quotes_Export(WPARAM wp, LPARAM lp);
 INT_PTR Quotes_Import(WPARAM wp, LPARAM lp);
 
-#ifdef TEST_IMPORT_EXPORT
 INT_PTR QuotesMenu_ImportAll(WPARAM wp, LPARAM lp);
 INT_PTR QuotesMenu_ExportAll(WPARAM wp, LPARAM lp);
-#endif
 #endif //__F86374E6_713C_4600_85FB_903A5CDF7251_IMPORT_EXPORT_H__
--- plugins/Quotes/src/stdafx.h
+++ plugins/Quotes/src/stdafx.h
@@ -5,7 +5,6 @@
 
 #pragma once
 
-#define TEST_IMPORT_EXPORT
 //#define CHART_IMPLEMENT
 #define _CRT_SECURE_NO_WARNINGS
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
