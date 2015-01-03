1db6a5f4acffe17a3e1d2f74997794e7750f78bf
George Hazan <george.hazan@gmail.com>
2015-01-03 16:51:17 +0000

    StdSsl - standard SSL/TLS driver for Miranda NG
    git-svn-id: http://svn.miranda-ng.org/main/trunk@11746 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

---
+++ src/core/stdssl/src/main.cpp
@@ -0,0 +1,68 @@
+/*
+
+Standard SSL driver for Miranda NG
+
+Copyright (ñ) 2014-15 Miranda NG project (http://miranda-ng.org)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#include "commonheaders.h"
+
+int LoadSslModule(void);
+void UnloadSslModule(void);
+
+HINSTANCE hInst;
+int hLangpack;
+
+PLUGININFOEX pluginInfo = {
+	sizeof(PLUGININFOEX),
+	__PLUGIN_NAME,
+	MIRANDA_VERSION_DWORD,
+	__DESCRIPTION,
+	__AUTHOR,
+	__AUTHOREMAIL,
+	__COPYRIGHT,
+	__AUTHORWEB,
+	UNICODE_AWARE,
+	// 1e64fd80-299e-48a0-9441-de2868563b6f
+	{ 0x1e64fd80, 0x299e, 0x48a0, {0x94, 0x41, 0xde, 0x28, 0x68, 0x56, 0x3b, 0x6f}}
+};
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+	hInst = hinstDLL;
+	return TRUE;
+}
+
+extern "C" __declspec(dllexport) PLUGININFOEX* MirandaPluginInfoEx(DWORD mirandaVersion)
+{
+	return &pluginInfo;
+}
+
+extern "C" __declspec(dllexport) const MUUID MirandaInterfaces[] = { MIID_SSL, MIID_LAST };
+
+extern "C" int __declspec(dllexport) Load(void)
+{
+	mir_getLP(&pluginInfo);
+
+	return LoadSslModule();
+}
+
+extern "C" int __declspec(dllexport) Unload(void)
+{
+	UnloadSslModule();
+	return 0;
+}
