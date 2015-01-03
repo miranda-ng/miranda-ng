b29f2b9f90fd8bf49d2a11f8aa82c5526c280851
George Hazan <george.hazan@gmail.com>
2015-01-03 16:51:17 +0000

    StdSsl - standard SSL/TLS driver for Miranda NG
    git-svn-id: http://svn.miranda-ng.org/main/trunk@11746 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

---
+++ src/core/stdssl/src/commonheaders.h
@@ -0,0 +1,75 @@
+/*
+
+Copyright 2000-12 Miranda IM, 2012-15 Miranda NG project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#define _WIN32_WINNT 0x0501
+#define _WIN32_IE 0x0501
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <windows.h>
+#include <windowsx.h>
+#include <ole2.h>
+#include <shellapi.h>
+#include <shlobj.h>
+#include <commctrl.h>
+#include <vssym32.h>
+#include <Uxtheme.h>
+
+#define SECURITY_WIN32
+#include <security.h>
+#include <schannel.h>
+
+#include <malloc.h>
+#include <time.h>
+
+#include <win2k.h>
+
+#include <newpluginapi.h>
+#include <m_system.h>
+#include <m_database.h>
+#include <m_langpack.h>
+#include <m_button.h>
+#include <m_clist.h>
+#include <m_clistint.h>
+#include <m_clc.h>
+#include <m_clui.h>
+#include <m_options.h>
+#include <m_protosvc.h>
+#include <m_utils.h>
+#include <m_skin.h>
+#include <m_contacts.h>
+#include <m_userinfo.h>
+#include <m_history.h>
+#include <m_addcontact.h>
+#include <m_message.h>
+#include <m_file.h>
+#include <m_icolib.h>
+#include <m_fontservice.h>
+#include <m_timezones.h>
+#include <m_avatars.h>
+#include <m_netlib.h>
+#include <m_metacontacts.h>
+#include <m_popup.h>
+#include <m_ssl.h>
+
+#include "version.h"
+
+extern HINSTANCE g_hInst;
---
+++ src/core/stdssl/src/version.h
@@ -0,0 +1,14 @@
+
+#include <m_version.h>
+
+#define __FILEVERSION_STRING      MIRANDA_VERSION_FILEVERSION
+#define __VERSION_STRING          MIRANDA_VERSION_STRING
+
+#define __PLUGIN_NAME            "Standard SSL Driver"
+#define __INTERNAL_NAME          "stdssl"
+#define __FILENAME               "stdssl.dll"
+#define __DESCRIPTION            "Core driver for Windows native ssl support."
+#define __AUTHOR                 "Miranda NG Development Team"
+#define __AUTHOREMAIL            ""
+#define __AUTHORWEB              "http://miranda-ng.org/p/StdSsl/"
+#define __COPYRIGHT              "© 2014-15 Miranda NG Development Team"
