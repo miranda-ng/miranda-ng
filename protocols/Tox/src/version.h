dfe76cf65e6f20d5f9c92982a241ffecf6bb3405
Alexander Lantsev <aunsane@gmail.com>
2015-02-04 19:24:57 +0000

    Tox: - added delay for resending chunk of file - updated tox core - version bump
    git-svn-id: http://svn.miranda-ng.org/main/trunk@12003 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/Tox/src/version.h
+++ protocols/Tox/src/version.h
@@ -1,14 +1,14 @@
 #define __MAJOR_VERSION            0
 #define __MINOR_VERSION            11
 #define __RELEASE_NUM              0
-#define __BUILD_NUM                4
+#define __BUILD_NUM                5
 
 #include <stdver.h>
 
 #define __PLUGIN_NAME              "Tox protocol"
 #define __FILENAME                 "Tox.dll"
 #define __DESCRIPTION              "Tox protocol support for Miranda NG."
-#define __AUTHOR                   "ForNeVeR, Mataes, unsane"
+#define __AUTHOR                   "Miranda NG Team"
 #define __AUTHOREMAIL              ""
 #define __AUTHORWEB                "http://miranda-ng.org/p/Tox/"
-#define __COPYRIGHT                "© 2014 Miranda NG Team"
+#define __COPYRIGHT                "© 2014-15 Miranda NG Team"
