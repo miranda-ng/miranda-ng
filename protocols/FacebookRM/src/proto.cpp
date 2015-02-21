370ee3e9fdee080643048aa5d13ecffc0370bfc3
Vadim Dashevskiy <watcherhd@gmail.com>
2015-02-21 08:51:44 +0000

    FacebookRM: marking notifications unread fix
    git-svn-id: http://svn.miranda-ng.org/main/trunk@12230 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/FacebookRM/src/proto.cpp
+++ protocols/FacebookRM/src/proto.cpp
@@ -921,7 +921,7 @@ void FacebookProto::ReadNotificationWorker(void *p)
 		return;
 	}
 
-	std::string data = "seen=true&asyncSignal=&__dyn=&__rev=&__req=&alert_ids%5B0%5D=" + utils::url::encode(*id);
+	std::string data = "seen=0&asyncSignal=&__dyn=&__rev=&__req=&alert_ids%5B0%5D=" + utils::url::encode(*id);
 	data += "&fb_dtsg=" + facy.dtsg_;
 	data += "&__user=" + facy.self_.user_id;
 	data += "&ttstamp=" + facy.ttstamp();
