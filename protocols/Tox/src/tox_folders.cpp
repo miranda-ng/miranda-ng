2dce86d9994eb5eb12c4b6215cf4ff3e6b8291ac
Alexander Lantsev <aunsane@gmail.com>
2015-03-05 19:25:49 +0000

    Tox: more correct way to register folders
    git-svn-id: http://svn.miranda-ng.org/main/trunk@12332 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/Tox/src/tox_folders.cpp
+++
@@ -1,8 +0,0 @@
-#include "common.h"
-
-HANDLE CToxProto::hProfileFolderPath = NULL;
-
-void CToxProto::InitFolders()
-{
-	hProfileFolderPath = FoldersRegisterCustomPathT("Tox", Translate("Profiles folder"), MIRANDA_USERDATAT);
-}
