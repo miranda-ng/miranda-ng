72fee5431ff14fff8be1280bf3d75026e6a644d9
Tobias Weimer <wishmaster51@googlemail.com>
2015-03-06 23:30:29 +0000

    Clist_nicer: - compile fix
    git-svn-id: http://svn.miranda-ng.org/main/trunk@12365 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Clist_nicer/src/Coolsb/coolsblib.cpp
+++ plugins/Clist_nicer/src/Coolsb/coolsblib.cpp
@@ -389,7 +389,7 @@ int	WINAPI CoolSB_SetScrollInfo(HWND hwnd, int fnBar, LPSCROLLINFO lpsi, BOOL fR
 
 	SCROLLBAR *sbar = GetScrollBarFromHwnd(hwnd, fnBar);
 	if (sbar == NULL)
-		return;
+		return 0;
 
 	if ((lpsi->fMask & SIF_DISABLENOSCROLL) || (sbar->fScrollFlags & CSBS_THUMBALWAYS)) {
 		if (!sbar->fScrollVisible) {
