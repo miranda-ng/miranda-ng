b07697239f23c21592928ea6531c2e2a04e00527
George Hazan <george.hazan@gmail.com>
2015-03-10 19:03:29 +0000

    fix for the events' sorting order
    git-svn-id: http://svn.miranda-ng.org/main/trunk@12377 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Dbx_mdb/src/commonheaders.h
+++ plugins/Dbx_mdb/src/commonheaders.h
@@ -42,6 +42,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <m_crypto.h>
 #include <m_metacontacts.h>
 #include <m_protocols.h>
+#include <m_netlib.h>
 
 extern "C"
 {
