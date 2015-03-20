1f480a2b40066e478c9ff2824c24b12168fa4573
Vadim Dashevskiy <watcherhd@gmail.com>
2015-03-20 07:42:03 +0000

    Serbian language folder structure cerated, until and if the author provides his sources of the langpack
    git-svn-id: http://svn.miranda-ng.org/main/trunk@12444 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

---
+++ langpacks/serbian/UpdateLang.bat
@@ -0,0 +1,9 @@
+for %%* in (.) do set CurrDirName=%%~n*
+set langdir=%CD%
+set lang=%CurrDirName%
+
+del /f /q %langdir%\Untranslated\*.txt
+
+pushd ..\..\tools\lpgen
+cscript /nologo translate.js /sourcelang:"%lang%" /noref:"yes" /popuntranslated:"yes" /out:"%langdir%" /untranslated:"%langdir%\Untranslated" /langpack:"%langdir%\Deprecated\old_langpack_%lang%.txt"
+popd
