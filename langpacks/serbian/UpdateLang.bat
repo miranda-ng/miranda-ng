a6b2124006307fd92cb3d9f4fd8fe54374c2265e
Vadim Dashevskiy <watcherhd@gmail.com>
2015-03-20 08:39:50 +0000

    unneeded file removed
    git-svn-id: http://svn.miranda-ng.org/main/trunk@12446 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- langpacks/serbian/UpdateLang.bat
+++ langpacks/serbian/UpdateLang.bat
@@ -1,9 +1,10 @@
 for %%* in (.) do set CurrDirName=%%~n*
 set langdir=%CD%
 set lang=%CurrDirName%
+set OldLangpackPath=
 
 del /f /q %langdir%\Untranslated\*.txt
 
 pushd ..\..\tools\lpgen
-cscript /nologo translate.js /sourcelang:"%lang%" /noref:"yes" /popuntranslated:"yes" /out:"%langdir%" /untranslated:"%langdir%\Untranslated" /langpack:"%langdir%\Deprecated\old_langpack_%lang%.txt"
+cscript /nologo translate.js /sourcelang:"%lang%" /noref:"yes" /popuntranslated:"yes" /out:"%langdir%" /untranslated:"%langdir%\Untranslated" /langpack:%OldLangpackPath%
 popd
