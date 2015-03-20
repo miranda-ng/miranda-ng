190307ca7aee92d6b862db0bf78cde10acfc95d0
Vadim Dashevskiy <watcherhd@gmail.com>
2015-03-20 09:38:02 +0000

    git-svn-id: http://svn.miranda-ng.org/main/trunk@12447 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

---
+++ langpacks/serbian/UpdateLangWithEmptyLinesAndRef.bat
@@ -0,0 +1,10 @@
+for %%* in (.) do set CurrDirName=%%~n*
+set langdir=%CD%
+set lang=%CurrDirName%
+set OldLangpackPath=
+
+del /f /q %langdir%\Untranslated\*.txt
+
+pushd ..\..\tools\lpgen
+cscript /nologo translate.js /sourcelang:"%lang%" /out:"%langdir%" /untranslated:"%langdir%\Untranslated" /langpack:%OldLangpackPath%
+popd
