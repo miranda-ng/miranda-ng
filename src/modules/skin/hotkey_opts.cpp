3f52c0846802526781c66d12d7e041cd991bc459
Tobias Weimer <wishmaster51@googlemail.com>
2015-03-23 17:46:06 +0000

    Fix for r12483 -Using TranslateT -Fixed missing break
    git-svn-id: http://svn.miranda-ng.org/main/trunk@12485 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- src/modules/skin/hotkey_opts.cpp
+++ src/modules/skin/hotkey_opts.cpp
@@ -44,7 +44,43 @@ static TCHAR *sttHokeyVkToName(WORD vkKey)
 	case VK_NUMLOCK:
 	case VK_CAPITAL:
 	case VK_SCROLL:
-		return _T("");
+		return TranslateT("");
+	case VK_BROWSER_BACK:
+		return TranslateT("Browser: Back");
+	case VK_BROWSER_FORWARD:
+		return TranslateT("Browser: Forward");
+	case VK_BROWSER_REFRESH:
+		return TranslateT("Browser: Refresh");
+	case VK_BROWSER_STOP:
+		return TranslateT("Browser: Stop");
+	case VK_BROWSER_SEARCH:
+		return TranslateT("Browser: Search");
+	case VK_BROWSER_FAVORITES:
+		return TranslateT("Browser: Fav");
+	case VK_BROWSER_HOME:
+		return TranslateT("Browser: Home");
+	case VK_VOLUME_MUTE:
+		return TranslateT("Mute");
+	case VK_VOLUME_DOWN:
+		return TranslateT("Vol-");
+	case VK_VOLUME_UP:
+		return TranslateT("Vol+");
+	case VK_MEDIA_NEXT_TRACK:
+		return TranslateT("Media: Next Track");
+	case VK_MEDIA_PREV_TRACK:
+		return TranslateT("Media: Prev. Track");
+	case VK_MEDIA_STOP:
+		return TranslateT("Media: Stop");
+	case VK_MEDIA_PLAY_PAUSE:
+		return TranslateT("Media: Play/Pause");
+	case VK_LAUNCH_MAIL:
+		return TranslateT("Mail");
+	case VK_LAUNCH_MEDIA_SELECT:
+		return TranslateT("Media: Select");
+	case VK_LAUNCH_APP1:
+		return TranslateT("App 1");
+	case VK_LAUNCH_APP2:
+		return TranslateT("App 2");
 
 	case VK_DIVIDE:
 	case VK_INSERT:
@@ -58,44 +94,6 @@ static TCHAR *sttHokeyVkToName(WORD vkKey)
 	case VK_UP:
 	case VK_DOWN:
 		code |= (1UL << 24);
-
-	case VK_BROWSER_BACK:
-		return _T("Browser_Back");
-	case VK_BROWSER_FORWARD:
-		return _T("Browser_Forward");
-	case VK_BROWSER_REFRESH:
-		return _T("Browser_Refresh");
-	case VK_BROWSER_STOP:
-		return _T("Browser_Stop");
-	case VK_BROWSER_SEARCH:
-		return _T("Browser_Search");
-	case VK_BROWSER_FAVORITES:
-		return _T("Browser_Fav");
-	case VK_BROWSER_HOME:
-		return _T("Browser_Home");
-	case VK_VOLUME_MUTE:
-		return _T("Mute");
-	case VK_VOLUME_DOWN:
-		return _T("Vol-");
-	case VK_VOLUME_UP:
-		return _T("Vol+");
-	case VK_MEDIA_NEXT_TRACK:
-		return _T("Media_Next_Track");
-	case VK_MEDIA_PREV_TRACK:
-		return _T("Media_Prev_Track");
-	case VK_MEDIA_STOP:
-		return _T("Media_Stop");
-	case VK_MEDIA_PLAY_PAUSE:
-		return _T("Media_Play/Pause");
-	case VK_LAUNCH_MAIL:
-		return _T("Mail");
-	case VK_LAUNCH_MEDIA_SELECT:
-		return _T("Media_Select");
-	case VK_LAUNCH_APP1:
-		return _T("App1");
-	case VK_LAUNCH_APP2:
-		return _T("App2");
-
 	}
 
 	GetKeyNameText(code, buf, 256);
