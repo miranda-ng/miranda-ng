76771752262e40fa41c189bbac761de9fb5eb1e0
Kirill Volinsky <mataes2007@gmail.com>
2015-04-06 13:35:50 +0000

    preparing set avatar
    git-svn-id: http://svn.miranda-ng.org/main/trunk@12629 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/SkypeWeb/src/requests/avatars.h
+++ protocols/SkypeWeb/src/requests/avatars.h
@@ -10,4 +10,19 @@ public:
 	}
 };
 
+class SetAvatarRequest : public HttpRequest
+{
+public:
+	SetAvatarRequest(const char *token, const char *skypename, const char *data) :
+		HttpRequest(REQUEST_PUT, FORMAT, "api.skype.com/users/%s/profile/avatar ", skypename)
+	{
+		Headers
+			<< CHAR_VALUE("X-Skypetoken", token)
+			<< CHAR_VALUE("Content-Type", "application/json; charset=UTF-8");
+
+		Body << VALUE(data);
+	}
+};
+
+
 #endif //_SKYPE_REQUEST_AVATAR_H_
