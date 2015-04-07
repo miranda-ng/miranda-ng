7f1871aed138dfe22ac879749bb40220da3f8da4
George Hazan <george.hazan@gmail.com>
2015-04-07 21:26:51 +0000

    libpng updated to 1.6.17 release
    git-svn-id: http://svn.miranda-ng.org/main/trunk@12663 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/AdvaImg/src/version.h
+++ plugins/AdvaImg/src/version.h
@@ -1,14 +1,14 @@
-#define __MAJOR_VERSION            0
-#define __MINOR_VERSION            11
-#define __RELEASE_NUM            0
-#define __BUILD_NUM               1
+#define __MAJOR_VERSION        0
+#define __MINOR_VERSION       11
+#define __RELEASE_NUM          0
+#define __BUILD_NUM            2
 
 #include <stdver.h>
 
-#define __PLUGIN_NAME            "Miranda image services"
-#define __FILENAME               "AdvaImg.dll"
-#define __DESCRIPTION             "Generic image services for Miranda NG."
+#define __PLUGIN_NAME          "Miranda image services"
+#define __FILENAME             "AdvaImg.dll"
+#define __DESCRIPTION          "Generic image services for Miranda NG."
 #define __AUTHOR               "Nightwish, The FreeImage project (http://freeimage.sourceforge.net/)"
-#define __AUTHOREMAIL            ""
-#define __AUTHORWEB               "http://miranda-ng.org/p/AdvaImg/"
-#define __COPYRIGHT               "© 2000-2012 Miranda-IM project, 2012-15 Miranda NG project uses the FreeImage distribution"
+#define __AUTHOREMAIL          ""
+#define __AUTHORWEB            "http://miranda-ng.org/p/AdvaImg/"
+#define __COPYRIGHT            "© 2000-2012 Miranda-IM project, 2012-15 Miranda NG project uses the FreeImage distribution"
