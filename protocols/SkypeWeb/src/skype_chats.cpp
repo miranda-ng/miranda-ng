ac707e772777df3d0533b80fc222efcb22861664
MikalaiR <nikolay.romanovich@narod.ru>
2015-04-07 14:15:39 +0000

    SkypeWeb: Chats support part 1.2.
    git-svn-id: http://svn.miranda-ng.org/main/trunk@12655 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/SkypeWeb/src/skype_chats.cpp
+++ protocols/SkypeWeb/src/skype_chats.cpp
@@ -1,8 +1,20 @@
 #include "common.h"
 
+MCONTACT CSkypeProto::GetChat(const char *skypename)
+{
+	MCONTACT hContact = NULL;
+	for (hContact = db_find_first(m_szModuleName); hContact; hContact = db_find_next(hContact, m_szModuleName))
+	{
+		ptrA cSkypename(getStringA(hContact, "ChatID"));
+		if (mir_strcmpi(skypename, cSkypename) == 0)
+			break;
+	}
+	return hContact;
+}
+
 MCONTACT CSkypeProto::AddChatRoom(const char *chatname)
 {
-	MCONTACT hContact = GetContact(chatname);
+	MCONTACT hContact = GetChat(chatname);
 	if (!hContact)
 	{
 		hContact = (MCONTACT)CallService(MS_DB_CONTACT_ADD, 0, 0);
