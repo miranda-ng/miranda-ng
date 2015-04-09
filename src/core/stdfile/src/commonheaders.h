7c1f6c82a6bf7ea49c5b9566f0b3e62efac24602
George Hazan <george.hazan@gmail.com>
2015-04-09 18:10:23 +0000

    potentially unsafe string operation removed
    git-svn-id: http://svn.miranda-ng.org/main/trunk@12714 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- src/core/stdfile/src/commonheaders.h
+++ src/core/stdfile/src/commonheaders.h
@@ -76,6 +76,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <m_modernopt.h>
 #include <m_timezones.h>
 #include <m_metacontacts.h>
+#include <m_string.h>
 
 #include "version.h"
 
