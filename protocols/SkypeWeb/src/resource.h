fb9f07e3c9b82a08e774d0997ecbe856ce77605d
MikalaiR <nikolay.romanovich@narod.ru>
2015-04-14 13:49:26 +0000

    SkypeWeb: Cosmetics.
    git-svn-id: http://svn.miranda-ng.org/main/trunk@12808 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/SkypeWeb/src/resource.h
+++ protocols/SkypeWeb/src/resource.h
@@ -12,6 +12,11 @@
 #define IDC_SAVEPERMANENTLY             108
 #define IDC_AUTOSYNC                    1028
 #define IDC_LOCALTIME                   1029
+#define IDC_RADIO1                      1030
+#define IDC_RADIO2                      1031
+#define IDC_RADIO3                      1032
+#define IDC_RADIO4                      1033
+#define IDC_STATIC_MARC_AS_READ         1034
 
 // Next default values for new objects
 // 
@@ -19,7 +24,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        109
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1030
+#define _APS_NEXT_CONTROL_VALUE         1035
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
