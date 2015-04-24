b53f73d5762f09620ad95bfd81c74660c2077dad
Vadim Dashevskiy <watcherhd@gmail.com>
2015-04-24 19:36:19 +0000

    Alarms - common project
    git-svn-id: http://svn.miranda-ng.org/main/trunk@13103 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Alarms/src/alarmlist.cpp
+++ plugins/Alarms/src/alarmlist.cpp
@@ -1,4 +1,4 @@
-#include "common.h"
+#include "stdafx.h"
 #include "alarmlist.h"
 
 AlarmList alarms;
--- plugins/Alarms/src/stdafx.h
+++ plugins/Alarms/src/stdafx.h
@@ -7,7 +7,6 @@
 
 #define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
 #define VC_EXTRALEAN
-#define _CRT_SECURE_NO_DEPRECATE
 
 #include <windows.h>
 #include <shellapi.h>
