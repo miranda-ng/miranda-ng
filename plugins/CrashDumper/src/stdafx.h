e3aacbdefb8bca970e60c2993b87869d4d7cf828
Vadim Dashevskiy <watcherhd@gmail.com>
2015-04-25 23:36:15 +0000

    CrashDumper - common project
    git-svn-id: http://svn.miranda-ng.org/main/trunk@13156 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/CrashDumper/src/stdafx.h
+++ plugins/CrashDumper/src/stdafx.h
@@ -16,8 +16,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <windows.h>
 #include <richedit.h>
 
