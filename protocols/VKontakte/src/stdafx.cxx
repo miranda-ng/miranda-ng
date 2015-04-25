a4c76afff5195837bb36290ad70d54fcfb161b03
Sergey Bolhovskoy <elzorfox@ya.ru>
2015-04-25 08:55:05 +0000

    VKontakte - common project
    git-svn-id: http://svn.miranda-ng.org/main/trunk@13126 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/VKontakte/src/stdafx.cxx
+++ protocols/VKontakte/src/stdafx.cxx
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2013-15 Miranda NG project (http://miranda-ng.org)
+Copyright (C) 2012-15 Miranda NG project (http://miranda-ng.org)
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
