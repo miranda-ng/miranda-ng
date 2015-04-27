00a53500d3321bba28a1f1a670957fc8e6de4ea4
George Hazan <george.hazan@gmail.com>
2015-04-27 19:05:09 +0000

    unified project for GTalkExt protocol
    git-svn-id: http://svn.miranda-ng.org/main/trunk@13203 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/GTalkExt/src/stdafx.h
+++ protocols/GTalkExt/src/stdafx.h
@@ -22,7 +22,6 @@
 #pragma once
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-#define _CRT_SECURE_NO_WARNINGS
 
 #include <assert.h>
 #include <malloc.h>
