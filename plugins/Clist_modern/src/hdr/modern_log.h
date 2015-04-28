d458827cc76d2c817456fa19ece19e4b584f7c75
Kirill Volinsky <mataes2007@gmail.com>
2015-04-28 05:49:24 +0000

    removed not used files
    git-svn-id: http://svn.miranda-ng.org/main/trunk@13217 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Clist_modern/src/hdr/modern_log.h
+++ plugins/Clist_modern/src/hdr/modern_log.h
@@ -6,21 +6,13 @@
 #ifdef _DEBUG
 
 void Log(const char *file,int line,const char *fmt,...);
-#define logg()  Log(__FILE__,__LINE__,"")
 #define log0(s)  Log(__FILE__,__LINE__,s)
 #define log1(s,a)  Log(__FILE__,__LINE__,s,a)
-#define log2(s,a,b)  Log(__FILE__,__LINE__,s,a,b)
-#define log3(s,a,b,c)  Log(__FILE__,__LINE__,s,a,b,c)
-#define log4(s,a,b,c,d)  Log(__FILE__,__LINE__,s,a,b,c,d)
 
 #else
 
-#define logg()
 #define log0(s)
 #define log1(s,a)
-#define log2(s,a,b)
-#define log3(s,a,b,c)
-#define log4(s,a,b,c,d)
 
 #endif
 
