c3f7c1299bf80c55ab97e3f0120ec99ce7d40768
Kirill Volinsky <mataes2007@gmail.com>
2015-04-30 20:10:57 +0000

    fix [13311]
    git-svn-id: http://svn.miranda-ng.org/main/trunk@13318 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Clist_modern/src/modern_skinned_profile.cpp
+++ plugins/Clist_modern/src/modern_skinned_profile.cpp
@@ -13,7 +13,7 @@ CSkinnedProfile CSkinnedProfile::_me;
 
 CSkinnedProfile::CSkinnedProfile()
 {
-	Init();	
+	Init();
 }
 
 CSkinnedProfile::~CSkinnedProfile()
@@ -24,90 +24,92 @@ CSkinnedProfile::~CSkinnedProfile()
 HRESULT CSkinnedProfile::Init()
 {
 	mir_cslock lck(_Lock);
+	SkinnedProfile.clear();
 	return S_OK;
 }
 
 HRESULT CSkinnedProfile::Clear()
 {
 	mir_cslock lck(_Lock);
+	SkinnedProfile.clear();
 	return S_OK;
 }
 
 
-ValueVariant* CSkinnedProfile::_GetValue( const char * szSection, const char * szKey )
+ValueVariant* CSkinnedProfile::_GetValue(const char * szSection, const char * szKey)
 {
-	KeyList_t::iterator k_it = SkinnedProfile.find( szSection );
-	if ( k_it == SkinnedProfile.end() ) 
+	KeyList_t::iterator k_it = SkinnedProfile.find(szSection);
+	if (k_it == SkinnedProfile.end())
 		return NULL; // Not skinned
 
-	ValueList_t::iterator v_it = k_it->second.find( szKey );
-	if ( v_it == k_it->second.end() )
+	ValueList_t::iterator v_it = k_it->second.find(szKey);
+	if (v_it == k_it->second.end())
 		return NULL; // Not skinned
 
-	return &( v_it->second );
+	return &(v_it->second);
 };
 
-BYTE CSkinnedProfile::SpiGetSkinByte( HANDLE hContact, const char * szSection, const char * szKey, const BYTE defValue )
+BYTE CSkinnedProfile::SpiGetSkinByte(HANDLE hContact, const char * szSection, const char * szKey, const BYTE defValue)
 {
-	if ( hContact ) 
-		return db_get_b( hContact, szSection, szKey, defValue ); //per-contact settings are not skinnablr at all
+	if (hContact)
+		return db_get_b(hContact, szSection, szKey, defValue); //per-contact settings are not skinnablr at all
 
 	mir_cslock lck(_Lock);
 
-	ValueVariant* value = SkinProfile()->_GetValue( szSection, szKey );
+	ValueVariant* value = SkinProfile()->_GetValue(szSection, szKey);
 
-	if ( value == NULL ) // not skinned => return DB
-		return db_get_w( hContact, szSection, szKey, defValue );
+	if (value == NULL) // not skinned => return DB
+		return db_get_w(hContact, szSection, szKey, defValue);
 
-	else if ( !value->IsEmpty() ) 
+	else if (!value->IsEmpty())
 		return value->GetByte();
 
 	return defValue; // skinned but empty => return default value
 }
 
-WORD CSkinnedProfile::SpiGetSkinWord( HANDLE hContact, const char * szSection, const char * szKey, const WORD defValue )
+WORD CSkinnedProfile::SpiGetSkinWord(HANDLE hContact, const char * szSection, const char * szKey, const WORD defValue)
 {
-	if ( hContact ) 
-		return db_get_w( hContact, szSection, szKey, defValue ); //per-contact settings are not skinnablr at all
+	if (hContact)
+		return db_get_w(hContact, szSection, szKey, defValue); //per-contact settings are not skinnablr at all
 
 	mir_cslock lck(_Lock);
 
-	ValueVariant* value = SkinProfile()->_GetValue( szSection, szKey );
-	
-	if ( value == NULL ) // not skinned => return DB
-		return db_get_w( hContact, szSection, szKey, defValue );
-	
-	else if ( !value->IsEmpty() ) 
+	ValueVariant* value = SkinProfile()->_GetValue(szSection, szKey);
+
+	if (value == NULL) // not skinned => return DB
+		return db_get_w(hContact, szSection, szKey, defValue);
+
+	else if (!value->IsEmpty())
 		return value->GetWord();
 
 	return defValue; // skinned but empty => return default value
 }
 
-DWORD CSkinnedProfile::SpiGetSkinDword( HANDLE hContact, const char * szSection, const char * szKey, const DWORD defValue )
+DWORD CSkinnedProfile::SpiGetSkinDword(HANDLE hContact, const char * szSection, const char * szKey, const DWORD defValue)
 {
-	if ( hContact ) 
-		return db_get_dw( hContact, szSection, szKey, defValue ); //per-contact settings are not skinnablr at all
+	if (hContact)
+		return db_get_dw(hContact, szSection, szKey, defValue); //per-contact settings are not skinnablr at all
 
 	mir_cslock lck(_Lock);
 
-	ValueVariant* value = SkinProfile()->_GetValue( szSection, szKey );
+	ValueVariant* value = SkinProfile()->_GetValue(szSection, szKey);
 
-	if ( value == NULL ) // not skinned => return DB
-		return db_get_dw( hContact, szSection, szKey, defValue );
-	
-	else if ( !value->IsEmpty() ) 
-		return value->GetDword();	
-	
+	if (value == NULL) // not skinned => return DB
+		return db_get_dw(hContact, szSection, szKey, defValue);
+
+	else if (!value->IsEmpty())
+		return value->GetDword();
+
 	return defValue; // skinned but empty => return default value
 }
 
-BOOL CSkinnedProfile::SpiCheckSkinned( HANDLE hContact, const char * szSection, const char * szKey )
+BOOL CSkinnedProfile::SpiCheckSkinned(HANDLE hContact, const char * szSection, const char * szKey)
 {
-	if ( hContact ) 
+	if (hContact)
 		return FALSE;
 
 	mir_cslock lck(_Lock);
 
-	return ( SkinProfile()->_GetValue( szSection, szKey ) == NULL );
+	return (SkinProfile()->_GetValue(szSection, szKey) == NULL);
 }
 #endif // _DEBUG // REMOVE ME, as soon as ghazan will implement msvcprt.lib msvcrt.lib analogs to vc6tovc7.lib
