6f8c7b4f1bf939c43e2fe124a01e9bec2b672616
Kirill Volinsky <mataes2007@gmail.com>
2015-05-11 15:10:35 +0000

    cleanup
    git-svn-id: http://svn.miranda-ng.org/main/trunk@13542 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Watrack_MPD/src/utilities.h
+++ plugins/Watrack_MPD/src/utilities.h
@@ -1,7 +1,6 @@
 #ifndef UTILITIES_H
 #define UTILITIES_H
 
-HANDLE CreateThreadEx(pThreadFuncEx AFunc, void* arg, DWORD* pThreadID);
 TCHAR* __stdcall UniGetContactSettingUtf(MCONTACT hContact, const char *szModule,const char* szSetting, TCHAR* szDef);
 
 #endif
