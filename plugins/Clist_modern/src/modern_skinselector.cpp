242a58d5adc1e038a6ce0e31ccda0061a1f7821e
Kirill Volinsky <mataes2007@gmail.com>
2015-05-16 15:37:16 +0000

    aplrtly revert [13616]
    git-svn-id: http://svn.miranda-ng.org/main/trunk@13619 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Clist_modern/src/modern_skinselector.cpp
+++ plugins/Clist_modern/src/modern_skinselector.cpp
@@ -95,7 +95,7 @@ DWORD mod_CalcHash(const char *szStr)
 			mov  esi, szStr
 			mov  al, [esi]
 			xor  cl, cl
-		lph_top :	 //only 4 of 9 instructions in here don't use AL, so optimal pipe use is impossible
+lph_top :	 //only 4 of 9 instructions in here don't use AL, so optimal pipe use is impossible
 		xor  edx, eax
 			inc  esi
 			xor  eax, eax
@@ -195,7 +195,8 @@ int SortMaskList(LISTMODERNMASK * mmList)
 				pos = 1;
 		}
 		else pos++;
-	} while (pos < mmList->dwMaskCnt);
+	}
+		while (pos < mmList->dwMaskCnt);
 
 	return 1;
 }
@@ -326,7 +327,7 @@ int ParseToModernMask(MODERNMASK *mm, char *szText)
 		}
 		else //ParamName = 'Module'
 		{
-			param.szName = mir_strdup("Module");
+			param.szName = _strdup("Module");
 			param.dwId = mod_CalcHash(param.szName);
 		}
 
@@ -413,7 +414,7 @@ int AddStrModernMaskToList(DWORD maskID, char *szStr, char *objectName, LISTMODE
 
 SKINOBJECTDESCRIPTOR *skin_FindObjectByMask(MODERNMASK *mm, LISTMODERNMASK *mmTemplateList)
 {
-	for (DWORD i = 0; i < mmTemplateList->dwMaskCnt; i++)
+	for (DWORD i = 0;i < mmTemplateList->dwMaskCnt;i++)
 		if (CompareModernMask(mm, &(mmTemplateList->pl_Masks[i])))
 			return (SKINOBJECTDESCRIPTOR*)mmTemplateList->pl_Masks[i].pObject;
 
@@ -464,7 +465,7 @@ WCHAR* GetParamN(WCHAR *string, WCHAR *buf, int buflen, BYTE paramN, WCHAR Delim
 				i--;
 		}
 		len = ((int)(i - start) < buflen) ? i - start : buflen;
-		mir_tstrncpy(buf, string + start, len);
+		_tcsncpy(buf, string + start, len);
 		buf[len] = '\0';
 	}
 	else buf[0] = '\0';
@@ -490,7 +491,7 @@ char * GetParamN(char * string, char * buf, int buflen, BYTE paramN, char Delim,
 				i--;
 		}
 		len = ((int)(i - start) < buflen) ? i - start : buflen;
-		mir_strncpy(buf, string + start, len);
+		strncpy(buf, string + start, len);
 		buf[len] = '\0';
 	}
 	else buf[0] = '\0';
@@ -641,6 +642,6 @@ int SkinDrawGlyphMask(HDC hdc, RECT *rcSize, RECT *rcClip, MODERNMASK *ModernMas
 	rq.hDC = hdc;
 	rq.rcDestRect = *rcSize;
 	rq.rcClipRect = *rcClip;
-	mir_strncpy(rq.szObjectID, "Masked draw", SIZEOF(rq.szObjectID));
+	strncpy(rq.szObjectID, "Masked draw", SIZEOF(rq.szObjectID)-1);
 	return ske_Service_DrawGlyph((WPARAM)&rq, (LPARAM)ModernMask);
 }
