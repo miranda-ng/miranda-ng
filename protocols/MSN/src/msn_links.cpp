d3087e6da133a1c0b7e6e63f4e6f54c027b9f58c
George Hazan <george.hazan@gmail.com>
2015-06-04 13:10:57 +0000

    unified project for MSN
    git-svn-id: http://svn.miranda-ng.org/main/trunk@13997 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/MSN/src/msn_links.cpp
+++ protocols/MSN/src/msn_links.cpp
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "msn_global.h"
+#include "stdafx.h"
 #include "msn_proto.h"
 
 #include <m_addcontact.h>
--- protocols/MSN/src/stdafx.cxx
+++ protocols/MSN/src/stdafx.cxx
@@ -15,4 +15,4 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "msn_global.h"
+#include "stdafx.h"
