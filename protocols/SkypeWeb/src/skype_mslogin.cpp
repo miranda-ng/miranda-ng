5818b938088b78cd44b5bc76acadb64de9fea8b1
MikalaiR <nikolay.romanovich@narod.ru>
2015-06-07 10:27:56 +0000

    ooops
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14041 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

---
+++ protocols/SkypeWeb/src/skype_mslogin.cpp
@@ -0,0 +1,159 @@
+/*
+Copyright (c) 2015 Miranda NG project (http://miranda-ng.org)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "stdafx.h"
+
+void CSkypeProto::OnMSLoginFirst(const NETLIBHTTPREQUEST *response)
+{
+	if (response == NULL || response->pData == NULL)
+	{
+		ProtoBroadcastAck(NULL, ACKTYPE_LOGIN, ACKRESULT_FAILED, NULL, LOGIN_ERROR_UNKNOWN);
+		SetStatus(ID_STATUS_OFFLINE);
+		return;
+	}
+
+	std::regex regex;
+	std::smatch match;
+	std::map<std::string, std::string> scookies;
+	std::string content = response->pData;
+
+	regex = "<input type=\"hidden\" name=\"PPFT\" id=\"i0327\" value=\"(.+?)\"/>";
+
+	if (!std::regex_search(content, match, regex))
+	{
+		ProtoBroadcastAck(NULL, ACKTYPE_LOGIN, ACKRESULT_FAILED, NULL, LOGIN_ERROR_UNKNOWN);
+		SetStatus(ID_STATUS_OFFLINE);
+		return;
+	}
+	std::string PPTF = match[1];
+	
+	for (int i = 0; i < response->headersCount; i++)
+	{
+		if (mir_strcmpi(response->headers[i].szName, "Set-Cookie"))
+			continue;
+
+		regex = "^(.+?)=(.+?);";
+		content = response->headers[i].szValue;
+		if (std::regex_search(content, match, regex))
+			scookies[match[1]] = match[2];
+	}
+
+	CMStringA mscookies(FORMAT, "MSPRequ=%s;MSPOK=%s;CkTst=G%lld;", scookies["MSPRequ"].c_str(), scookies["MSPOK"].c_str(), time(NULL));
+
+	SendRequest (new LoginMSRequest(ptrA(getStringA(SKYPE_SETTINGS_ID)), ptrA(getStringA(SKYPE_SETTINGS_PASSWORD)), mscookies.c_str(), PPTF.c_str()), &CSkypeProto::OnMSLoginSecond);
+}
+
+void CSkypeProto::OnMSLoginSecond(const NETLIBHTTPREQUEST *response)
+{
+	if (response == NULL || response->pData == NULL)
+	{
+		ProtoBroadcastAck(NULL, ACKTYPE_LOGIN, ACKRESULT_FAILED, NULL, LOGIN_ERROR_UNKNOWN);
+		SetStatus(ID_STATUS_OFFLINE);
+		return;
+	}
+
+	std::regex regex;
+	std::smatch match;
+	std::string content = response->pData;
+	ptrA szContent(response->pData);
+
+	regex = "<meta name=\"PageID\" content=\"(.+?)\"/>";
+	if (std::regex_search(content, match, regex))
+	{
+		if (match[1] == "i5600")
+		{
+			CMStringA cookies;
+			for (int i = 0; i < response->headersCount; i++)
+			{
+				if (mir_strcmpi(response->headers[i].szName, "Set-Cookie"))
+					continue;
+
+				regex = "^(.+?)=(.+?);";
+				content = response->headers[i].szValue;
+				if (std::regex_search(content, match, regex))
+					if (!std::string(match[2]).empty() && std::string(match[2]) != " ")
+						cookies.AppendFormat("%s=%s;", std::string(match[1]).c_str(), std::string(match[2]).c_str());
+			}
+
+			CMStringA url(GetStringChunk(szContent, "urlPost:'", "'"));
+			CMStringA ppft(GetStringChunk(szContent, "sFT:'", "'"));
+			
+
+			ptrA code(mir_utf8encodeT(RunConfirmationCode()));
+
+			SendRequest(new LoginMSRequest(url.c_str(), ptrA(getStringA(SKYPE_SETTINGS_ID)), cookies.c_str(), ppft.c_str(), code), &CSkypeProto::OnMSLoginEnd);
+			return;
+		}
+	}
+
+
+	regex = "=\"t\" value=\"(.+?)\"/>";
+
+	if (!std::regex_search(content, match, regex))
+	{
+		ProtoBroadcastAck(NULL, ACKTYPE_LOGIN, ACKRESULT_FAILED, NULL, LOGIN_ERROR_UNKNOWN);
+		SetStatus(ID_STATUS_OFFLINE);
+		return;
+	}
+	std::string t = match[1];
+
+	SendRequest(new LoginMSRequest(t.c_str()), &CSkypeProto::OnMSLoginEnd);
+}
+
+void CSkypeProto::OnMSLoginEnd(const NETLIBHTTPREQUEST *response)
+{
+	if (response == NULL || response->pData == NULL)
+	{
+		ProtoBroadcastAck(NULL, ACKTYPE_LOGIN, ACKRESULT_FAILED, NULL, LOGIN_ERROR_UNKNOWN);
+		SetStatus(ID_STATUS_OFFLINE);
+		return;
+	}
+
+	std::regex regex;
+	std::smatch match;
+	std::string content = response->pData;
+
+	regex = "<input type=\"hidden\" name=\"skypetoken\" value=\"(.+?)\"/>"; 
+	if (!std::regex_search(content, match, regex))
+	{
+		ProtoBroadcastAck(NULL, ACKTYPE_LOGIN, ACKRESULT_FAILED, NULL, LOGIN_ERROR_UNKNOWN);
+		SetStatus(ID_STATUS_OFFLINE);
+		return;
+	}
+	std::string token = match[1];
+	setString("TokenSecret", token.c_str()); 
+	regex = "<input type=\"hidden\" name=\"expires_in\" value=\"(.+?)\"/>"; 
+
+	if (std::regex_search(content, match, regex))
+	{
+		std::string expiresIn = match[1];
+		int seconds = atoi(expiresIn.c_str());
+		setDword("TokenExpiresIn", time(NULL) + seconds);
+	}
+
+	OnLoginSuccess();
+}
+
+CMString CSkypeProto::RunConfirmationCode()
+{
+	ENTER_STRING pForm = { sizeof(pForm) };
+	pForm.type = ESF_PASSWORD;
+	pForm.caption = TranslateT("Enter confirmation code");
+	pForm.ptszInitVal = NULL;
+	pForm.szModuleName = m_szModuleName;
+	return (!EnterString(&pForm)) ? CMString() : CMString(ptrT(pForm.ptszResult));
+}
