f2bc6761680a9f7fca23eb68331570b734aa682f
MikalaiR <nikolay.romanovich@narod.ru>
2015-06-09 18:05:49 +0000

    MessageState: Cleanup.
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14083 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/MessageState/src/messagestate.cpp
+++ plugins/MessageState/src/messagestate.cpp
@@ -1,7 +1,5 @@
 #include "stdafx.h"
 
-HANDLE hDummyService;
-
 LONGLONG GetLastSentMessageTime(MCONTACT hContact)
 {
 	for (MEVENT hDbEvent = db_event_last(hContact); hDbEvent; hDbEvent = db_event_prev(hContact, hDbEvent))
@@ -144,20 +142,12 @@ int OnSrmmWindowEvent(WPARAM, LPARAM lParam)
 
 INT_PTR DummyService(WPARAM, LPARAM){ return 0; }
 
-void UnInitModule()
-{
-	if (hDummyService)
-		DestroyServiceFunction(hDummyService);
-}
-
 int OnModulesLoaded(WPARAM, LPARAM)
 {
 	HookEvent(ME_MSG_WINDOWEVENT, OnSrmmWindowEvent);
 	HookEvent(ME_DB_CONTACT_SETTINGCHANGED, OnContactSettingChanged);
 	HookEvent(ME_PROTO_ACK, OnProtoAck);
 	HookEvent(ME_DB_EVENT_FILTER_ADD, OnEventFilterAdd);
-
-	hDummyService = CreateServiceFunction(MODULENAME "/DummyService", DummyService);
 
 	// IcoLib support
 	for (size_t i = 0; i < SIZEOF(Icons); i++)
