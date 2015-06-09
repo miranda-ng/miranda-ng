83a128bfad194b2510ffdd4d7291762b0f4c73db
MikalaiR <nikolay.romanovich@narod.ru>
2015-06-09 11:38:48 +0000

    MessageState: Initial commit.
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14072 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

---
+++ plugins/MessageState/src/resource.h
@@ -0,0 +1,19 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ..\res\mobilestate.rc
+//
+#define IDI_READ                      105
+#define IDI_UNREAD                    106
+#define IDI_FAIL                      107
+#define IDI_NOSENT                    108
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        109
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
---
+++ plugins/MessageState/src/stdafx.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <windows.h>
+#include <time.h>
+#include <locale.h>
+
+#include <newpluginapi.h>
+#include <m_database.h>
+#include <m_protocols.h>
+#include <m_protosvc.h>
+#include <m_langpack.h>
+#include <m_icolib.h>
+#include <m_extraicons.h>
+#include <m_message.h>
+#include <m_timezones.h>
+#include "resource.h"
+#include "Version.h"
+
+#include "global.h"
+
+#define MODULENAME "MessageState"
+
+extern HINSTANCE g_hInst;
