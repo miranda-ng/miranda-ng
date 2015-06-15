86e683b70b975f18609c23e95a456136be6c276b
George Hazan <george.hazan@gmail.com>
2015-06-15 14:35:05 +0000

    separate scripts for copying libraries
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14178 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

---
+++ copylibapp.cmd
@@ -0,0 +1,6 @@
+@echo off
+copy /Y .\Bin12\Debug\Obj\mir_app\mir_app.lib .\Bin10\lib\mir_app.lib
+copy /Y .\Bin12\Debug64\Obj\mir_app\mir_app.lib .\Bin10\lib\mir_app64.lib
+
+copy /Y .\Bin12\Debug\Obj\mir_app\mir_app.lib .\Bin12\lib\mir_app.lib
+copy /Y .\Bin12\Debug64\Obj\mir_app\mir_app.lib .\Bin12\lib\mir_app64.lib
