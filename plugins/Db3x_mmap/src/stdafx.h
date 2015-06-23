bf67ab399466c7d643472be07f2487e87442dc68
Kirill Volinsky <mataes2007@gmail.com>
2015-06-23 05:51:07 +0000

    Dbx_mmap: unified project
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14341 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Db3x_mmap/src/stdafx.h
+++ plugins/Db3x_mmap/src/stdafx.h
@@ -21,8 +21,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-#define _CRT_SECURE_NO_WARNINGS
-
 #pragma warning(disable:4509)
 
 #include <windows.h>
