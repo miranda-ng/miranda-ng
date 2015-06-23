36d6b1b19aff8f1ab07a1931fe51813d589cd5ef
Kirill Volinsky <mataes2007@gmail.com>
2015-06-23 05:36:23 +0000

    Fingerprint: unified project
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14340 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/FingerprintNG/src/main.cpp
+++ plugins/FingerprintNG/src/main.cpp
@@ -18,7 +18,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
 //Start of header
-#include "global.h"
+#include "stdafx.h"
 
 #define LIB_REG		2
 #define LIB_USE		3
