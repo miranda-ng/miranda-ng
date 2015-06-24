130475ec7ace51c31551ce737ce17e3e9909f9f8
Alexander Lantsev <aunsane@gmail.com>
2015-06-24 18:16:34 +0000

    MirLua: version bump
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14366 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/MirLua/src/version.h
+++ plugins/MirLua/src/version.h
@@ -1,13 +1,13 @@
 #define __MAJOR_VERSION            0
 #define __MINOR_VERSION            11
 #define __RELEASE_NUM              0
-#define __BUILD_NUM                0
+#define __BUILD_NUM                1
 
 #include <stdver.h>
 
 #define __PLUGIN_NAME              "MirLua"
 #define __FILENAME                 "MirLua.dll"
-#define __DESCRIPTION              "Allow you to extend Miranda NG functionality with lua scripts."
+#define __DESCRIPTION              "Extends Miranda NG functionality with lua scripts."
 #define __AUTHOR                   "Miranda NG team"
 #define __AUTHOREMAIL              ""
 #define __AUTHORWEB                "http://miranda-ng.org/p/MirLua/"
