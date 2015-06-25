503f26d1660a9b06f7eb1dcdeadc7038b27b1490
MikalaiR <nikolay.romanovich@narod.ru>
2015-06-25 12:40:46 +0000

    SkypeWeb: unused include removed
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14378 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/SkypeWeb/src/stdafx.h
+++ protocols/SkypeWeb/src/stdafx.h
@@ -26,7 +26,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 #include <vector>
 #include <regex>
 #include <map>
-#include <atlenc.h>
+
 #include <newpluginapi.h>
 
 #include <m_protoint.h>
