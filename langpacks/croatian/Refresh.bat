25d6e2b681cd7e16408a24280b83875334f2b498
zvonimirek <zvonimirek@miranda-ng.org>
2015-06-28 15:06:48 +0000

    lang croatiannew translation. more to come soon
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14427 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

---
+++ langpacks/croatian/Refresh.bat
@@ -0,0 +1,15 @@
+@echo off
+
+REM ====== VARIABLES ========
+
+set "language=croatian"
+set "oldLangpackPath="
+set "mirandaPath=C:\Program files\Miranda NG"
+
+REM =========================
+
+cd "..\..\tools\lpgen\"
+call refresher.bat %language% "%oldLangpackPath%"
+
+REM copy and reload langpack in your Miranda (uncoment next line + enable cmdline.dll plugin to use it)
+REM call installer.bat %language% "%mirandaPath%"
