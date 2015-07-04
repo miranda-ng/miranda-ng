7a82eba37f1dd4567b16292bec78e6b07dd94fe4
Alexander Lantsev <aunsane@gmail.com>
2015-07-04 17:37:55 +0000

    MirLua: added module m_windows
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14489 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/MirLua/src/stdafx.h
+++ plugins/MirLua/src/stdafx.h
@@ -88,6 +88,9 @@ LUAMOD_API int (luaopen_m_toptoolbar)(lua_State *L);
 #define MLUA_VARIABLES	"m_variables"
 LUAMOD_API int (luaopen_m_variables)(lua_State *L);
 
+#define MLUA_WINDOWS	"m_windows"
+LUAMOD_API int (luaopen_m_windows)(lua_State *L);
+
 int luaM_print(lua_State *L);
 int luaM_atpanic(lua_State *L);
 
