8a338144ce9bdc1e32edcdcf657c6e0e546b004a
Alexander Lantsev <aunsane@gmail.com>
2015-07-06 14:16:46 +0000

    MirLua: fixed m_core examples
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14499 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/MirLua/docs/examples/core.lua
+++ plugins/MirLua/docs/examples/core.lua
@@ -1,11 +1,22 @@
 -- core module (m) is included by default
 --- include m_clist module
 local clist = require('m_clist')
+--- include m_genmenu module
+local genmenu = require('m_genmenu')
 -- include m_icolib module
 local icolib = require('m_icolib')
 
 -- Add icon for menu items
 local hRestartIcon = icolib.AddIcon('restartIcon', 'Restart')
 
--- Add menu item to main menu that allow to restart Miranda NG
-clist.AddMainMenuItem("Restart", 0, 0, hRestartIcon, "Miranda/System/Restart")
+-- Subscribe to Lua script loaded event
+m.OnScriptLoaded(function()
+  -- Add menu item to main menu that allow to restart Miranda NG
+  hRestartMenuItem = clist.AddMainMenuItem({ Name = "Restart", Icon = hRestartIcon, Service = "Miranda/System/Restart" })
+end)
+
+-- Subscribe to Lua script unload event
+m.OnScriptUnload(function()
+  -- remove menu item from main menu
+  hRestartMenuItem = genmenu.RemoveMenuItem(hRestart)
+end)
