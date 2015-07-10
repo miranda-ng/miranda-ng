bebff12e3f28da68c2a8f771d869152c2b4d8e61
Alexander Lantsev <aunsane@gmail.com>
2015-07-10 18:20:29 +0000

    MirLua: added PopupData support in m_popup
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14526 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/MirLua/src/stdafx.h
+++ plugins/MirLua/src/stdafx.h
@@ -35,6 +35,8 @@ extern "C"
 #include "resource.h"
 
 #include "mlua.h"
+#include "mlua_module.h"
+#include "mlua_m_clist.h"
 #include "mlua_module_loader.h"
 #include "mlua_script_loader.h"
 #include "mlua_options.h"
