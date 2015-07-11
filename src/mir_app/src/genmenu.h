ceb6ba36ec14c80e89102cb17bd6b0fda0ac48aa
George Hazan <george.hazan@gmail.com>
2015-07-11 20:58:33 +0000

    unused code removed
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14538 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- src/mir_app/src/genmenu.h
+++ src/mir_app/src/genmenu.h
@@ -24,8 +24,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #ifndef GENMENU_H
 #define GENMENU_H
-//general menu object module
-#include "m_genmenu.h"
 
 /* genmenu structs */
 
