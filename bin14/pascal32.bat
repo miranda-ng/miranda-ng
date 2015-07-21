190ec3cc07eff6074223fd0d7c7c633023919cc0
Kirill Volinsky <mataes2007@gmail.com>
2015-07-21 06:44:33 +0000

    preparing for vc2015
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14593 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

---
+++ bin14/pascal32.bat
@@ -0,0 +1,39 @@
+@echo off
+
+pushd ..\plugins
+
+pushd Actman
+call make.bat fpc 14
+if errorlevel 1 goto :Error
+popd
+
+pushd ImportTXT
+call make.bat fpc 14
+if errorlevel 1 goto :Error
+popd
+
+pushd mRadio
+call make.bat fpc 14
+if errorlevel 1 goto :Error
+popd
+
+pushd QuickSearch
+call make.bat fpc 14
+if errorlevel 1 goto :Error
+popd
+
+pushd Watrack
+call make.bat fpc 14
+if errorlevel 1 goto :Error
+cd icons
+call makeicons.bat fpc 14
+if errorlevel 1 goto :Error
+popd
+
+popd
+goto :eof
+
+:Error
+echo ============================= FAIL! =============================
+pause
+exit
