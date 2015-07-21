f0a3ab0c2961e003fbda41df8b265eee8e824912
Kirill Volinsky <mataes2007@gmail.com>
2015-07-21 20:24:30 +0000

    TabSRMM: vc2015 compilation fix
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14614 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/TabSRMM/src/msgs.cpp
+++ plugins/TabSRMM/src/msgs.cpp
@@ -741,12 +741,12 @@ struct {
 	int nItems;
 }
 static ICONBLOCKS[] = {
-	{ LPGEN("Message Sessions")"/"LPGEN("Default"), _deficons, _countof(_deficons) },
-	{ LPGEN("Message Sessions")"/"LPGEN("Toolbar"), _toolbaricons, _countof(_toolbaricons) },
-	{ LPGEN("Message Sessions")"/"LPGEN("Toolbar"), _exttoolbaricons, _countof(_exttoolbaricons) },
-	{ LPGEN("Message Sessions")"/"LPGEN("Toolbar"), _chattoolbaricons, _countof(_chattoolbaricons) },
-	{ LPGEN("Message Sessions")"/"LPGEN("Message Log"), _logicons, _countof(_logicons) },
-	{ LPGEN("Message Sessions")"/"LPGEN("Animated Tray"), _trayIcon, _countof(_trayIcon) }
+	{ LPGEN("Message Sessions") "/" LPGEN("Default"), _deficons, _countof(_deficons) },
+	{ LPGEN("Message Sessions") "/" LPGEN("Toolbar"), _toolbaricons, _countof(_toolbaricons) },
+	{ LPGEN("Message Sessions") "/" LPGEN("Toolbar"), _exttoolbaricons, _countof(_exttoolbaricons) },
+	{ LPGEN("Message Sessions") "/" LPGEN("Toolbar"), _chattoolbaricons, _countof(_chattoolbaricons) },
+	{ LPGEN("Message Sessions") "/" LPGEN("Message Log"), _logicons, _countof(_logicons) },
+	{ LPGEN("Message Sessions") "/" LPGEN("Animated Tray"), _trayIcon, _countof(_trayIcon) }
 };
 
 static int GetIconPackVersion(HMODULE hDLL)
@@ -812,7 +812,7 @@ static int TSAPI SetupIconLibConfig()
 		}
 	}
 
-	sid.section.a = LPGEN("Message Sessions")"/"LPGEN("Default");
+	sid.section.a = LPGEN("Message Sessions") "/" LPGEN("Default");
 	sid.pszName = "tabSRMM_clock_symbol";
 	sid.description.a = LPGEN("Clock symbol (for the info panel clock)");
 	sid.iDefaultIndex = -IDI_CLOCK;
