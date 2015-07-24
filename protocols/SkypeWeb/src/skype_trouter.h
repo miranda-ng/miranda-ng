0d1ee3abd981d7b33e36020caf16852827bede8b
MikalaiR <nikolay.romanovich@narod.ru>
2015-07-24 16:35:24 +0000

    ooops
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14679 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

---
+++ protocols/SkypeWeb/src/skype_trouter.h
@@ -0,0 +1,17 @@
+#ifndef _SKYPE_TROUTER_H_
+#define _SKYPE_TROUTER_H_
+
+struct TRInfo
+{
+	std::string socketIo,
+		connId,
+		st,
+		se,
+		instance,
+		ccid,
+		sessId,
+		sig,
+		url;
+};
+
+#endif //_SKYPE_TROUTER_H_
