78b9cb478a8aec236007c330f2d3ebfbf0df25a4
Kirill Volinsky <mataes2007@gmail.com>
2015-07-25 19:42:32 +0000

    mydetails: common project
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14703 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/MyDetails/src/mydetails.cpp
+++ plugins/MyDetails/src/mydetails.cpp
@@ -17,7 +17,7 @@ not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.
 */
 
-#include "commons.h"
+#include "stdafx.h"
 
 CLIST_INTERFACE *pcli;
 HINSTANCE hInst;
