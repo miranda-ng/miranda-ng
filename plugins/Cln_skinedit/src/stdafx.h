fb8b0186a7bdc11ac111542b4b7a77b5054a7014
Kirill Volinsky <mataes2007@gmail.com>
2015-07-25 14:25:13 +0000

    Cln_skinedit: common project
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14687 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

---
+++ plugins/Cln_skinedit/src/stdafx.h
@@ -0,0 +1,36 @@
+/*
+
+Miranda NG: the free IM client for Microsoft* Windows*
+
+Copyright (ñ) 2012-15 Miranda NG project (http://miranda-ng.org),
+Copyright (c) 2000-04 Miranda ICQ/IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#include <windows.h>
+#include <commctrl.h>
+#include <time.h>
+
+#include <newpluginapi.h>
+#include <m_database.h>
+#include <m_langpack.h>
+#include <m_protosvc.h>
+#include <m_cln_skinedit.h>
+
+#include "Version.h"
+#include "skineditres.h"
