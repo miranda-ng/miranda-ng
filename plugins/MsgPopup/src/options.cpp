50eb44034f1372d682800d248e27fb2502b025af
Kirill Volinsky <mataes2007@gmail.com>
2015-07-25 19:18:08 +0000

    MsgPopup: common project
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14701 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/MsgPopup/src/options.cpp
+++ plugins/MsgPopup/src/options.cpp
@@ -19,7 +19,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
-#include "common.h"
+#include "stdafx.h"
 
 int idOptionControls[4][3] = {
 	IDC_ASTERISK_FG, IDC_ASTERISK_BG, IDC_TIMEOUT1,
