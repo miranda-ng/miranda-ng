4f7c351439f63ff61a5093e1c20bcb2e2bd781d3
Kirill Volinsky <mataes2007@gmail.com>
2015-07-26 07:40:03 +0000

    NewXstatusNotify: common project
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14709 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/NewXstatusNotify/src/popup.cpp
+++ plugins/NewXstatusNotify/src/popup.cpp
@@ -19,7 +19,7 @@
 	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 	*/
 
-#include "common.h"
+#include "stdafx.h"
 
 void ShowChangePopup(MCONTACT hContact, HICON hIcon, WORD newStatus, const TCHAR *stzText, PLUGINDATA *pdp)
 {
--- plugins/NewXstatusNotify/src/xstatus.cpp
+++ plugins/NewXstatusNotify/src/xstatus.cpp
@@ -17,7 +17,7 @@
 	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 	*/
 
-#include "common.h"
+#include "stdafx.h"
 
 XSTATUSCHANGE *NewXSC(MCONTACT hContact, char *szProto, int xstatusType, int action, TCHAR *stzTitle, TCHAR *stzText)
 {
