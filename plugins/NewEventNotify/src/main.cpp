9a086691c6229c4efd5e906a17d3a6d4cfd975f8
Kirill Volinsky <mataes2007@gmail.com>
2015-07-26 06:53:50 +0000

    neweventnotify: common project
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14706 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/NewEventNotify/src/main.cpp
+++ plugins/NewEventNotify/src/main.cpp
@@ -22,7 +22,7 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-#include "neweventnotify.h"
+#include "stdafx.h"
 
 int g_IsSrmmWindowAPI = 0;
 extern PLUGIN_DATA* PopupList[20];
