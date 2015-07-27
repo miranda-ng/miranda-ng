1ab85ee9ff6103c66ac9e7847aead3992ca66921
Alexander Lantsev <aunsane@gmail.com>
2015-07-27 21:50:40 +0000

    Tox: updated tox core
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14741 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/Tox/src/version.h
+++ protocols/Tox/src/version.h
@@ -1,7 +1,7 @@
 #define __MAJOR_VERSION            0
 #define __MINOR_VERSION            11
 #define __RELEASE_NUM              1
-#define __BUILD_NUM                11
+#define __BUILD_NUM                12
 
 #include <stdver.h>
 
@@ -11,4 +11,4 @@
 #define __AUTHOR                   "Miranda NG Team"
 #define __AUTHOREMAIL              ""
 #define __AUTHORWEB                "http://miranda-ng.org/p/Tox/"
-#define __COPYRIGHT                "© 2014-15 Miranda NG Team"
+#define __COPYRIGHT                "ï¿½ 2014-15 Miranda NG Team"
