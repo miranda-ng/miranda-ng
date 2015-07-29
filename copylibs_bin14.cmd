c8552d6fdcfc78c246f4e84a7ae56eca8b02b99b
George Hazan <george.hazan@gmail.com>
2015-07-29 20:49:44 +0000

    vs2015 not to be used to build common .def & .lib files
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14756 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- copylibs_bin14.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-copy /Y .\Bin14\Debug\Obj\mir_core\mir_core.lib .\Bin14\lib
-copy /Y .\Bin14\Debug64\Obj\mir_core\mir_core.lib .\Bin14\lib\mir_core64.lib
-
-copy /Y .\Bin14\Debug\Obj\mir_app\mir_app.lib .\Bin14\lib\mir_app.lib
-copy /Y .\Bin14\Debug64\Obj\mir_app\mir_app.lib .\Bin14\lib\mir_app64.lib
-
-copy /Y .\Bin14\Debug\Obj\libjson\libjson.lib .\Bin14\lib\libjson.lib
-copy /Y .\Bin14\Debug64\Obj\libjson\libjson.lib .\Bin14\lib\libjson64.lib
