2500676eceafa86e3697e95061b9a276b40eb522
Vadim Dashevskiy <watcherhd@gmail.com>
2015-07-30 20:48:48 +0000

    warning fixes
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14760 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/PluginUpdater/src/stdafx.h
+++ plugins/PluginUpdater/src/stdafx.h
@@ -17,8 +17,6 @@ not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.
 */
 
-#define _CRT_SECURE_NO_WARNINGS
-
 // Windows Header Files:
 #include <time.h>
 #include <stddef.h>
