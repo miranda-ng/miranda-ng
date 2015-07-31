cd45782fb74301b3806afa796bb6a6ac104817dd
MikalaiR <nikolay.romanovich@narod.ru>
2015-07-31 10:42:27 +0000

    SkypeWeb: small fixes
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14764 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/SkypeWeb/src/skype_db.h
+++ protocols/SkypeWeb/src/skype_db.h
@@ -1,3 +1,22 @@
+/*
+Copyright (c) 2015 Miranda NG project (http://miranda-ng.org)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _SKYPE_DB_H_
+#define _SKYPE_DB_H_
 
 enum SKYPE_DB_EVENT_TYPE
 {
@@ -13,3 +32,5 @@ enum SKYPE_DB_EVENT_TYPE
 #define SKYPE_SETTINGS_ID "Skypename"
 #define SKYPE_SETTINGS_PASSWORD "Password"
 #define SKYPE_SETTINGS_GROUP "DefaultGroup"
+
+#endif //_SKYPE_DB_H_
