78e485fb288d552f938997cbfc0f159e360bcbdb
Kirill Volinsky <mataes2007@gmail.com>
2015-08-01 20:34:40 +0000

    watrack_mpd: common project
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14797 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Watrack_MPD/src/options.cpp
+++ plugins/Watrack_MPD/src/options.cpp
@@ -14,7 +14,7 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-#include "commonheaders.h"
+#include "stdafx.h"
 
 static INT_PTR CALLBACK DlgProcWaMpdOpts(HWND hwndDlg, UINT msg, WPARAM wParam, LPARAM lParam)
 {
---
+++ plugins/Watrack_MPD/src/stdafx.h
@@ -0,0 +1,46 @@
+// Copyright © 2008 sss, chaos.persei
+// 
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#ifndef COMMONHEADERS_H
+#define COMMONHEADERS_H
+
+#include <windows.h>
+
+#include <newpluginapi.h>
+#include <m_database.h>
+#include <m_netlib.h>
+#include <m_utils.h>
+#include <m_options.h>
+#include <m_langpack.h>
+
+#include <m_music.h>
+
+#include "resource.h"
+#include "utilities.h"
+#include "Version.h"
+
+#define szModuleName "Watrack_MPD"
+
+extern HINSTANCE hInst;
+extern HANDLE ghNetlibUser;
+extern BOOL bWatrackService;
+extern TCHAR *gbHost, *gbPassword;
+extern WORD gbPort;
+extern char *date();
+
+extern int WaMpdOptInit(WPARAM, LPARAM);
+extern void RegisterPlayer();
+
+#endif
