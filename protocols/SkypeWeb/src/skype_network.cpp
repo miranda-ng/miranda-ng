4db92a34e0b0d9c090858c541d177d268d9f16e0
MikalaiR <nikolay.romanovich@narod.ru>
2015-08-01 18:52:57 +0000

    oops
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14784 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/SkypeWeb/src/skype_network.cpp
+++ protocols/SkypeWeb/src/skype_network.cpp
@@ -19,10 +19,12 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 void CSkypeProto::InitNetwork()
 {
-	NETLIBUSER nlu;
+	TCHAR name[128];
+	mir_sntprintf(name, _countof(name), TranslateT("%s connection"), m_tszUserName);
+	NETLIBUSER nlu = { 0 };
 	nlu.cbSize = sizeof(nlu);
 	nlu.flags = NUF_OUTGOING | NUF_INCOMING | NUF_HTTPCONNS | NUF_UNICODE;
-	nlu.ptszDescriptiveName = CMString(FORMAT, TranslateT("%s connection"), m_tszUserName).GetBuffer();
+	nlu.ptszDescriptiveName = name;
 	nlu.szSettingsModule = m_szModuleName;
 	m_hNetlibUser = (HANDLE)CallService(MS_NETLIB_REGISTERUSER, 0, (LPARAM)&nlu);
 }
