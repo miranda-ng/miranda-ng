ad5e273a04e89a45d41c87a6a21c6d6fd937ac43
Kirill Volinsky <mataes2007@gmail.com>
2015-08-01 20:28:18 +0000

    Variables: common project
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14796 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Variables/src/options.cpp
+++ plugins/Variables/src/options.cpp
@@ -17,7 +17,7 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-#include "variables.h"
+#include "stdafx.h"
 
 static INT_PTR CALLBACK SetOptsDlgProc(HWND hwndDlg,UINT msg,WPARAM wParam,LPARAM lParam)
 {
