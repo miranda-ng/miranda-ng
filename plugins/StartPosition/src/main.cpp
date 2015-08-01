6c18e42ff55c8bf4c4b11dbe2e970bc553d00467
Kirill Volinsky <mataes2007@gmail.com>
2015-08-01 16:37:27 +0000

    StartPosition: common project
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14780 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/StartPosition/src/main.cpp
+++ plugins/StartPosition/src/main.cpp
@@ -21,7 +21,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
-#include "Common.h"
+#include "stdafx.h"
 
 HINSTANCE hInst;
 int hLangpack;
