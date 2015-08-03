a4058bdd418f3a3168e26d1fc2a07ae2e9bab0c6
MikalaiR <nikolay.romanovich@narod.ru>
2015-08-03 12:52:26 +0000

    forgotten operator
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14831 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/SkypeWeb/src/skype_db.h
+++ protocols/SkypeWeb/src/skype_db.h
@@ -39,19 +39,20 @@ public:
 	__inline explicit pass_ptrA() : mir_ptr(){}
 	__inline explicit pass_ptrA(char* _p) : mir_ptr(_p) {}
 	__inline ~pass_ptrA() { zero(); }
+	__inline char* operator = (char *_p){ zero(); mir_ptr::operator=(_p); }
 	__inline void zero() 
-	{ char *data = mir_ptr::operator char *();
-	  if (data) SecureZeroMemory(data, mir_strlen(data)); 
+	{ char *_data = mir_ptr::operator char *();
+	  if (_data) SecureZeroMemory(_data, mir_strlen(_data)); 
 	}
 };
 
 class pass_ptrW : public mir_ptr<WCHAR>
 {
-
 public:
 	__inline explicit pass_ptrW() : mir_ptr(){}
 	__inline explicit pass_ptrW(WCHAR* _p) : mir_ptr(_p) {}
 	__inline ~pass_ptrW() { zero(); }
+	__inline WCHAR* operator = (WCHAR *_p){ zero(); mir_ptr::operator=(_p); }
 	__inline void zero() 
 	{ WCHAR *_data = mir_ptr::operator WCHAR *();
 	  if (_data) SecureZeroMemory(_data, mir_wstrlen(_data)*sizeof(WCHAR));
