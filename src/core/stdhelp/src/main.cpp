072e042f692e29331645f59fb70f80e084f4bb33
Kirill Volinsky <mataes2007@gmail.com>
2015-08-08 20:10:49 +0000

    stdhelp: common project
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14867 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- src/core/stdhelp/src/main.cpp
+++ src/core/stdhelp/src/main.cpp
@@ -19,7 +19,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
-#include "commonheaders.h"
+#include "stdafx.h"
 
 int LoadHelpModule(void);
 
