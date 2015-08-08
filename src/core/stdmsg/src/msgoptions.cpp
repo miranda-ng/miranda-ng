b9703bb8be514ca0d9aaae40c0771ea90cb8318c
Kirill Volinsky <mataes2007@gmail.com>
2015-08-08 20:20:21 +0000

    stdmsg: common project
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14869 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- src/core/stdmsg/src/msgoptions.cpp
+++ src/core/stdmsg/src/msgoptions.cpp
@@ -19,7 +19,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-#include "commonheaders.h"
+#include "stdafx.h"
 
 #include "m_fontservice.h"
 #include "m_modernopt.h"
