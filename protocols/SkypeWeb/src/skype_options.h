e6242b0e9de14d2d6929116980aa398757714058
MikalaiR <nikolay.romanovich@narod.ru>
2015-08-08 09:07:44 +0000

    SkypeWeb: bbcodes in edited messages
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14859 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/SkypeWeb/src/skype_options.h
+++ protocols/SkypeWeb/src/skype_options.h
@@ -29,6 +29,7 @@ private:
 	CCtrlCheck m_localtime;
 	CCtrlCheck m_allasunread;
 	CCtrlCheck m_usehostname;
+	CCtrlCheck m_usebb;
 
 protected:
 	CSkypeOptionsMain(CSkypeProto *proto, int idDialog);
