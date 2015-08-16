c080cfada2c6e55da9d14a875c4687268965d3cc
George Hazan <george.hazan@gmail.com>
2015-08-16 14:09:56 +0000

    minus 3 warnings
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14964 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- libs/zlib/src/gzclose.c
+++ libs/zlib/src/gzclose.c
@@ -5,21 +5,21 @@
 
 #include "gzguts.h"
 
-/* gzclose() is in a separate file so that it is linked in only if it is used.
-   That way the other gzclose functions can be used instead to avoid linking in
-   unneeded compression or decompression routines. */
+ /* gzclose() is in a separate file so that it is linked in only if it is used.
+	 That way the other gzclose functions can be used instead to avoid linking in
+	 unneeded compression or decompression routines. */
 int ZEXPORT gzclose(file)
-    gzFile file;
+gzFile file;
 {
-#ifndef NO_GZCOMPRESS
-    gz_statep state;
+	#ifndef NO_GZCOMPRESS
+	gz_statep state;
 
-    if (file == NULL)
-        return Z_STREAM_ERROR;
-    state = (gz_statep)file;
+	if (file == NULL)
+		return Z_STREAM_ERROR;
+	state = (gz_statep)file;
 
-    return state->mode == GZ_READ ? gzclose_r(file) : gzclose_w(file);
-#else
-    return gzclose_r(file);
-#endif
+	return state->mode == GZ_READ ? gzclose_r(file) : gzclose_w(file);
+	#else
+	return gzclose_r(file);
+	#endif
 }
