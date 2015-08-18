ce2529939b498acc2be8c3c09c897c6f96eee9f7
MikalaiR <nikolay.romanovich@narod.ru>
2015-08-18 11:14:19 +0000

    SkypeWeb: more fixes
    git-svn-id: http://svn.miranda-ng.org/main/trunk@14982 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/SkypeWeb/src/skype_trouter.h
+++ protocols/SkypeWeb/src/skype_trouter.h
@@ -18,18 +18,4 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 #ifndef _SKYPE_TROUTER_H_
 #define _SKYPE_TROUTER_H_
 
-struct TRInfo
-{
-	std::string socketIo,
-		connId,
-		st,
-		se,
-		instance,
-		ccid,
-		sessId,
-		sig,
-		url;
-	time_t lastRegistrationTime;
-};
-
 #endif //_SKYPE_TROUTER_H_
