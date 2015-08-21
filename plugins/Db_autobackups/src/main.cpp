d131ea1bc4bce30516a2f9cbb7579f1174f46389
Tobias Weimer <wishmaster51@googlemail.com>
2015-08-21 19:48:01 +0000

    db_autobackups: - compilation fix
    git-svn-id: http://svn.miranda-ng.org/main/trunk@15006 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Db_autobackups/src/main.cpp
+++ plugins/Db_autobackups/src/main.cpp
@@ -19,14 +19,71 @@ PLUGININFOEX pluginInfo = {
 	{ 0x81c220a6, 0x226, 0x4ad6, { 0xbf, 0xca, 0x21, 0x7b, 0x17, 0xa1, 0x60, 0x53 } }
 };
 
-int ModulesLoad(WPARAM, LPARAM);
-int PreShutdown(WPARAM, LPARAM);
-
-
 BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD, LPVOID)
 {
 	g_hInstance = hInstance;
 	return TRUE;
+}
+
+static INT_PTR ABService(WPARAM, LPARAM)
+{
+	BackupStart(NULL);
+	return 0;
+}
+
+static INT_PTR DBSaveAs(WPARAM, LPARAM)
+{
+	TCHAR fname_buff[MAX_PATH], tszFilter[200];
+	OPENFILENAME ofn = { 0 };
+	CallService(MS_DB_GETPROFILENAMET, _countof(fname_buff), (LPARAM)fname_buff);
+
+	mir_sntprintf(tszFilter, _countof(tszFilter), _T("%s (*.dat)%c*.dat%c%s (*.zip)%c*.zip%c%s (*.*)%c*%c"),
+		TranslateT("Miranda NG databases"), 0, 0,
+		TranslateT("Compressed Miranda NG databases"), 0, 0,
+		TranslateT("All files"), 0, 0);
+
+	ofn.lStructSize = sizeof(ofn);
+	ofn.lpstrFile = fname_buff;
+	ofn.nMaxFile = _countof(fname_buff);
+	ofn.Flags = OFN_NOREADONLYRETURN | OFN_OVERWRITEPROMPT;
+	ofn.lpstrFilter = tszFilter;
+	ofn.nFilterIndex = 1;
+	ofn.lpstrDefExt = _T("dat");
+
+	if (GetSaveFileName(&ofn))
+		BackupStart(fname_buff);
+	return 0;
+}
+
+static int FoldersGetBackupPath(WPARAM, LPARAM)
+{
+	FoldersGetCustomPathT(hFolder, options.folder, _countof(options.folder), DIR SUB_DIR);
+	return 0;
+}
+
+static int ModulesLoad(WPARAM, LPARAM)
+{
+	profilePath = Utils_ReplaceVarsT(_T("%miranda_userdata%"));
+
+	if (hFolder = FoldersRegisterCustomPathT(LPGEN("Database backups"), LPGEN("Backup folder"), DIR SUB_DIR)) {
+		HookEvent(ME_FOLDERS_PATH_CHANGED, FoldersGetBackupPath);
+		FoldersGetBackupPath(0, 0);
+	}
+
+	if (options.backup_types & BT_START)
+		BackupStart(NULL);
+	return 0;
+}
+
+// can't do this on unload, since other plugins will be have already been unloaded, but their hooks
+// for setting changed event not cleared. the backup on exit function will write to the db, calling those hooks.
+static int PreShutdown(WPARAM, LPARAM)
+{
+	if (options.backup_types & BT_EXIT) {
+		options.disable_popups = 1; // Don't try to show popups on exit
+		BackupStart(NULL);
+	}
+	return 0;
 }
 
 extern "C" __declspec(dllexport) PLUGININFOEX* MirandaPluginInfoEx(DWORD)
@@ -72,64 +129,4 @@ extern "C" __declspec(dllexport) int Unload(void)
 	return 0;
 }
 
-
-
-static int FoldersGetBackupPath(WPARAM, LPARAM)
-{
-	FoldersGetCustomPathT(hFolder, options.folder, _countof(options.folder), DIR SUB_DIR);
-	return 0;
-}
-INT_PTR ABService(WPARAM, LPARAM)
-{
-	BackupStart(NULL);
-	return 0;
-}
-INT_PTR DBSaveAs(WPARAM, LPARAM)
-{
-	TCHAR fname_buff[MAX_PATH], tszFilter[200];
-	OPENFILENAME ofn = { 0 };
-	CallService(MS_DB_GETPROFILENAMET, _countof(fname_buff), (LPARAM)fname_buff);
-
-	mir_sntprintf(tszFilter, _countof(tszFilter), _T("%s (*.dat)%c*.dat%c%s (*.zip)%c*.zip%c%s (*.*)%c*%c"),
-		TranslateT("Miranda NG databases"), 0, 0,
-		TranslateT("Compressed Miranda NG databases"), 0, 0,
-		TranslateT("All files"), 0, 0);
-
-	ofn.lStructSize = sizeof(ofn);
-	ofn.lpstrFile = fname_buff;
-	ofn.nMaxFile = _countof(fname_buff);
-	ofn.Flags = OFN_NOREADONLYRETURN | OFN_OVERWRITEPROMPT;
-	ofn.lpstrFilter = tszFilter;
-	ofn.nFilterIndex = 1;
-	ofn.lpstrDefExt = _T("dat");
-
-	if (GetSaveFileName(&ofn))
-		BackupStart(fname_buff);
-	return 0;
-}
-
-int ModulesLoad(WPARAM, LPARAM)
-{
-	profilePath = Utils_ReplaceVarsT(_T("%miranda_userdata%"));
-
-	if (hFolder = FoldersRegisterCustomPathT(LPGEN("Database backups"), LPGEN("Backup folder"), DIR SUB_DIR)) {
-		HookEvent(ME_FOLDERS_PATH_CHANGED, FoldersGetBackupPath);
-		FoldersGetBackupPath(0, 0);
-	}
-
-	if (options.backup_types & BT_START)
-		BackupStart(NULL);
-	return 0;
-}
-
-// can't do this on unload, since other plugins will be have already been unloaded, but their hooks
-// for setting changed event not cleared. the backup on exit function will write to the db, calling those hooks.
-int PreShutdown(WPARAM, LPARAM)
-{
-	if (options.backup_types & BT_EXIT) {
-		options.disable_popups = 1; // Don't try to show popups on exit
-		BackupStart(NULL);
-	}
-	return 0;
-}
 
