5a36b78ad280e5026e73cee9faba65f3ec98d997
MikalaiR <nikolay.romanovich@narod.ru>
2015-08-29 13:20:42 +0000

    oooops
    git-svn-id: http://svn.miranda-ng.org/main/trunk@15093 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

---
+++ plugins/Toaster/src/add_to_start_menu.h
@@ -0,0 +1,12 @@
+wchar_t* GetShortcutPath();
+HRESULT InstallShortcut(_In_z_ wchar_t *shortcutPath);
+
+__forceinline bool ShortcutExists()
+{
+	return !(GetFileAttributes(ptrW(GetShortcutPath())) < 0xFFFFFFF);
+}
+
+__forceinline HRESULT TryCreateShortcut()
+{
+	return (ShortcutExists() ? S_OK : InstallShortcut(ptrW(GetShortcutPath())));
+}
