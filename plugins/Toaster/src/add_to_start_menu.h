04db25c2488765cadb207b71c5fac77eb93bbeea
MikalaiR <nikolay.romanovich@narod.ru>
2015-08-30 09:15:36 +0000

    Toaster: fix shortcut
    git-svn-id: http://svn.miranda-ng.org/main/trunk@15113 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Toaster/src/add_to_start_menu.h
+++ plugins/Toaster/src/add_to_start_menu.h
@@ -2,11 +2,9 @@ wchar_t* GetShortcutPath();
 HRESULT InstallShortcut(_In_z_ wchar_t *shortcutPath);
 
 __forceinline bool ShortcutExists()
-{
-	return !(GetFileAttributes(ptrW(GetShortcutPath())) < 0xFFFFFFF);
+{	return (GetFileAttributes(ptrW(GetShortcutPath())) < 0xFFFFFFF);
 }
 
 __forceinline HRESULT TryCreateShortcut()
-{
-	return (ShortcutExists() ? S_OK : InstallShortcut(ptrW(GetShortcutPath())));
+{	return (ShortcutExists() ? S_OK : InstallShortcut(ptrW(GetShortcutPath())));
 }
