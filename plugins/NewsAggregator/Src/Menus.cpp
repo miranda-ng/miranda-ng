960470c34f3e37a35b1e25265e37fcc17f137aa2
Alexander Lantsev <aunsane@gmail.com>
2015-09-04 20:04:41 +0000

    NewsAggregator: menu uid
    git-svn-id: http://svn.miranda-ng.org/main/trunk@15234 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/NewsAggregator/Src/Menus.cpp
+++ plugins/NewsAggregator/Src/Menus.cpp
@@ -27,6 +27,7 @@ void InitMenu()
 	mi.flags = CMIF_TCHAR | CMIF_NOTOFFLINE;
 	mi.root = Menu_CreateRoot(MO_MAIN, LPGENT("News Aggregator"), 500099000);
 
+	SET_UID(mi, 0x3ec91864, 0xefa7, 0x4994, 0xb7, 0x75, 0x6c, 0x96, 0xcb, 0x29, 0x2f, 0x93);
 	mi.position = 10100001;
 	if (db_get_b(NULL, MODULE, "AutoUpdate", 1))
 		mi.name.t = LPGENT("Auto Update Enabled");
@@ -36,23 +37,27 @@ void InitMenu()
 	mi.pszService = MS_NEWSAGGREGATOR_ENABLED;
 	hService2[0] = Menu_AddMainMenuItem(&mi);
 
+	SET_UID(mi, 0x8076bb4d, 0x1e44, 0x43af, 0x97, 0x1e, 0x31, 0xd8, 0xa4, 0xe9, 0xb8, 0x37);
 	mi.position = 20100001;
 	mi.name.t = LPGENT("Check All Feeds");
 	mi.pszService = MS_NEWSAGGREGATOR_CHECKALLFEEDS;
 	hService2[1] = Menu_AddMainMenuItem(&mi);
 
+	SET_UID(mi, 0xb876484d, 0x28aa, 0x4e03, 0x9e, 0x98, 0xed, 0xbc, 0xd1, 0xcf, 0x31, 0x80);
 	mi.position = 20100002;
 	mi.hIcolibItem = GetIconHandle("addfeed");
 	mi.name.t = LPGENT("Add Feed");
 	mi.pszService = MS_NEWSAGGREGATOR_ADDFEED;
 	hService2[2] = Menu_AddMainMenuItem(&mi);
 
+	SET_UID(mi, 0x600bf2c2, 0xa974, 0x44d3, 0x98, 0xf9, 0xe6, 0x65, 0x7c, 0x1f, 0x63, 0x37);
 	mi.position = 20100003;
 	mi.hIcolibItem = GetIconHandle("importfeeds");
 	mi.name.t = LPGENT("Import Feeds");
 	mi.pszService = MS_NEWSAGGREGATOR_IMPORTFEEDS;
 	hService2[3] = Menu_AddMainMenuItem(&mi);
 
+	SET_UID(mi, 0xc09c8119, 0x64c2, 0x49bd, 0x81, 0xf, 0x54, 0x20, 0x69, 0xd7, 0x30, 0xcf);
 	mi.position = 20100004;
 	mi.hIcolibItem = GetIconHandle("exportfeeds");
 	mi.name.t = LPGENT("Export Feeds");
@@ -60,14 +65,15 @@ void InitMenu()
 	hService2[4] = Menu_AddMainMenuItem(&mi);
 
 	// adding contact menu items
+	SET_UID(mi, 0x92be499c, 0x928c, 0x4789, 0x8f, 0x36, 0x28, 0xa2, 0x9f, 0xb7, 0x1a, 0x97);
 	mi.root = NULL;
 	mi.position = -0x7FFFFFFA;
-
 	mi.hIcolibItem = GetIconHandle("checkfeed");
 	mi.name.t = LPGENT("Check feed");
 	mi.pszService = MS_NEWSAGGREGATOR_CHECKFEED;
 	hService2[5] = Menu_AddContactMenuItem(&mi, MODULE);
 
+	SET_UID(mi, 0x41a70fbc, 0x9241, 0x44c0, 0x90, 0x90, 0x87, 0xd2, 0xc5, 0x9f, 0xc9, 0xac);
 	mi.name.t = LPGENT("Change feed");
 	mi.pszService = MS_NEWSAGGREGATOR_CHANGEFEED;
 	hService2[6] = Menu_AddContactMenuItem(&mi, MODULE);
