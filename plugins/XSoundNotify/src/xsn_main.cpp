5366c801c27beeb8dbe581ef0fe0c3351537c0f8
Alexander Lantsev <aunsane@gmail.com>
2015-09-05 11:57:25 +0000

    XSoundNotify: menu uid
    git-svn-id: http://svn.miranda-ng.org/main/trunk@15274 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/XSoundNotify/src/xsn_main.cpp
+++ plugins/XSoundNotify/src/xsn_main.cpp
@@ -178,6 +178,7 @@ static int OnLoadInit(WPARAM, LPARAM)
 	mir_getCI(NULL);
 
 	CMenuItem mi;
+	SET_UID(mi, 0x5d72ca1f, 0xc52, 0x436d, 0x81, 0x47, 0x29, 0xf6, 0xc3, 0x28, 0xb5, 0xd1);
 	mi.position = -0x7FFFFFFF;
 	mi.flags = CMIF_TCHAR;
 	mi.hIcolibItem = Skin_LoadIcon(SKINICON_OTHER_MIRANDA);
