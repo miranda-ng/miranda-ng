8fd80b25369cbd14219127f504b1727644a6f8ae
Alexander Lantsev <aunsane@gmail.com>
2015-09-05 11:26:57 +0000

    SMS: menu uid
    git-svn-id: http://svn.miranda-ng.org/main/trunk@15262 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/SMS/src/SMS_svc.cpp
+++ plugins/SMS/src/SMS_svc.cpp
@@ -28,6 +28,8 @@ int LoadModules(void)
 	mir_snprintf(szServiceFunction, "%s%s", PROTOCOL_NAMEA, SMS_SEND);
 
 	CMenuItem mi;
+
+	SET_UID(mi, 0x3ce387db, 0xbaac, 0x490f, 0xac, 0xab, 0x8c, 0xf7, 0xe9, 0xcd, 0x86, 0xa1);
 	mi.position = 300050000;
 	mi.hIcolibItem = Skin_LoadIcon(SKINICON_OTHER_SMS);
 	mi.name.t = SMS_SEND_STR;
@@ -35,6 +37,7 @@ int LoadModules(void)
 	mi.flags = CMIF_TCHAR;
 	Menu_AddMainMenuItem(&mi);
 
+	SET_UID(mi, 0x736e4cff, 0x769e, 0x45dc, 0x8b, 0x78, 0x83, 0xf9, 0xe4, 0xbb, 0x81, 0x9e);
 	mi.position = -2000070000;
 	mi.hIcolibItem = Skin_LoadIcon(SKINICON_OTHER_SMS);
 	mi.name.t = SMS_SEND_CM_STR;
