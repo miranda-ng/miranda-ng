9fa74ab4f864a651f22a107818baee2cc55fca10
Vadim Dashevskiy <watcherhd@gmail.com>
2015-09-07 20:48:58 +0000

    xfire moved to deprecated pt. 1
    git-svn-id: http://svn.miranda-ng.org/main/trunk@15300 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/Xfire/src/all_statusmsg.h
+++
@@ -1,7 +0,0 @@
-//definitionen für die statusmsg handling geschichte
-#include "baseProtocol.h"
-
-//sichert die statusmeldungen in den speicher
-BOOL BackupStatusMsg();
-BOOL SetGameStatusMsg();
-BOOL SetOldStatusMsg();
--- protocols/Xfire/src/passworddialog.h
+++
@@ -1,4 +0,0 @@
-#include "baseProtocol.h"
-#include "resource.h"
-
-void ShowPasswordDialog(char*pw, char*mynick = NULL);
