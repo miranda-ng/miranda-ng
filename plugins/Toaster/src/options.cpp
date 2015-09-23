a8dfca9c4a079c6d9706fa819b8993a2df5d7270
MikalaiR <nikolay.romanovich@narod.ru>
2015-09-23 11:26:04 +0000

    Toaster: options page moved
    git-svn-id: http://svn.miranda-ng.org/main/trunk@15428 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Toaster/src/options.cpp
+++ plugins/Toaster/src/options.cpp
@@ -35,11 +35,10 @@ void COptions::Preview_OnClick(CCtrlBase*)
 int OnOptionsInitialized(WPARAM wParam, LPARAM)
 {
 	OPTIONSDIALOGPAGE odp = { 0 };
-	odp.ptszTitle = _T(MODULE);
-	odp.flags = ODPF_BOLDGROUPS | ODPF_TCHAR | ODPF_DONTTRANSLATE;
-	odp.ptszGroup = LPGENT("Popups");
-
-	odp.ptszTab = _T("Main");
+	odp.ptszTitle = _T("Popups"); //_T(MODULE);
+	odp.flags = ODPF_BOLDGROUPS | ODPF_TCHAR;
+	odp.position = -790000000;
+	odp.ptszTab = _T(MODULE);
 	odp.pDialog = new COptions();
 	Options_AddPage(wParam, &odp);
 
