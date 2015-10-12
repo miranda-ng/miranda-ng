9a8a534b524afa4db766b9307a5779ef17908973
Dart Raiden <wowemuh@gmail.com>
2015-10-12 21:21:37 +0000

    langpacks/belarusian: simpler to maintain, with standart update scripts
    git-svn-id: http://svn.miranda-ng.org/main/trunk@15544 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- langpacks/belarusian/UpdateLang.bat
+++
@@ -1,7 +0,0 @@
-for %%* in (.) do set CurrDirName=%%~n*
-set langdir=%CD%
-set lang=%CurrDirName%
-
-pushd ..\..\tools\lpgen
-cscript /nologo translate.js /sourcelang:"%lang%" /popuntranslated:"yes" /out:"%langdir%"
-popd
