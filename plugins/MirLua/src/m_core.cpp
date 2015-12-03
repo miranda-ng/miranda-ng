3bfd759c3a7817c3b791ac83a7f5c4f4d30edf7f
Alexander Lantsev <aunsane@gmail.com>
2015-12-03 19:48:11 +0000

    MirLua: fix plugin uid loading too
    git-svn-id: http://svn.miranda-ng.org/main/trunk@15820 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/MirLua/src/m_core.cpp
+++ plugins/MirLua/src/m_core.cpp
@@ -198,9 +198,9 @@ static int lua_IsPluginLoaded(lua_State *L)
 	const char *value = lua_tostring(L, 1);
 
 	MUUID uuid = { 0 };
-	int res = CLSIDFromString((LPCOLESTR)ptrT(mir_utf8decodeT(value)), (LPCLSID)&uuid) == NOERROR;
-	if (res == TRUE)
-		res = ::IsPluginLoaded(uuid);
+	bool res = UuidFromStringA((RPC_CSTR)value, (UUID*)&uuid) == RPC_S_OK;
+	if (res)
+		res = IsPluginLoaded(uuid) > 0;
 	lua_pushboolean(L, res);
 
 	return 1;
