4306f45cd0380db1221ea5a67f1c1534f70e2cae
George Hazan <george.hazan@gmail.com>
2015-12-20 18:08:18 +0000

    MRA contact menu isn't displayed in the editor
    git-svn-id: http://svn.miranda-ng.org/main/trunk@15916 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/MRA/src/Mra_menus.cpp
+++ protocols/MRA/src/Mra_menus.cpp
@@ -355,11 +355,12 @@ HGENMENU CMraProto::CListCreateMenu(LONG lPosition, LONG lPopupPosition, BOOL bI
 	}
 
 	SET_UID(mi, 0x83C8B6A7, 0xEC0D, 0x41D6, 0x8A, 0x0E, 0xAC, 0x90, 0x8C, 0xEE, 0xAF, 0xFE);
-	mi.flags = CMIF_SYSTEM;
+	mi.flags = 0;
 	mi.name.a = LPGEN("Services...");
 	mi.hIcolibItem = g_hMainIcon;
 	hRootMenu = fnAddFunc(&mi, m_szModuleName);
 
+	mi.flags = CMIF_SYSTEM;
 	mi.root = hRootMenu;
 	mi.pszService = szServiceFunction;
 
