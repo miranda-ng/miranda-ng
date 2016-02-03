24949e548f6e2cdf7f04097686e94dc3cdda34fe
Dart Raiden <wowemuh@gmail.com>
2016-02-03 18:55:30 +0000

    public servers list now on xmpp.net
    git-svn-id: http://svn.miranda-ng.org/main/trunk@16219 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/JabberG/src/jabber_opt.cpp
+++ protocols/JabberG/src/jabber_opt.cpp
@@ -388,7 +388,7 @@ public:
 		m_btnUnregister(this, IDC_UNREGISTER),
 		m_btnChangePassword(this, IDC_BUTTON_CHANGE_PASSWORD),
 		m_gotservers(false),
-		m_lnkServers(this, IDC_LINK_PUBLIC_SERVER, "http://xmpp.org/services/")
+		m_lnkServers(this, IDC_LINK_PUBLIC_SERVER, "https://xmpp.net/directory.php")
 	{
 		CreateLink(m_txtUsername, "LoginName", _T(""));
 		CreateLink(m_txtPriority, "Priority", DBVT_WORD, 0);
