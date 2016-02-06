edf2ac2358c5551f17bfe9e7a58c2ae23b0a1a3f
Goraf <goraf@miranda-ng.org>
2016-02-06 18:09:52 +0000

    ContextHelp: richedit20.dll -> msftedit
    git-svn-id: http://svn.miranda-ng.org/main/trunk@16228 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/ContextHelp/src/stdafx.h
+++ plugins/ContextHelp/src/stdafx.h
@@ -23,6 +23,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <windows.h>
 #include <Uxtheme.h>
 #include <Vsstyle.h>
+#include <richedit.h>
 
 #include <newpluginapi.h>
 //#include <win2k.h>
