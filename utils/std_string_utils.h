e2e382f6628618725ad22245f2a3d710e7775349
Robert Pösel <robyer@seznam.cz>
2016-02-12 01:58:30 +0000

    Facebook: Improve login procedure (not finished) and fix getting login errors; version bump
    git-svn-id: http://svn.miranda-ng.org/main/trunk@16275 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- utils/std_string_utils.h
+++ utils/std_string_utils.h
@@ -72,7 +72,7 @@ namespace utils
 		std::string trim(const std::string &data, bool rtrim = false);
 		std::string source_get_value(std::string* data, unsigned int argument_count, ...);
 		std::string source_get_value2(std::string* data, const char *term, const char *endings, bool wholeString = false);
-		std::string source_get_form_data(std::string* data);
+		std::string source_get_form_data(std::string* data, boolean hiddenOnly = false);
 		std::string rand_string(int len, const char *chars = "0123456789" "ABCDEFGHIJKLMNOPQRSTUVWXYZ" "abcdefghijklmnopqrstuvwxyz", unsigned int *number = NULL);
 		void explode(std::string str, const std::string &separator, std::vector<std::string>* results);
 		void append_ordinal(unsigned long value, std::string* data);
