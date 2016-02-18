c9bf09d88ffe7db1c2a3e2cdc3cc3085573cb23c
MikalaiR <nikolay.romanovich@narod.ru>
2016-02-18 13:33:24 +0000

    extended crypto provider selector
    git-svn-id: http://svn.miranda-ng.org/main/trunk@16303 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/Db3x_mmap/src/resource.h
+++ plugins/Db3x_mmap/src/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
 // Включаемый файл, созданный в Microsoft Visual C++.
-// Используется d:\Others\SVN\MirandaNG\trunk\plugins\Db3x_mmap\res\db3x_mmap.rc
+// Используется D:\Others\SVN\MirandaNG\trunk\plugins\Db3x_mmap\res\db3x_mmap.rc
 //
 #define IDREMOVE                        3
 #define IDI_ICONPASS                    100
@@ -19,6 +19,7 @@
 #define IDC_STANDARD                    1007
 #define IDC_TOTAL                       1008
 #define IDC_SELECTCRYPT_COMBO           1010
+#define IDC_CRYPTOPROVIDER_DESCR        1011
 
 // Next default values for new objects
 // 
@@ -26,7 +27,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        107
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_CONTROL_VALUE         1012
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
