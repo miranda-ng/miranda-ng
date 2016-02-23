f818cfac32e8c78c104065333fde3b0075a7d9e4
MikalaiR <nikolay.romanovich@narod.ru>
2016-02-23 19:19:21 +0000

    CryptoPP: memeleaks fix
    git-svn-id: http://svn.miranda-ng.org/main/trunk@16333 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/CryptoPP/src/base64.cpp
+++ plugins/CryptoPP/src/base64.cpp
@@ -8,13 +8,13 @@ string base64encode(const string& buf)
 string base64decode(const string& buf)
 {
 	unsigned len;
-	char *plain = (char*)mir_base64_decode(buf.data(), &len);
+	ptrA plain((char*)mir_base64_decode(buf.data(), &len));
 	return (plain == NULL) ? string() : string(plain, len);
 }
 
 string base64decode(const char *buf)
 {
 	unsigned len;
-	char *plain = (char*)mir_base64_decode(buf, &len);
+	ptrA plain((char*)mir_base64_decode(buf, &len));
 	return (plain == NULL) ? string() : string(plain, len);
 }
