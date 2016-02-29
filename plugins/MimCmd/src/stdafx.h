19e1407076aa7fb6040c36426fe911499cb4a7d4
MikalaiR <nikolay.romanovich@narod.ru>
2016-02-29 16:34:59 +0000

    MimCmd: unicode in output
    git-svn-id: http://svn.miranda-ng.org/main/trunk@16379 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- plugins/MimCmd/src/stdafx.h
+++ plugins/MimCmd/src/stdafx.h
@@ -21,6 +21,10 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <stdio.h>
 #include <windows.h>
 
+#include <iostream>
+#include <io.h>
+#include <fcntl.h>
+
 #define NO_MIMCMD_COMMANDS
 
 #include <newpluginapi.h>
