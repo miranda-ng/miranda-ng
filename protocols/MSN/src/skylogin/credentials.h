baf1b63fc2a555eba39451b7dca876b13e76a862
George Hazan <george.hazan@gmail.com>
2016-03-19 17:21:24 +0000

    built-in skylogin library (gotten from https://github.com/msndevs/skylogin.git)
    git-svn-id: http://svn.miranda-ng.org/main/trunk@16512 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

---
+++ protocols/MSN/src/skylogin/credentials.h
@@ -0,0 +1,3 @@
+Memory_U Credentials_Write(Skype_Inst *pInst);
+int Credentials_Read(Skype_Inst *pInst, Memory_U creds, SResponse *LoginDatas);
+int Credentials_Parse(Memory_U creds, SResponse *LoginDatas);
---
+++ protocols/MSN/src/skylogin/login.h
@@ -0,0 +1,2 @@
+int PerformLogin(Skype_Inst *pInst, const char *User, const char *Pass);
+void InitInstance(Skype_Inst *pInst);
