16b8513856718601bfb7474eb0a6838e4399f86b
Robert Pösel <robyer@seznam.cz>
2016-04-24 15:39:01 +0000

    Steam: Fix dialog's used big/small icon
    git-svn-id: http://svn.miranda-ng.org/main/trunk@16763 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- protocols/Steam/src/steam_dialogs.cpp
+++ protocols/Steam/src/steam_dialogs.cpp
@@ -11,8 +11,8 @@ void CSteamPasswordEditor::OnInitDialog()
 {
 	char iconName[100];
 	mir_snprintf(iconName, "%s_%s", MODULE, "main");
-	SendMessage(m_hwnd, WM_SETICON, ICON_BIG, (LPARAM)IcoLib_GetIcon(iconName, false));
-	SendMessage(m_hwnd, WM_SETICON, ICON_SMALL, (LPARAM)IcoLib_GetIcon(iconName, true));
+	SendMessage(m_hwnd, WM_SETICON, ICON_BIG, (LPARAM)IcoLib_GetIcon(iconName, true));
+	SendMessage(m_hwnd, WM_SETICON, ICON_SMALL, (LPARAM)IcoLib_GetIcon(iconName, false));
 
 	SendMessage(m_password.GetHwnd(), EM_LIMITTEXT, 64, 0);
 
@@ -52,8 +52,8 @@ void CSteamGuardDialog::OnInitDialog()
 {
 	char iconName[100];
 	mir_snprintf(iconName, "%s_%s", MODULE, "main");
-	SendMessage(m_hwnd, WM_SETICON, ICON_BIG, (LPARAM)IcoLib_GetIcon(iconName, false));
-	SendMessage(m_hwnd, WM_SETICON, ICON_SMALL, (LPARAM)IcoLib_GetIcon(iconName, true));
+	SendMessage(m_hwnd, WM_SETICON, ICON_BIG, (LPARAM)IcoLib_GetIcon(iconName, true));
+	SendMessage(m_hwnd, WM_SETICON, ICON_SMALL, (LPARAM)IcoLib_GetIcon(iconName, false));
 
 	SendMessage(m_text.GetHwnd(), EM_LIMITTEXT, 5, 0);
 
@@ -100,8 +100,8 @@ void CSteamCaptchaDialog::OnInitDialog()
 {
 	char iconName[100];
 	mir_snprintf(iconName, "%s_%s", MODULE, "main");
-	SendMessage(m_hwnd, WM_SETICON, ICON_BIG, (LPARAM)IcoLib_GetIcon(iconName, false));
-	SendMessage(m_hwnd, WM_SETICON, ICON_SMALL, (LPARAM)IcoLib_GetIcon(iconName, true));
+	SendMessage(m_hwnd, WM_SETICON, ICON_BIG, (LPARAM)IcoLib_GetIcon(iconName, true));
+	SendMessage(m_hwnd, WM_SETICON, ICON_SMALL, (LPARAM)IcoLib_GetIcon(iconName, false));
 
 	SendMessage(m_text.GetHwnd(), EM_LIMITTEXT, 6, 0);
 
