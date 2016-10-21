6b4e23886eade6f3b209e0f46ac6381cce1362e9
George Hazan <g.hazan@alol.io>
2016-10-21 18:44:15 +0300

    correct long file path processing

--- git_read.cmd
+++ git_read.cmd
@@ -1,5 +1,5 @@
 @echo off
-git pull --rebase=true --progress -v 2>&1 >%temp%\report.txt
+git pull --rebase=true --progress -v 2>&1 > "%temp%\report.txt"
 call .\build\make_ver.bat
-start %temp%\report.txt
+start "" "%temp%\report.txt"
 goto :eof
