462d365fd7b2095e768685716b09e1bd36e690ad
George Hazan <ghazan@miranda.im>
2016-11-01 23:30:50 +0300

    - StdChat removed from all solutions; - MIID_CHAT removed

--- plugins/Scriver/src/srmm.cpp
+++ plugins/Scriver/src/srmm.cpp
@@ -57,7 +57,7 @@ extern "C" __declspec(dllexport) PLUGININFOEX *MirandaPluginInfoEx(DWORD)
 	return &pluginInfo;
 }
 
-extern "C" __declspec(dllexport) const MUUID MirandaInterfaces[] = { MIID_SRMM, MIID_CHAT, MIID_LAST };
+extern "C" __declspec(dllexport) const MUUID MirandaInterfaces[] = { MIID_SRMM, MIID_LAST };
 
 extern "C" __declspec(dllexport) int Load(void)
 {
