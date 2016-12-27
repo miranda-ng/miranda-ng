083d339f5b79ccdb80c275a4c93f227b1bc3513a
George Hazan <ghazan@miranda.im>
2016-12-27 19:27:32 +0300

    Discord: initial commit

---
+++ protocols/Discord/src/stdafx.h
@@ -0,0 +1,41 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include <Windows.h>
+#include <Shlwapi.h>
+#include <Wincrypt.h>
+#include <stdio.h>
+#include <direct.h>
+#include <time.h>
+
+#include "resource.h"
+
+#include <m_system_cpp.h>
+#include <newpluginapi.h>
+#include <m_chat.h>
+#include <m_clist.h>
+#include <m_database.h>
+#include <m_folders.h>
+#include <m_history.h>
+#include <m_langpack.h>
+#include <m_message.h>
+#include <m_netlib.h>
+#include <m_options.h>
+#include <m_popup.h>
+#include <m_protocols.h>
+#include <m_protosvc.h>
+#include <m_protoint.h>
+#include <m_skin.h>
+#include <m_system.h>
+#include <m_userinfo.h>
+#include <m_icolib.h>
+#include <m_utils.h>
+#include <m_hotkeys.h>
+#include <m_json.h>
+#include <win2k.h>
+
+extern HINSTANCE g_hInstance;
---
+++ protocols/Discord/src/version.h
@@ -0,0 +1,14 @@
+#define __MAJOR_VERSION            1
+#define __MINOR_VERSION            3
+#define __RELEASE_NUM              0
+#define __BUILD_NUM                2
+
+#include <stdver.h>
+
+#define __PLUGIN_NAME              "Twitter protocol"
+#define __FILENAME                 "Twitter.dll"
+#define __DESCRIPTION              "Twitter protocol support for Miranda NG."
+#define __AUTHOR                   "dentist, omniwolf, Thief"
+#define __AUTHOREMAIL              ""
+#define __AUTHORWEB                "http://miranda-ng.org/p/Twitter/"
+#define __COPYRIGHT                "© 2009-2010 dentist, 2010-2012 omniwolf and Thief"
