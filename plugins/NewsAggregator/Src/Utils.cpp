133826f5022c5b0d9beca9285d66afcb604e2fc4
Kirill Volinsky <mataes2007@gmail.com>
2017-05-28 21:47:59 +0300

    NewsAggregator: correct version of 774 code

--- plugins/NewsAggregator/Src/Utils.cpp
+++ plugins/NewsAggregator/Src/Utils.cpp
@@ -467,7 +467,7 @@ LPCTSTR ClearText(CMStringW &result, const wchar_t *message)
 	result.Replace(L"&#235;", L"ë"); //euml
 	result.Replace(L"&#246;", L"ö"); //ouml
 	result.Replace(L"&#382;", L"ž"); //Latin Small Letter Z With Caron
-	result.Replace(L"и&#774;", L"й"); //Combining Breve
+	result.Replace(L"&#774;", L"˘"); //Combining Breve
 	result.Replace(L"&#769;", L"´"); //Combining Acute Accent острое ударение
 	result.Replace(L"&#1123;", L"ѣ"); //Cyrillic Small Letter Yat
 	result.Replace(L"&#8203;", L"");
