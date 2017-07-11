3f7908311d1ce518bbe8752942f09db47f6077ae
George Hazan <ghazan@miranda.im>
2017-07-11 19:21:01 +0300

    PluginUpdater to load everything via https

--- plugins/PluginUpdater/src/stdafx.h
+++ plugins/PluginUpdater/src/stdafx.h
@@ -104,9 +104,9 @@ extern struct PlugOptions
 	#define DEFAULT_ONLYONCEADAY      1
 #endif
 
-#define DEFAULT_UPDATE_URL                L"http://miranda-ng.org/distr/stable/x%d"
-#define DEFAULT_UPDATE_URL_TRUNK          L"http://miranda-ng.org/distr/x%d"
-#define DEFAULT_UPDATE_URL_TRUNK_SYMBOLS  L"http://miranda-ng.org/distr/pdb_x%d"
+#define DEFAULT_UPDATE_URL                L"https://miranda-ng.org/distr/stable/x%d"
+#define DEFAULT_UPDATE_URL_TRUNK          L"https://miranda-ng.org/distr/x%d"
+#define DEFAULT_UPDATE_URL_TRUNK_SYMBOLS  L"https://miranda-ng.org/distr/pdb_x%d"
 
 
 #define FILENAME_X64 L"miranda64.exe"
