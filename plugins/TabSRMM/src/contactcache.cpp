2be2a04af85d58577b2cd7d5e67191208c7cf604
George Hazan <ghazan@miranda.im>
2017-08-22 17:20:54 +0300

    fixes #901 (Database corruption)

--- plugins/TabSRMM/src/contactcache.cpp
+++ plugins/TabSRMM/src/contactcache.cpp
@@ -383,7 +383,8 @@ void CContactCache::deletedHandler()
 	cc = &ccInvalid;
 	m_isValid = false;
 	if (m_dat)
-		::SendMessage(m_dat->GetHwnd(), WM_CLOSE, 1, 2);
+		// this message must be sent async to allow a contact to rest in peace before window gets closed
+		::PostMessage(m_dat->GetHwnd(), WM_CLOSE, 1, 2);
 
 	releaseAlloced();
 	m_hContact = INVALID_CONTACT_ID;
