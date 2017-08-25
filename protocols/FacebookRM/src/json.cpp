miranda-ng/miranda-ng

commit e04d28cfcfe33f0450b390bb1af6e9df823fc86a

Facebook: Ignore typing nofitications meant not for us (but e.g. for our page)

protocols/FacebookRM/src/json.cpp
@@ -783,12 +783,13 @@ int facebook_json_parser::parse_messages(std::string *pData, std::vector<faceboo
 		else if (t == "typ") { // revised 5.3.2017
 			// chat typing notification
 			const JSONNode &from_ = (*it)["from"]; // user fbid
-			//const JSONNode &to_ = (*it)["to"]; // user fbid (should be our own)
+			const JSONNode &to_ = (*it)["to"]; // user fbid (should be our own, but could be from our page too)
 			//const JSONNode &realtime_viewer_fbid_ = (*it)["realtime_viewer_fbid"]; // our user fbid
 			//const JSONNode &from_mobile_ = (*it)["from_mobile"]; // boolean // TODO: perhaps we should update user client based on this?
 			const JSONNode &st_ = (*it)["st"]; // typing status - 1 = started typing, 0 = stopped typing
 
-			if (!from_)
+			// Ignore wrong (without "from") typing notifications or that are not meant for us (but e.g. for our page)
+			if (!from_ || to_.as_string() == proto->facy.self_.user_id)
 				continue;
 
 			facebook_user fbu;

commit 8b737fdfa7bb85fd27c4c850515863e79a72edd9

Facebook: Fix receiving typing notifications (broken in e04d28cfcfe33f0450b390bb1af6e9df823fc86a)

protocols/FacebookRM/src/json.cpp
@@ -789,7 +789,7 @@ int facebook_json_parser::parse_messages(std::string *pData, std::vector<faceboo
 			const JSONNode &st_ = (*it)["st"]; // typing status - 1 = started typing, 0 = stopped typing
 
 			// Ignore wrong (without "from") typing notifications or that are not meant for us (but e.g. for our page)
-			if (!from_ || to_.as_string() == proto->facy.self_.user_id)
+			if (!from_ || to_.as_string() != proto->facy.self_.user_id)
 				continue;
 
 			facebook_user fbu;