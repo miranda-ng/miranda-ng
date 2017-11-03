87d1468cf0dd65d6c27d77984aaf7256a62cbbde
Goraf <22941576+Goraf@users.noreply.github.com>
2017-11-03 22:02:28 +0100

    ZeroNotification: fix displaying menu item on startup

--- plugins/ZeroNotification/src/main.cpp
+++ plugins/ZeroNotification/src/main.cpp
@@ -228,13 +228,13 @@ extern "C" __declspec(dllexport) int Load(void)
 		CreateServiceFunction(MODNAME "/MenuCommand", NoSoundMenuCommand);
 
 		CMenuItem mi;
+		SET_UID(mi, 0x6bd635eb, 0xc4bb, 0x413b, 0xb9, 0x3, 0x81, 0x6d, 0x8f, 0xf1, 0x9b, 0xb0);
 		mi.position = -0x7FFFFFFF;
 		mi.flags = CMIF_UNICODE;
-		UpdateMenuItem();
-
-		SET_UID(mi, 0x6bd635eb, 0xc4bb, 0x413b, 0xb9, 0x3, 0x81, 0x6d, 0x8f, 0xf1, 0x9b, 0xb0);
 		mi.pszService = MODNAME "/MenuCommand";
 		noSoundMenu = Menu_AddMainMenuItem(&mi);
+
+		UpdateMenuItem();
 	}
 
 	HookEvent(ME_PROTO_ACK, ProtoAck);
