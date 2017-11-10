5a3c6c39fa1cc3202a20e8c45a95fff9496d6205
George Hazan <ghazan@miranda.im>
2017-11-10 18:37:31 +0300

    fixes #1018 (libcurl doesn't have SSL support)

--- libs/libcurl/src/curl_setup.h
+++ libs/libcurl/src/curl_setup.h
@@ -36,49 +36,10 @@
  * configuration file for platforms which lack config tool.
  */
 
-#ifdef HAVE_CONFIG_H
+#include "config-win32.h"
 
-#include "curl_config.h"
-
-#else /* HAVE_CONFIG_H */
-
-#ifdef _WIN32_WCE
-#  include "config-win32ce.h"
-#else
-#  ifdef WIN32
-#    include "config-win32.h"
-#  endif
-#endif
-
-#if defined(macintosh) && defined(__MRC__)
-#  include "config-mac.h"
-#endif
-
-#ifdef __riscos__
-#  include "config-riscos.h"
-#endif
-
-#ifdef __AMIGA__
-#  include "config-amigaos.h"
-#endif
-
-#ifdef __SYMBIAN32__
-#  include "config-symbian.h"
-#endif
-
-#ifdef __OS400__
-#  include "config-os400.h"
-#endif
-
-#ifdef TPF
-#  include "config-tpf.h"
-#endif
-
-#ifdef __VXWORKS__
-#  include "config-vxworks.h"
-#endif
-
-#endif /* HAVE_CONFIG_H */
+#define USE_SSLEAY
+#define USE_OPENSSL
 
 /* ================================================================ */
 /* Definition of preprocessor macros/symbols which modify compiler  */
