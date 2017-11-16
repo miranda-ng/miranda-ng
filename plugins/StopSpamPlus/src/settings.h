c589cd1a36407cd2ea6e34eb229030b3bc9f6d94
dartraiden <wowemuh@gmail.com>
2017-11-16 22:56:48 +0300

    spelling correction

--- plugins/StopSpamPlus/src/settings.h
+++ plugins/StopSpamPlus/src/settings.h
@@ -64,7 +64,7 @@ public:
 	Settings():
 		Question("Question", TranslateW(L"Spammers made me to install small anti-spam system you are now speaking with. "
 			L"Please reply \"nospam\" without quotes and spaces if you want to contact me.")),
-		AuthRepl("AuthReply", TranslateW(L"StopSpam: send a message and reply to a anti-spam bot question.")),
+		AuthRepl("AuthReply", TranslateW(L"StopSpam: send a message and reply to an anti-spam bot question.")),
 		Answer("Answer", L"nospam"),
 		Congratulation("Congratulation", TranslateW(L"Congratulations! You just passed human/robot test. Now you can write me a message.")),
 		DisabledProtoList("DisabledProtoList", "MetaContacts RSSNews"),
