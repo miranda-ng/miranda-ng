6bb0abf1e62b18e4029ae0b7b73ee47f3fe3b39d
George Hazan <ghazan@miranda.im>
2017-11-23 21:14:42 +0300

    compilation fix

--- plugins/Db3x_mmap/src/stdafx.h
+++ plugins/Db3x_mmap/src/stdafx.h
@@ -25,6 +25,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include <windows.h>
 #include <wincred.h>
+#include <WinIoCtl.h>
+
 #include <time.h>
 #include <process.h>
 #include <memory>
