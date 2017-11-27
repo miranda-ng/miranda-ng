40030dfffc72c8a44b8d472b42ef983d0086e245
Kirill Volinsky <Mataes2007@gmail.com>
2017-11-27 16:11:26 +0300

    Tox: fix for copy dll and create pdb file

--- protocols/Tox/copydll.cmd
+++ protocols/Tox/copydll.cmd
@@ -1,5 +1,7 @@
-echo off
-cd /d ~dp%0
-copy /y bin\%1\libtox.* "%2"
-tools\cv2pdb.exe "%2\libtox.dll"
+@echo off
+set p1=%1
+set p2=%2
+set p3="%~2\libtox.dll"
+copy /y bin\%p1%\libtox.dll %p2%
+tools\cv2pdb.exe %p3%
 exit 0
