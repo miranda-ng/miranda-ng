5efeb0de659f76071673bc22ba5893c5c93bcf87
Kirill Volinsky <mataes2007@gmail.com>
2017-11-28 21:45:08 +0300

    mir_app: fix after conversion

--- src/mir_app/src/chat.h
+++ src/mir_app/src/chat.h
@@ -2,7 +2,7 @@
 
 Chat module plugin for Miranda IM
 
-Copyright (C) 2003 Jцrgen Persson
+Copyright (C) 2003 Jörgen Persson
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
