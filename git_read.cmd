dcdd37ce708c169c765200e96a2eab00e3ce7458
George Hazan <ghazan@miranda.im>
2017-12-20 16:38:04 +0300

    we need to refresh submodules as well

--- git_read.cmd
+++ git_read.cmd
@@ -1,5 +1,6 @@
 @echo off
 git pull --rebase=true --progress -v 2>&1 > "%temp%\report.txt"
+git submodule update --recursive --rebase
 call .\build\make_ver.bat
 start "" "%temp%\report.txt"
 goto :eof
