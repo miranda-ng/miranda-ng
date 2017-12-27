37e7f30e2f09dc5bd904a0b777147e2203cc3b1a
George Hazan <ghazan@miranda.im>
2017-12-27 23:57:33 +0300

    we need to compile boost with /Zc:threadSafeInit- compiler's option

--- tools/boost/build_boost.bat
+++ tools/boost/build_boost.bat
@@ -7,7 +7,13 @@ if  "%p2%" == "" (echo "please specify target msvc compiler by adding '10' for M
 call bootstrap.bat
 
 if /i '%1' == '86' (
-	b2 --toolset=msvc-%2.0 runtime-link=shared threading=multi link=static architecture=x86 address-model=32
+	call "%VS141COMNTOOLS%\..\..\VC\Auxiliary\Build\vcvars32.bat"
+	pause
+
+	b2 --toolset=msvc-%2.0 runtime-link=shared threading=multi link=static architecture=x86 address-model=32 cxxflags=/Zc:threadSafeInit-
 ) else if /i '%1' == '64' (
-	b2 --toolset=msvc-%2.0 runtime-link=shared threading=multi link=static architecture=x86 address-model=64 --stagedir=stage64
+	call "%VS141COMNTOOLS%\..\..\VC\Auxiliary\Build\vcvars64.bat"
+	pause
+
+	b2 --toolset=msvc-%2.0 runtime-link=shared threading=multi link=static architecture=x86 address-model=64 cxxflags=/Zc:threadSafeInit- --stagedir=stage64
 )
