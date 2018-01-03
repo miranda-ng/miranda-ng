ca1b61ff87246386e5aaa70d1ae4ee5e3641790c
dartraiden <wowemuh@gmail.com>
2018-01-03 16:43:28 +0300

    langpacks: update

---
+++ langpacks/belarusian/Refresh.bat
@@ -0,0 +1,15 @@
+@echo off
+
+REM ====== VARIABLES ========
+
+set "language=belarusian"
+set "oldLangpackPath="
+set "mirandaPath="
+
+REM =========================
+
+cd "..\..\tools\lpgen\"
+call refresher.bat %language% "%oldLangpackPath%"
+
+REM copy and reload langpack in your Miranda (uncoment next line + enable cmdline.dll plugin to use it)
+REM call installer.bat %language% "%mirandaPath%"
