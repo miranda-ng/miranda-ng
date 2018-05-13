2530c0baecb30ca4a97b5118c6ddab02318c7ca1
George Hazan <ghazan@miranda.im>
2018-05-13 20:55:00 +0300

    fixes #1354 completely (StdMsg and Scriver: add status text to window caption)

--- src/core/stdmsg/src/msgdialog.cpp
+++ src/core/stdmsg/src/msgdialog.cpp
@@ -647,8 +647,9 @@ void CSrmmWindow::UpdateTitle()
 	if (m_hContact && m_szProto) {
 		m_wStatus = db_get_w(m_hContact, m_szProto, "Status", ID_STATUS_OFFLINE);
 		wchar_t *contactName = Clist_GetContactDisplayName(m_hContact);
+		wchar_t *szStatus = Clist_GetStatusModeDescription(m_wStatus, 0);
+		mir_snwprintf(newtitle, L"%s (%s): %s", contactName, szStatus, TranslateT("Message session"));
 
-		mir_snwprintf(newtitle, L"%s - %s", contactName, TranslateT("Message session"));
 		m_wOldStatus = m_wStatus;
 	}
 	else mir_wstrncpy(newtitle, TranslateT("Message session"), _countof(newtitle));
