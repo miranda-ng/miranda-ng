miranda-ng/miranda-ng

commit 8a74e7495ce5ad39de4f5c25121a84d35df90c36

CMPlugin to receive a reference to PLUGININFOEX

plugins/AutoShutdown/src/frame.cpp
@@ -288,14 +288,14 @@ static LRESULT CALLBACK FrameWndProc(HWND hwndFrame, UINT msg, WPARAM wParam, LP
 		}
 	case M_SET_COUNTDOWN:
 		if (dat->fTimeFlags&SDWTF_ST_TIME) {
-			dat->settingLastTime = (time_t)db_get_dw(NULL, "AutoShutdown", "TimeStamp", SETTING_TIMESTAMP_DEFAULT);
+			dat->settingLastTime = (time_t)db_get_dw(NULL, MODULENAME, "TimeStamp", SETTING_TIMESTAMP_DEFAULT);
 			dat->countdown = time(0);
 			if (dat->settingLastTime > dat->countdown) dat->countdown = dat->settingLastTime - dat->countdown;
 			else dat->countdown = 0;
 		}
 		else if (dat->flags&FWPDF_COUNTDOWNINVALID) {
-			dat->countdown = (time_t)db_get_dw(NULL, "AutoShutdown", "Countdown", SETTING_COUNTDOWN_DEFAULT);
-			dat->countdown *= (time_t)db_get_dw(NULL, "AutoShutdown", "CountdownUnit", SETTING_COUNTDOWNUNIT_DEFAULT);
+			dat->countdown = (time_t)db_get_dw(NULL, MODULENAME, "Countdown", SETTING_COUNTDOWN_DEFAULT);
+			dat->countdown *= (time_t)db_get_dw(NULL, MODULENAME, "CountdownUnit", SETTING_COUNTDOWNUNIT_DEFAULT);
 		}
 		dat->flags &= ~FWPDF_COUNTDOWNINVALID;
 		/* commctl 4.70+, Win95: 1-100 will work fine (wrap around) */
@@ -499,7 +499,7 @@ void ShowCountdownFrame(WORD fTimeFlags)
 		clf.align = alBottom;
 		clf.height = GetSystemMetrics(SM_CYICON);
 		clf.Flags = F_VISIBLE | F_SHOWTBTIP | F_NOBORDER | F_SKINNED;
-		clf.name = Translate("AutoShutdown");
+		clf.name = Translate(MODULENAME);
 		clf.TBname = Translate("Automatic shutdown");
 		clf.hWnd = hwndCountdownFrame;
 		hFrame = (WORD)CallService(MS_CLIST_FRAMES_ADDFRAME, (WPARAM)&clf, 0);
@@ -553,13 +553,13 @@ static int FrameModulesLoaded(WPARAM, LPARAM)
 		LOGFONT lf;
 		/* built-in font module is not available before this hook */
 		COLORREF clr = GetDefaultColor(FRAMEELEMENT_TEXT);
-		FontService_RegisterFont("AutoShutdown", "CountdownFont", LPGENW("Automatic shutdown"), LPGENW("Countdown on frame"), LPGENW("Automatic shutdown"), LPGENW("Background"), 0, FALSE, GetDefaultFont(&lf), clr);
+		FontService_RegisterFont(MODULENAME, "CountdownFont", LPGENW("Automatic shutdown"), LPGENW("Countdown on frame"), LPGENW("Automatic shutdown"), LPGENW("Background"), 0, FALSE, GetDefaultFont(&lf), clr);
 		clr = GetDefaultColor(FRAMEELEMENT_BKGRND);
-		FontService_RegisterColor("AutoShutdown", "BkgColor", LPGENW("Automatic shutdown"), LPGENW("Background"), clr);
+		FontService_RegisterColor(MODULENAME, "BkgColor", LPGENW("Automatic shutdown"), LPGENW("Background"), clr);
 		if (!IsThemeActive()) {
 			/* progressbar color can only be changed with classic theme */
 			clr = GetDefaultColor(FRAMEELEMENT_BAR);
-			FontService_RegisterColor("AutoShutdown", "ProgressColor", TranslateT("Automatic shutdown"), TranslateT("Progress bar"), clr);
+			FontService_RegisterColor(MODULENAME, "ProgressColor", TranslateT("Automatic shutdown"), TranslateT("Progress bar"), clr);
 		}
 	}
 	return 0;

plugins/Alarms/src/frame.cpp
@@ -46,13 +46,13 @@ LRESULT CALLBACK FrameContainerWindowProc(HWND hwnd, UINT msg, WPARAM wParam, LP
 
 	case WM_SHOWWINDOW:
 		if (wParam) {
-			db_set_b(0, MODULE, "ReminderFrameVisible", 1);
-			Utils_RestoreWindowPosition(hwnd, 0, MODULE, "reminders_window");
+			db_set_b(0, MODULENAME, "ReminderFrameVisible", 1);
+			Utils_RestoreWindowPosition(hwnd, 0, MODULENAME, "reminders_window");
 			PostMessage(hwnd, WM_SIZE, 0, 0);
 		}
 		else {
-			db_set_b(0, MODULE, "ReminderFrameVisible", 0);
-			Utils_SaveWindowPosition(hwnd, 0, MODULE, "reminders_window");
+			db_set_b(0, MODULENAME, "ReminderFrameVisible", 0);
+			Utils_SaveWindowPosition(hwnd, 0, MODULENAME, "reminders_window");
 		}
 		break;
 	case WM_SIZE:
@@ -68,7 +68,7 @@ LRESULT CALLBACK FrameContainerWindowProc(HWND hwnd, UINT msg, WPARAM wParam, LP
 		break;
 
 	case WM_CLOSE:
-		Utils_SaveWindowPosition(hwnd, 0, MODULE, "reminders_window");
+		Utils_SaveWindowPosition(hwnd, 0, MODULENAME, "reminders_window");
 		ShowWindow(hwnd, SW_HIDE);
 		FixMainMenu();
 		return TRUE;
@@ -519,7 +519,7 @@ int CreateFrame()
 
 		///////////////////////
 		// create menu item
-		CreateServiceFunction(MODULE "/ShowHideReminders", ShowHideMenuFunc);
+		CreateServiceFunction(MODULENAME "/ShowHideReminders", ShowHideMenuFunc);
 
 		CMenuItem mi;
 		mi.root = Menu_CreateRoot(MO_MAIN, LPGENW("Alarms"), 0);
@@ -528,7 +528,7 @@ int CreateFrame()
 		SET_UID(mi, 0x27556ea9, 0xfa19, 0x4c2e, 0xb0, 0xc9, 0x48, 0x2, 0x5c, 0x17, 0xba, 0x5);
 		mi.hIcolibItem = hIconMenuShowHide;
 		mi.name.a = LPGEN("Show reminders");
-		mi.pszService = MODULE "/ShowHideReminders";
+		mi.pszService = MODULENAME "/ShowHideReminders";
 		mi.position = 500010000;
 		hMenuShowReminders = Menu_AddMainMenuItem(&mi);
 		/////////////////////
@@ -542,7 +542,7 @@ int CreateFrame()
 				else ShowWindow(hwnd_frame, SW_HIDE);
 			}
 			else {
-				if (db_get_b(0, MODULE, "ReminderFrameVisible", 1) == 1) {
+				if (db_get_b(0, MODULENAME, "ReminderFrameVisible", 1) == 1) {
 					ShowWindow(hwnd_frame, SW_SHOW);
 					RefreshReminderFrame();
 				}
@@ -558,13 +558,13 @@ int CreateFrame()
 	font_id.cbSize = sizeof(font_id);
 	strncpy_s(font_id.group, LPGEN("Frames"), _TRUNCATE);
 	strncpy_s(font_id.name, LPGEN("Alarm reminders"), _TRUNCATE);
-	strncpy_s(font_id.dbSettingsGroup, MODULE, _TRUNCATE);
+	strncpy_s(font_id.dbSettingsGroup, MODULENAME, _TRUNCATE);
 	strncpy_s(font_id.prefix, "Font", _TRUNCATE);
 	font_id.order = 0;
 	Font_Register(&font_id);
 
 	framebk_colour_id.cbSize = sizeof(ColourID);
-	strncpy_s(framebk_colour_id.dbSettingsGroup, MODULE, _TRUNCATE);
+	strncpy_s(framebk_colour_id.dbSettingsGroup, MODULENAME, _TRUNCATE);
 	strncpy_s(framebk_colour_id.group, LPGEN("Frames"), _TRUNCATE);
 	strncpy_s(framebk_colour_id.name, LPGEN("Alarm reminders"), _TRUNCATE);
 	strncpy_s(framebk_colour_id.setting, "clFrameBack", _TRUNCATE);

plugins/MyDetails/src/frame.cpp
@@ -74,8 +74,8 @@ BYTE font_styles[] = { DBFONTF_BOLD, 0, 0, DBFONTF_ITALIC, DBFONTF_ITALIC };
 COLORREF font_colors[] = { RGB(0, 0, 0), RGB(0, 0, 0), RGB(0, 0, 0), RGB(150, 150, 150), RGB(150, 150, 150) };
 
 static ColourID
-bg_colour = { sizeof(bg_colour), LPGEN("My details"), LPGEN("Background"), MODULE_NAME, "BackgroundColor", 0, GetSysColor(COLOR_BTNFACE) },
-av_colour = { sizeof(av_colour), LPGEN("My details"), LPGEN("Avatar border"), MODULE_NAME, "AvatarBorderColor", 0, RGB(0, 0, 0) };
+bg_colour = { sizeof(bg_colour), LPGEN("My details"), LPGEN("Background"), MODULENAME, "BackgroundColor", 0, GetSysColor(COLOR_BTNFACE) },
+av_colour = { sizeof(av_colour), LPGEN("My details"), LPGEN("Avatar border"), MODULENAME, "AvatarBorderColor", 0, RGB(0, 0, 0) };
 
 int CreateFrame();
 void FixMainMenu();
@@ -236,7 +236,7 @@ int CreateFrame()
 		font_id[i].cbSize = sizeof(FontIDW);
 		mir_wstrncpy(font_id[i].group, LPGENW("My details"), _countof(font_id[i].group));
 		mir_wstrncpy(font_id[i].name, font_names[i], _countof(font_id[i].name));
-		mir_strncpy(font_id[i].dbSettingsGroup, MODULE_NAME, _countof(font_id[i].dbSettingsGroup));
+		mir_strncpy(font_id[i].dbSettingsGroup, MODULENAME, _countof(font_id[i].dbSettingsGroup));
 		mir_wstrncpy(font_id[i].backgroundName, LPGENW("Background"), _countof(font_id[i].backgroundName));
 		mir_wstrncpy(font_id[i].backgroundGroup, LPGENW("My details"), _countof(font_id[i].backgroundGroup));
 
@@ -336,11 +336,11 @@ int CreateFrame()
 		mi.position = 1;
 		mi.hIcolibItem = Skin_LoadIcon(SKINICON_OTHER_USERDETAILS);
 		mi.name.w = LPGENW("Show my details");
-		mi.pszService = MODULE_NAME"/ShowHideMyDetails";
+		mi.pszService = MODULENAME"/ShowHideMyDetails";
 		hMenuShowHideFrame = Menu_AddMainMenuItem(&mi);
 		Menu_ConfigureItem(hMenuShowHideFrame, MCI_OPT_EXECPARAM, -0x7FFFFFFF);
 
-		if (db_get_b(0, MODULE_NAME, SETTING_FRAME_VISIBLE, 1) == 1) {
+		if (db_get_b(0, MODULENAME, SETTING_FRAME_VISIBLE, 1) == 1) {
 			ShowWindow(hwnd_container, SW_SHOW);
 			FixMainMenu();
 		}
@@ -365,9 +365,9 @@ LRESULT CALLBACK FrameContainerWindowProc(HWND hwnd, UINT msg, WPARAM wParam, LP
 	switch (msg) {
 	case WM_SHOWWINDOW:
 		if ((BOOL)wParam)
-			Utils_RestoreWindowPosition(hwnd, 0, MODULE_NAME, WINDOW_NAME_PREFIX);
+			Utils_RestoreWindowPosition(hwnd, 0, MODULENAME, WINDOW_NAME_PREFIX);
 		else
-			Utils_SaveWindowPosition(hwnd, 0, MODULE_NAME, WINDOW_NAME_PREFIX);
+			Utils_SaveWindowPosition(hwnd, 0, MODULENAME, WINDOW_NAME_PREFIX);
 		break;
 
 	case WM_ERASEBKGND:
@@ -386,7 +386,7 @@ LRESULT CALLBACK FrameContainerWindowProc(HWND hwnd, UINT msg, WPARAM wParam, LP
 		return TRUE;
 
 	case WM_CLOSE:
-		db_set_b(0, MODULE_NAME, SETTING_FRAME_VISIBLE, 0);
+		db_set_b(0, MODULENAME, SETTING_FRAME_VISIBLE, 0);
 		ShowWindow(hwnd, SW_HIDE);
 		FixMainMenu();
 		return TRUE;
@@ -2186,7 +2186,7 @@ INT_PTR ShowHideFrameFunc(WPARAM, LPARAM)
 			SendMessage(hwnd_container, WM_CLOSE, 0, 0);
 		else {
 			ShowWindow(hwnd_container, SW_SHOW);
-			db_set_b(0, MODULE_NAME, SETTING_FRAME_VISIBLE, 1);
+			db_set_b(0, MODULENAME, SETTING_FRAME_VISIBLE, 1);
 		}
 
 		FixMainMenu();
@@ -2204,7 +2204,7 @@ INT_PTR ShowFrameFunc(WPARAM, LPARAM)
 	else {
 		if (!MyDetailsFrameVisible()) {
 			ShowWindow(hwnd_container, SW_SHOW);
-			db_set_b(0, MODULE_NAME, SETTING_FRAME_VISIBLE, 1);
+			db_set_b(0, MODULENAME, SETTING_FRAME_VISIBLE, 1);
 
 			FixMainMenu();
 		}

commit c081ab4395c72e319399c01e1ded10e1b8929425

AutoShutdown: fix regression introduced in https://github.com/miranda-ng/miranda-ng/commit/8a74e7495ce5ad39de4f5c25121a84d35df90c36

plugins/AutoShutdown/src/frame.cpp
@@ -21,6 +21,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "stdafx.h"
 
+#define MODULENAME LPGEN("AutoShutdown")
+
 /* Show Frame */
 static HWND hwndCountdownFrame;
 static WORD hFrame;
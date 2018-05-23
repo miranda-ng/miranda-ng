c081ab4395c72e319399c01e1ded10e1b8929425
dartraiden <wowemuh@gmail.com>
2018-05-23 18:13:26 +0300

    AutoShutdown: fix regression introduced in https://github.com/miranda-ng/miranda-ng/commit/8a74e7495ce5ad39de4f5c25121a84d35df90c36

--- plugins/AutoShutdown/src/frame.cpp
+++ plugins/AutoShutdown/src/frame.cpp
@@ -21,6 +21,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "stdafx.h"
 
+#define MODULENAME LPGEN("AutoShutdown")
+
 /* Show Frame */
 static HWND hwndCountdownFrame;
 static WORD hFrame;
