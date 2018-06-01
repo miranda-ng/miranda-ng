2d9052bcd2250ec9ce38d2c9fc92ebb918263b14
George Hazan <ghazan@miranda.im>
2018-06-01 19:04:08 +0300

    these binaries should be compiled with static runtime

--- plugins/NotifyAnything/SendLog/compile64.bat
+++ plugins/NotifyAnything/SendLog/compile64.bat
@@ -1,7 +1,7 @@
-@echo off
+rem @echo off
 
-cl.exe /MD /O2 /Os /EHsc SendLog.cpp SLImp.cpp /link ws2_32.lib user32.lib
-cl.exe /MD /O2 /Os /EHsc SendLogWin.cpp SLImp.cpp /link ws2_32.lib user32.lib
+cl.exe /MT /O2 /Os /EHsc /I..\..\..\include SendLog.cpp SLImp.cpp /link ws2_32.lib user32.lib
+cl.exe /MT /O2 /Os /EHsc  /I..\..\..\include SendLogWin.cpp SLImp.cpp /link ws2_32.lib user32.lib
 
 mkdir ..\..\..\%1\Release64 2>nul
 copy /Y *.exe ..\..\..\%1\Release64 >nul
