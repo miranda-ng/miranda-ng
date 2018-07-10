miranda-ng/miranda-ng

commit dae1e12d0bc0be3b6c243dc624b30e1ed3b07410

liblua updated to version 5.3.5

libs/liblua/src/lstate.c
@@ -339,9 +339,6 @@ LUA_API lua_State *lua_newstate (lua_Alloc f, void *ud) {
 
 
 LUA_API void lua_close (lua_State *L) {
-  if (!L)
-	 return;
-
   L = G(L)->mainthread;  /* only the main thread can be closed */
   lua_lock(L);
   close_state(L);

libs/liblua/docs/index.css
@@ -1,0 +1,21 @@
+ul {
+	list-style-type: none ;
+}
+
+ul.contents {
+	padding: 0 ;
+}
+
+table {
+	border: none ;
+	border-spacing: 0 ;
+	border-collapse: collapse ;
+}
+
+td {
+	vertical-align: top ;
+	padding: 0 ;
+	text-align: left ;
+	line-height: 1.25 ;
+	width: 15% ;
+}

libs/liblua/docs/manual.css
@@ -1,0 +1,21 @@
+h3 code {
+	font-family: inherit ;
+	font-size: inherit ;
+}
+
+pre, code {
+	font-size: 12pt ;
+}
+
+span.apii {
+	color: gray ;
+	float: right ;
+	font-family: inherit ;
+	font-style: normal ;
+	font-size: small ;
+}
+
+h2:before {
+	content: "" ;
+	padding-right: 0em ;
+}

commit 1886d6594c2c45208fca9bb9537185eb63e94df4

fix regression, introduced in dae1e12d0bc0be3b6c243dc624b30e1ed3b07410

libs/liblua/src/lstate.c
@@ -339,6 +339,9 @@ LUA_API lua_State *lua_newstate (lua_Alloc f, void *ud) {
 
 
 LUA_API void lua_close (lua_State *L) {
+  if (!L)
+	 return;
+
   L = G(L)->mainthread;  /* only the main thread can be closed */
   lua_lock(L);
   close_state(L);