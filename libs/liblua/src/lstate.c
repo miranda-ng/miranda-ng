dae1e12d0bc0be3b6c243dc624b30e1ed3b07410
aunsane <aunsane@gmail.com>
2018-07-10 21:40:09 +0300

    liblua updated to version 5.3.5

--- libs/liblua/src/lstate.c
+++ libs/liblua/src/lstate.c
@@ -339,9 +339,6 @@ LUA_API lua_State *lua_newstate (lua_Alloc f, void *ud) {
 
 
 LUA_API void lua_close (lua_State *L) {
-  if (!L)
-	 return;
-
   L = G(L)->mainthread;  /* only the main thread can be closed */
   lua_lock(L);
   close_state(L);
