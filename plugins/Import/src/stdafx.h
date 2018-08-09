e7d6c4e5d1081049511e07c91f57e0263b8a25c5
aunsane <aunsane@gmail.com>
2018-08-09 22:24:03 +0300

    Import: fix build

--- plugins/Import/src/stdafx.h
+++ plugins/Import/src/stdafx.h
@@ -30,6 +30,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <malloc.h>
 #include <time.h>
 
+#include <memory>
+
 #include <win2k.h>
 #include <newpluginapi.h>
 #include <m_langpack.h>
