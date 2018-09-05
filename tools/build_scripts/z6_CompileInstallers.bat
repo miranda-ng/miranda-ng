484a6d3c8e5caacf5b3e292cfb36b403d0fcbc22
George Hazan <ghazan@miranda.im>
2018-09-05 13:16:46 +0300

    separate setting for the NSIS path

--- tools/build_scripts/z6_CompileInstallers.bat
+++ tools/build_scripts/z6_CompileInstallers.bat
@@ -8,8 +8,8 @@ copy /V /Y ..\..\build\build.no tmp\build.no
 pushd tmp
 for /F "tokens=1,2,3 delims= " %%i in (build.no) do set MirVer=%%i.%%j.%%k
 popd
-"%ProgramFiles(x86)%\Inno Setup 5\ISCC.exe" /Dptx86 /DAppVer=%MirVer% /O"Output" "InnoNG_32\MirandaNG.iss"
-"%ProgramFiles(x86)%\Inno Setup 5\ISCC.exe" /DAppVer=%MirVer% /O"Output" "InnoNG_64\MirandaNG.iss"
+%ISCC% /Dptx86 /DAppVer=%MirVer% /O"Output" "InnoNG_32\MirandaNG.iss"
+%ISCC% /DAppVer=%MirVer% /O"Output" "InnoNG_64\MirandaNG.iss"
 call cleanup.bat
 copy /V /Y Output\miranda-ng-alpha-latest*.exe "%ArchDistr%\"
 rd /S /Q Output
