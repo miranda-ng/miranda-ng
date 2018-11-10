68fa25f827855c869c730327b79acfc1ef5576b1
George Hazan <ghazan@miranda.im>
2018-11-10 17:26:55 +0300

    keeping copy of string is safer that saving pointer to it (might be damaged by langpack restart)

--- plugins/Clist_modern/src/modern_clistsettings.cpp
+++ plugins/Clist_modern/src/modern_clistsettings.cpp
@@ -24,7 +24,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "stdafx.h"
 
-wchar_t* UnknownConctactTranslatedName = nullptr;
+CMStringW UnknownConctactTranslatedName;
 
 void cliFreeCacheItem(ClcCacheEntry *p)
 {
