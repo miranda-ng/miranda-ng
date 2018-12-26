a9ffe609a8f6af668eebda668efd8e04a8989ef6
George Hazan <ghazan@miranda.im>
2018-12-26 23:02:50 +0300

    - icqoscar8 moved to deprecated;
    - Icq10 is used now as a ICQ protocol;
    - solutions updated

---
+++ protocols/Icq10/icq_xstatus/src/resource.h
@@ -0,0 +1,103 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by resources.rc
+//
+#define IDI_XSTATUS1                    201
+#define IDI_XSTATUS2                    202
+#define IDI_XSTATUS3                    203
+#define IDI_XSTATUS4                    204
+#define IDI_XSTATUS5                    205
+#define IDI_XSTATUS6                    206
+#define IDI_XSTATUS7                    207
+#define IDI_XSTATUS8                    208
+#define IDI_XSTATUS9                    209
+#define IDI_XSTATUS10                   210
+#define IDI_XSTATUS11                   211
+#define IDI_XSTATUS12                   212
+#define IDI_XSTATUS13                   213
+#define IDI_XSTATUS14                   214
+#define IDI_XSTATUS15                   215
+#define IDI_XSTATUS16                   216
+#define IDI_XSTATUS17                   217
+#define IDI_XSTATUS18                   218
+#define IDI_XSTATUS19                   219
+#define IDI_XSTATUS20                   220
+#define IDI_XSTATUS21                   221
+#define IDI_XSTATUS22                   222
+#define IDI_XSTATUS23                   223
+#define IDI_XSTATUS24                   224
+#define IDI_XSTATUS25                   225
+#define IDI_XSTATUS26                   226
+#define IDI_XSTATUS27                   227
+#define IDI_XSTATUS28                   228
+#define IDI_XSTATUS29                   229
+#define IDI_XSTATUS30                   230
+#define IDI_XSTATUS31                   231
+#define IDI_XSTATUS32                   232
+#define IDI_XSTATUS33                   233
+#define IDI_XSTATUS34                   234
+#define IDI_XSTATUS35                   235
+#define IDI_XSTATUS36                   236
+#define IDI_XSTATUS37                   237
+#define IDI_XSTATUS38                   238
+#define IDI_XSTATUS39                   239
+#define IDI_XSTATUS40                   240
+#define IDI_XSTATUS41                   241
+#define IDI_XSTATUS42                   242
+#define IDI_XSTATUS43                   243
+#define IDI_XSTATUS44                   244
+#define IDI_XSTATUS45                   245
+#define IDI_XSTATUS46                   246
+#define IDI_XSTATUS47                   247
+#define IDI_XSTATUS48                   248
+#define IDI_XSTATUS49                   249
+#define IDI_XSTATUS50                   250
+#define IDI_XSTATUS51                   251
+#define IDI_XSTATUS52                   252
+#define IDI_XSTATUS53                   253
+#define IDI_XSTATUS54                   254
+#define IDI_XSTATUS55                   255
+#define IDI_XSTATUS56                   256
+#define IDI_XSTATUS57                   257
+#define IDI_XSTATUS58                   258
+#define IDI_XSTATUS59                   259
+#define IDI_XSTATUS60                   260
+#define IDI_XSTATUS61                   261
+#define IDI_XSTATUS62                   262
+#define IDI_XSTATUS63                   263
+#define IDI_XSTATUS64                   264
+#define IDI_XSTATUS65                   265
+#define IDI_XSTATUS66                   266
+#define IDI_XSTATUS67                   267
+#define IDI_XSTATUS68                   268
+#define IDI_XSTATUS69                   269
+#define IDI_XSTATUS70                   270
+#define IDI_XSTATUS71                   271
+#define IDI_XSTATUS72                   272
+#define IDI_XSTATUS73                   273
+#define IDI_XSTATUS74                   274
+#define IDI_XSTATUS75                   275
+#define IDI_XSTATUS76                   276
+#define IDI_XSTATUS77                   277
+#define IDI_XSTATUS78                   278
+#define IDI_XSTATUS79                   279
+#define IDI_XSTATUS80                   280
+#define IDI_XSTATUS81                   281
+#define IDI_XSTATUS82                   282
+#define IDI_XSTATUS83                   283
+#define IDI_XSTATUS84                   284
+#define IDI_XSTATUS85                   285
+#define IDI_XSTATUS86                   286
+
+#define IDS_IDENTIFY                    102
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        113
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1026
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
