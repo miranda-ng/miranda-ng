73b8de6d32ee10e80f1905e07ce5674de54f2571
ElzorFox <elzorfox@ya.ru>
2019-01-18 13:31:50 +0500

    cosmetic

--- plugins/ExternalAPI/m_seenplugin.h
+++ plugins/ExternalAPI/m_seenplugin.h
@@ -4,7 +4,7 @@
 * Get last seen string
 * WPARAM = hContact
 * LPARAM = NULL
-* returns a WCHAR* - containg the parsed last seen time. This need to be free using miranda free.
+* returns a LPWSTR - containg the parsed last seen time (as menu item option). This need to be free using miranda free.
 */
 
 #define MS_LASTSEEN_GET "Lastseen/Get"
