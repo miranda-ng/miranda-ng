34f7b71e7f52fe9a8368ed3ec182d3a1a0d48225
George Hazan <ghazan@miranda.im>
2019-02-11 19:23:19 +0300

    unused xml api items removed

--- include/m_xml.h
+++ include/m_xml.h
@@ -80,7 +80,6 @@ EXTERN_C MIR_APP_DLL(int)     xmlGetChildCount(HXML);
 EXTERN_C MIR_APP_DLL(HXML)    xmlGetChildByAttrValue(HXML parent, LPCTSTR name, LPCTSTR attrName, LPCTSTR attrValue);
 EXTERN_C MIR_APP_DLL(HXML)    xmlGetFirstChild(HXML parent);
 EXTERN_C MIR_APP_DLL(HXML)    xmlGetNthChild(HXML parent, LPCTSTR name, int i);
-EXTERN_C MIR_APP_DLL(HXML)    xmlGetNextChild(HXML parent, LPCTSTR name, int *i);
 EXTERN_C MIR_APP_DLL(HXML)    xmlGetChildByPath(HXML parent, LPCTSTR path, char createNodeIfMissing);
 EXTERN_C MIR_APP_DLL(HXML)    xmlGetNextNode(HXML node);
 EXTERN_C MIR_APP_DLL(LPCTSTR) xmlGetName(HXML);
@@ -98,37 +97,9 @@ EXTERN_C MIR_APP_DLL(void)    xmlAddAttrInt(HXML, LPCTSTR attrName, int attrValu
 EXTERN_C MIR_APP_DLL(void)    xmlFree(void*);
 
 // methods added in xml API v2
-EXTERN_C MIR_APP_DLL(char)    xmlIsDeclaration(HXML);
-EXTERN_C MIR_APP_DLL(LPTSTR)  xmlToStringWithFormatting(HXML node, int* datalen);
-EXTERN_C MIR_APP_DLL(HXML)    xmlDeepCopy(HXML);
-EXTERN_C MIR_APP_DLL(void)    xmlSetAttrByIndex(HXML, int i, LPCTSTR value);
-EXTERN_C MIR_APP_DLL(void)    xmlSetAttrByName(HXML, LPCTSTR name, LPCTSTR value);
-EXTERN_C MIR_APP_DLL(HXML)    xmlAddChildEx(HXML parent, LPCTSTR name, char isDeclaration, XML_ELEMENT_POS n);
-EXTERN_C MIR_APP_DLL(void)    xmlAddChildEx2(HXML child, HXML parent, XML_ELEMENT_POS n);
-EXTERN_C MIR_APP_DLL(int)     xmlGetTextCount(HXML);
-EXTERN_C MIR_APP_DLL(LPCTSTR) xmlGetTextByIndex(HXML, int i);
-EXTERN_C MIR_APP_DLL(void)    xmlAddText(HXML, LPCTSTR, XML_ELEMENT_POS n);
-EXTERN_C MIR_APP_DLL(void)    xmlSetTextByIndex(HXML, int i, LPCTSTR);
-EXTERN_C MIR_APP_DLL(int)     xmlGetClearCount(HXML);
 EXTERN_C MIR_APP_DLL(LPCTSTR) xmlGetClear(HXML, int i, LPCTSTR *openTag, LPCTSTR *closeTag);
-EXTERN_C MIR_APP_DLL(void)    xmlAddClear(HXML, LPCTSTR lpszValue, LPCTSTR openTag, LPCTSTR closeTag, XML_ELEMENT_POS n);
-EXTERN_C MIR_APP_DLL(void)    xmlSetClear(HXML, int i, LPCTSTR lpszValue);
-EXTERN_C MIR_APP_DLL(int)     xmlGetElementCount(HXML);
-EXTERN_C MIR_APP_DLL(int)     xmlGetElement(HXML, XML_ELEMENT_POS n, XML_ELEMENT_TYPE *type, HXML *child, LPCTSTR *value, LPCTSTR *name, LPCTSTR *openTag, LPCTSTR *closeTag);
-
-EXTERN_C MIR_APP_DLL(void)    xmlDeleteNodeContent(HXML); // forces the deletion of the content of this node and the subtree
-EXTERN_C MIR_APP_DLL(void)    xmlDeleteAttrByIndex(HXML, int i);
-EXTERN_C MIR_APP_DLL(void)    xmlDeleteAttrByName(HXML, LPCTSTR name);
-EXTERN_C MIR_APP_DLL(void)    xmlDeleteText(HXML, int i);
-EXTERN_C MIR_APP_DLL(void)    xmlDeleteClear(HXML, int i);
 
 EXTERN_C MIR_APP_DLL(HXML)     xmlParseFile(LPCTSTR filename, int *datalen, LPCTSTR tag);
 EXTERN_C MIR_APP_DLL(XMLError) xmlToFile(HXML node, LPCTSTR filename, int withformattiing);
-
-EXTERN_C MIR_APP_DLL(XML_ELEMENT_POS) xmlPositionOfChildByIndex(HXML, int i);
-EXTERN_C MIR_APP_DLL(XML_ELEMENT_POS) xmlPositionOfChildByNode(HXML, HXML);
-EXTERN_C MIR_APP_DLL(XML_ELEMENT_POS) xmlPositionOfChildByName(HXML, LPCTSTR name, int i);
-EXTERN_C MIR_APP_DLL(XML_ELEMENT_POS) xmlPositionOfText(HXML, int i);
-EXTERN_C MIR_APP_DLL(XML_ELEMENT_POS) xmlPositionOfClear(HXML, int i);
 
 #endif // M_XML_H__
