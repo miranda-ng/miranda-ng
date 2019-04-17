577c70da18d8fac21b0fd3581def6c44abd8761b
George Hazan <ghazan@miranda.im>
2019-04-17 12:10:29 +0300

    fix for building boost under VS2019

--- tools/boost/build_boost.bat
+++ tools/boost/build_boost.bat
@@ -6,10 +6,16 @@ if  "%p2%" == "" (echo "please specify target msvc compiler by adding '14.1' for
 
 call bootstrap.bat
 
+if "%p2%" == "14.1" (
+   set VCRoot=%VS141COMNTOOLS%
+) else (
+   set VCRoot=%VS142COMNTOOLS%
+)
+
 if /i '%1' == '86' (
-	call "%VS141COMNTOOLS%\..\..\VC\Auxiliary\Build\vcvars32.bat"
+	call "%VCRoot%\..\..\VC\Auxiliary\Build\vcvars32.bat"
 	b2 --toolset=msvc-%2 runtime-link=shared threading=multi link=static architecture=x86 address-model=32 cxxflags=/Zc:threadSafeInit-
 ) else if /i '%1' == '64' (
-	call "%VS141COMNTOOLS%\..\..\VC\Auxiliary\Build\vcvars64.bat"
+	call "%VCRoot%\..\..\VC\Auxiliary\Build\vcvars64.bat"
 	b2 --toolset=msvc-%2 runtime-link=shared threading=multi link=static architecture=x86 address-model=64 cxxflags=/Zc:threadSafeInit- --stagedir=stage64
 )
