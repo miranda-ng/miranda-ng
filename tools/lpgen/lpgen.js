c978ed512327913c63a384c084920e2db0ff2f51
George Hazan <ghazan@miranda.im>
2019-07-24 17:34:29 +0300

    lgpen.js: added support for TranslateU / TranslateU_LP

--- tools/lpgen/lpgen.js
+++ tools/lpgen/lpgen.js
@@ -523,7 +523,7 @@ function ParseSourceFile(FileTextVar, array) {
     //not store ?: functions LPGEN or LPGENT? or Translate(T or W) or _T, than any unnecessary space \s, than not stored ?: "(" followed by ' or " (stored and used as \1) than \S\s - magic with multiline capture, ending with not stored ?= \1 (we get " or ' after "("), than none or few spaces \x20 followed by )/m=multiline g=global
     //var find= /(?:LPGEN[TW]?|Translate[TW]?||Translate[AW]_LP|_T)(?:\s*?\(\s*?L?\s*)(['"])([\S\s]*?)(?=\1,?\x20*?(?:tmp)?\))/mg;
     //comment previous line and uncomment following line to output templates without _T() function in source files. Too many garbage from _T()..
-    var find = /(?:LPGEN[TW]?|Translate[TW]?|Translate[AW]_LP)(?:\s*?\(\s*?L?\s*)((?:(?:"[^"\\]*(?:\\[\S\s][^"\\]*)*")\s*)*)(?:\s*?,?\s*?(?:tmp)?\))/gm;
+    var find = /(?:LPGEN[TW]?|Translate[TUW]?|Translate[AUW]_LP)(?:\s*?\(\s*?L?\s*)((?:(?:"[^"\\]*(?:\\[\S\s][^"\\]*)*")\s*)*)(?:\s*?,?\s*?(?:tmp)?\))/gm;
     //now make a job, till end of matching regexp
     while ((string = find.exec(FileTextVar)) !== null) {
         //first, init empty var
