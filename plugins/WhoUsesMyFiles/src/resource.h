eeab4132bd82006bcdf078631515227324855414
George Hazan <ghazan@miranda.im>
2019-07-29 17:55:08 +0300

    fixes #2019 (WUMF: delete non-existing settings)

--- plugins/WhoUsesMyFiles/src/resource.h
+++ plugins/WhoUsesMyFiles/src/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by ..\res\resource.rc
+// Used by w:\miranda-ng\plugins\WhoUsesMyFiles\res\resource.rc
 //
 #define IDD_OPTIONS                     101
 #define IDI_DRIVE                       106
@@ -34,7 +34,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_3D_CONTROLS                     1
+#define _APS_3D_CONTROLS                1
 #define _APS_NEXT_RESOURCE_VALUE        109
 #define _APS_NEXT_COMMAND_VALUE         40081
 #define _APS_NEXT_CONTROL_VALUE         1063
