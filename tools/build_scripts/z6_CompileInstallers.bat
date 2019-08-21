e68bd4d1b2d30c1f1043ce6caf3a5722df34270c
George Hazan <ghazan@miranda.im>
2019-08-21 17:21:57 +0300

    one more fix

--- tools/build_scripts/z6_CompileInstallers.bat
+++ tools/build_scripts/z6_CompileInstallers.bat
@@ -8,8 +8,19 @@ copy /V /Y ..\..\build\build.no tmp\build.no
 pushd tmp
 for /F "tokens=1,2,3 delims= " %%i in (build.no) do set MirVer=%%i.%%j.%%k
 popd
-%ISCC% /Dptx86 /DAppVer=%MirVer% /O"Output" "InnoNG_32\MirandaNG.iss"
-%ISCC% /DAppVer=%MirVer% /O"Output" "InnoNG_64\MirandaNG.iss"
+
+%ISCC% /Dptx86 /DAppVer=%MirVer% /O"Output" "InnoNG_32\MirandaNG.iss" >iscc.log 2>&1
+if errorlevel 1 (
+  start "" iscc.log
+  exit 1
+)
+
+%ISCC% /DAppVer=%MirVer% /O"Output" "InnoNG_64\MirandaNG.iss" >iscc.log 2>&1
+if errorlevel 1 (
+  start "" iscc.log
+  exit 1
+)
+
 call cleanup.bat
 copy /V /Y Output\miranda-ng-alpha-latest*.exe "%ArchDistr%\"
 rd /S /Q Output
