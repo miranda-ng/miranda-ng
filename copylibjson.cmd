f4ba44ddae2289b44e11e42dc934cb03462793bf
George Hazan <ghazan@miranda.im>
2019-09-17 20:29:37 +0300

    development is made in VS2019

--- copylibjson.cmd
+++ copylibjson.cmd
@@ -1,3 +1,3 @@
 @echo off
-copy /Y .\Bin15\Debug\Obj\libjson\libjson.lib .\libs\win32
-copy /Y .\Bin15\Debug64\Obj\libjson\libjson.lib .\libs\win64
+copy /Y .\Bin16\Debug\Obj\libjson\libjson.lib .\libs\win32
+copy /Y .\Bin16\Debug64\Obj\libjson\libjson.lib .\libs\win64
