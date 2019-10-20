07c657da00c953f58f56cc5943d9da31450b563f
Mataes <mataes2007@gmail.com>
2019-10-20 19:56:00 +0300

    NewStory: cleanup

--- plugins/NewStory/src/options.cpp
+++ plugins/NewStory/src/options.cpp
@@ -6,14 +6,9 @@ static int OptionsInitialize(WPARAM wParam, LPARAM)
 {
 	OPTIONSDIALOGPAGE odp = { 0 };
 	odp.position = 100000000;
-	odp.pszTemplate = MAKEINTRESOURCEA(IDD_OPTIONS);
+	odp.szGroup.w = LPGENW("History");
 	odp.szTitle.w = LPGENW("Newstory");
 	odp.flags = ODPF_BOLDGROUPS | ODPF_UNICODE;
-
-	//	odp.szTab.w = LPGENW("General");
-	//	odp.pfnDlgProc = 0;
-	//	odp.pszTemplate = MAKEINTRESOURCEA(IDD_OPT_GENERAL);
-	//	g_plugin.addOptions(wParam, &odp);
 
 	odp.szTab.w = LPGENW("Templates");
 	odp.pfnDlgProc = OptTemplatesDlgProc;
--- plugins/NewStory/src/resource.h
+++ plugins/NewStory/src/resource.h
@@ -4,14 +4,12 @@
 //
 #define IDD_HISTORY                     101
 #define IDD_SEARCH                      105
-#define IDD_OPTIONS                     107
-#define IDD_MESSAGE                     112  //!!!
+#define IDD_MESSAGE                     112 //!!!
 #define IDR_POPUPS                      113
-#define IDR_ACCEL                       114  //!!!
+#define IDR_ACCEL                       114 //!!!
 #define IDD_CALENDARTOOL                118
 #define IDD_OPT_TEMPLATES               121
-#define IDD_FILTERMODE                  123  //!!!
-#define IDD_OPT_GENERAL                 124
+#define IDD_FILTERMODE                  123 //!!!
 #define ICO_NEWSTORY                    127
 #define ICO_USERINFO                    128
 #define ICO_USERMENU                    129
@@ -36,18 +34,15 @@
 #define ICO_PREVIEW                     150
 #define ICO_VARHELP                     151
 #define IDC_GPREVIEW                    1000
-#define IDC_LIST                        1000
-#define IDC_ITEMS2                      1000
-#define IDC_PROGRESS                    1002
-#define IDC_EDIT1                       1003
-#define IDC_SEARCHTEXT                  1003
+#define IDC_ITEMS2                      1001
+#define IDC_PROGRESS                    1002 //!!!
+#define IDC_EDIT1                       1003 //!!!
+#define IDC_SEARCHTEXT                  1004
 #define IDC_CLOSE                       1009
 #define IDC_MESSAGE                     1010
 #define IDC_USERINFO                    1011
 #define IDC_USERMENU                    1012
-#define IDC_TEXT                        1012  //!!!
 #define IDC_SEARCH                      1013
-#define IDC_USERNICK                    1013  //!!!
 #define IDC_EXPORT                      1014
 #define IDC_COPY                        1015
 #define IDC_FILTER                      1016
@@ -55,18 +50,20 @@
 #define IDC_LOGOPTIONS                  1018
 #define IDC_FINDPREV                    1019
 #define IDC_SEARCHICON                  1020
-#define IDC_MSGICON                     1022  //!!!
-#define IDC_DATEPOPUP                   1022
-#define IDC_ICO_MESSAGES_IN             1023
-#define IDC_ICO_MESSAGES_OUT            1024
-#define IDC_ICO_FILES_IN                1025
-#define IDC_ICO_FILES_OUT               1026
-#define IDC_ICO_TOTAL_IN                1027
-#define IDC_PREVIEW                     1028
-#define IDC_ICO_TOTAL_OUT               1028
-#define IDC_ICO_URLS_IN                 1029
-#define IDC_ICO_URLS_OUT                1030
-#define IDC_TAB                         1032
+#define IDC_TEXT                        1021 //!!!
+#define IDC_USERNICK                    1022 //!!!
+#define IDC_MSGICON                     1023 //!!!
+#define IDC_DATEPOPUP                   1024
+#define IDC_ICO_MESSAGES_IN             1025
+#define IDC_ICO_MESSAGES_OUT            1026
+#define IDC_ICO_FILES_IN                1027
+#define IDC_ICO_FILES_OUT               1028
+#define IDC_ICO_TOTAL_IN                1029
+#define IDC_ICO_TOTAL_OUT               1030
+#define IDC_ICO_URLS_IN                 1031
+#define IDC_ICO_URLS_OUT                1032
+#define IDC_PREVIEW                     1033
+#define IDC_TAB                         1034
 #define IDC_TIMETREE                    1035
 #define IDC_PROGRESS1                   1037  //!!!
 #define IDC_COMBO1                      1038  //!!!
@@ -77,17 +74,17 @@
 #define IDC_VARHELP                     1058
 #define IDC_CHK_IN                      1059  //!!!
 #define IDC_DISCARD                     1060
-#define IDC_CHK_FILE                    1060  //!!!
-#define IDC_CHK_URL                     1061  //!!!
 #define IDC_UPDATEPREVIEW               1061
-#define IDC_CHK_STATUS                  1062  //!!!
-#define IDC_CHK_OTHER                   1063  //!!!
-#define IDC_CHK_AUTOFILTER              1064  //!!!
-#define IDC_DATETIMEPICKER1             1065  //!!!
-#define IDC_DATETIMEPICKER2             1066  //!!!
-#define IDC_CHK_OUT                     1067  //!!!
-#define IDC_CHK_MESSAGES                1068  //!!!
-#define IDC_EDITTEMPLATE                1069
+#define IDC_CHK_FILE                    1062  //!!!
+#define IDC_CHK_URL                     1063  //!!!
+#define IDC_CHK_STATUS                  1064  //!!!
+#define IDC_CHK_OTHER                   1065  //!!!
+#define IDC_CHK_AUTOFILTER              1066  //!!!
+#define IDC_DATETIMEPICKER1             1067  //!!!
+#define IDC_DATETIMEPICKER2             1068  //!!!
+#define IDC_CHK_OUT                     1069  //!!!
+#define IDC_CHK_MESSAGES                1070  //!!!
+#define IDC_EDITTEMPLATE                1071
 #define IDC_ICO_MESSAGES                1078
 #define IDC_ICO_FILES                   1079
 #define IDC_ICO_URLS                    1080
