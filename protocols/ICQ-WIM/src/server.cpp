c4bd21ada7eb87b14ef79e075b943e0551061a8a
George Hazan <ghazan@miranda.im>
2020-07-17 21:03:32 +0300

    fixes #2482 (ICQ не всегда закрывает сессию при завершении работы)

--- protocols/ICQ-WIM/src/server.cpp
+++ protocols/ICQ-WIM/src/server.cpp
@@ -582,9 +582,9 @@ void CIcqProto::SetServerStatus(int iStatus)
 		<< AIMSID(this) << CHAR_PARAM("view", szStatus) << INT_PARAM("invisible", invisible));
 
 	if (iStatus == ID_STATUS_OFFLINE && !getByte(DB_KEY_PHONEREG)) {
-		auto *pReq = new AsyncHttpRequest(CONN_MAIN, REQUEST_GET, ICQ_API_SERVER "/aim/endSession", &CIcqProto::OnSessionEnd);
+		auto *pReq = new AsyncHttpRequest(CONN_NONE, REQUEST_GET, ICQ_API_SERVER "/aim/endSession", &CIcqProto::OnSessionEnd);
 		pReq << AIMSID(this) << INT_PARAM("invalidateToken", 1);
-		Push(pReq);
+		ExecuteRequest(pReq);
 	}
 
 	int iOldStatus = m_iStatus; m_iStatus = iStatus;
