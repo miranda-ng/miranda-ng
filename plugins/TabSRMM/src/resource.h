63fc6d6e73b980e76f0425ebbfcf4a4b78fda373
George Hazan <ghazan@miranda.im>
2020-07-30 20:39:10 +0300

    tabSRMM: senseless text from Advanced Tweaks dialog extincted

--- plugins/TabSRMM/src/resource.h
+++ plugins/TabSRMM/src/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by w:\miranda-ng\plugins\TabSRMM\res\chat.rc
+// Used by w:\miranda-ng\plugins\TabSRMM\res\resource.rc
 //
 #define IDD_TEMPLATEEDIT                1
 #define VS_VERSION_INFO                 1
@@ -831,7 +831,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_RESOURCE_VALUE        105
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
