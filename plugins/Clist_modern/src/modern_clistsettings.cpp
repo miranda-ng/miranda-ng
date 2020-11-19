b08465662997098e675a04f6ba59f2f4ef4ca74a
George Hazan <ghazan@miranda.im>
2020-11-19 19:24:38 +0300

    fixes #2606 (Не применяется режим вида при смене языка клиента)

--- plugins/Clist_modern/src/modern_clistsettings.cpp
+++ plugins/Clist_modern/src/modern_clistsettings.cpp
@@ -193,6 +193,6 @@ int OnLoadLangpack(WPARAM, LPARAM)
 {
 	UnknownConctactTranslatedName = TranslateT("(Unknown contact)");
 	if (!MirandaLoading())
-		ApplyViewMode(g_CluiData.current_viewmode, true);
+		ApplyViewMode(g_CluiData.current_viewmode, false);
 	return 0;
 }
