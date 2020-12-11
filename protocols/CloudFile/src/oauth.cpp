0e1523a47b8ad15b8c756718449ad66c3a775d96
George Hazan <ghazan@miranda.im>
2020-12-11 18:01:43 +0300

    fixes #2646 (CloudFile: Dropbox auth is broken)

--- protocols/CloudFile/src/oauth.cpp
+++ protocols/CloudFile/src/oauth.cpp
@@ -6,7 +6,6 @@ COAuthDlg::COAuthDlg(CCloudService *service, const char *authUrl, CCloudService:
 	m_authorize(this, IDC_OAUTH_AUTHORIZE, authUrl),
 	m_code(this, IDC_OAUTH_CODE), m_ok(this, IDOK)
 {
-	m_autoClose = CLOSE_ON_CANCEL;
 	m_code.OnChange = Callback(this, &COAuthDlg::Code_OnChange);
 }
 
