69b7dc9c316a6cbd1fdb846e2cc93fed96da329f
George Hazan <ghazan@miranda.im>
2021-01-11 22:02:20 +0300

    fixes #2686 ([IRC] SASL authentication failed)

--- protocols/IRCG/src/commandmonitor.cpp
+++ protocols/IRCG/src/commandmonitor.cpp
@@ -2162,7 +2162,7 @@ bool CIrcProto::OnIrc_AUTH_FAIL(const CIrcMessage*)
 bool CIrcProto::OnIrc_AUTHENTICATE(const CIrcMessage *pmsg)
 {
 	if (m_bUseSASL && pmsg->parameters[0] == "+") {
-		CMStringA payload(FORMAT, "%S%c%S%c%s%c", m_userID, 0, m_userID, 0, m_password, 0);
+		CMStringA payload(FORMAT, "%S%c%S%c%s%c", m_nick, 0, m_nick, 0, m_password, 0);
 		NLSend("AUTHENTICATE %s\r\n", ptrA(mir_base64_encode(payload, payload.GetLength())).get());
 		NLSend("CAP END\r\n");
 	}
