0c4285ffa8331b1ed55e51b7d511db52f3cfea19
George Hazan <ghazan@miranda.im>
2021-01-16 14:50:58 +0300

    fix for BS_AUTOCHECKBOX flag processing

--- src/mir_core/src/CDlgBase.cpp
+++ src/mir_core/src/CDlgBase.cpp
@@ -185,12 +185,14 @@ BOOL CALLBACK CDlgBase::GlobalFieldEnum(HWND hwnd, LPARAM lParam)
 	else if (!wcsicmp(wszClass, L"ComboBox"))
 		new CCtrlCombo(pDlg, id);
 	else if (!wcsicmp(wszClass, L"Button")) {
-		switch (GetWindowLongW(hwnd, GWL_STYLE) & (BS_CHECKBOX | BS_RADIOBUTTON | BS_AUTORADIOBUTTON)) {
+		switch (GetWindowLongW(hwnd, GWL_STYLE) & (BS_CHECKBOX | BS_RADIOBUTTON | BS_AUTOCHECKBOX | BS_AUTORADIOBUTTON)) {
 		case BS_CHECKBOX:
+		case BS_AUTOCHECKBOX:
 		case BS_RADIOBUTTON:
 		case BS_AUTORADIOBUTTON:
 			new CCtrlCheck(pDlg, id);
 			break;
+
 		default:
 			new CCtrlButton(pDlg, id);
 		}
