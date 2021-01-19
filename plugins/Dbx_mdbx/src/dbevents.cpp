5090efd0acaf21325a052a3041d1f2e52e14c5d8
George Hazan <ghazan@miranda.im>
2021-01-19 20:02:44 +0300

    Dbx_mdbx: fix for reversed event cursors behavior

--- plugins/Dbx_mdbx/src/dbevents.cpp
+++ plugins/Dbx_mdbx/src/dbevents.cpp
@@ -657,6 +657,13 @@ public:
 			if (mdbx_cursor_get(m_pOwner->m_curEventsSort, &key, &data, MDBX_SET_RANGE) != MDBX_SUCCESS)
 				return 0;
 			dbKey = *(const DBEventSortingKey *)key.iov_base;
+
+			if (!m_bForward && dbKey.hContact) {
+				if (mdbx_cursor_get(m_pOwner->m_curEventsSort, &key, &data, MDBX_PREV) != MDBX_SUCCESS)
+					return 0;
+
+				dbKey = *(const DBEventSortingKey *)key.iov_base;
+			}
 		}
 		else {
 			if (mdbx_cursor_get(m_pOwner->m_curEventsSort, &key, &data, MDBX_SET) != MDBX_SUCCESS)
