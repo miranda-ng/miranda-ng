6d8d0f1919042a189b806ed05d0bd51b6876b354
George Hazan <ghazan@miranda.im>
2021-01-20 18:18:30 +0300

    fixes #2692 ([Steam] Messages received by Steam client first are not downloaded after logging into Miranda)

--- protocols/Steam/src/steam_polling.cpp
+++ protocols/Steam/src/steam_polling.cpp
@@ -15,9 +15,6 @@ void CSteamProto::ParsePollData(const JSONNode &data)
 			hContact = GetContact(steamId.c_str());
 			if (hContact == 0)
 				continue;
-
-			if (timestamp > getDword(hContact, DB_KEY_LASTMSGTS))
-				setDword(hContact, DB_KEY_LASTMSGTS, timestamp);
 		}
 		else hContact = 0;
 
@@ -54,6 +51,9 @@ void CSteamProto::ParsePollData(const JSONNode &data)
 
 		if (type == "my_saytext" || type =="my_emote") {
 			json_string text = item["text"].as_string();
+
+			if (timestamp > getDword(hContact, DB_KEY_LASTMSGTS))
+				setDword(hContact, DB_KEY_LASTMSGTS, timestamp);
 
 			PROTORECVEVENT recv = { 0 };
 			recv.timestamp = timestamp;
