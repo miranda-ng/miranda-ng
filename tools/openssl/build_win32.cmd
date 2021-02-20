b37158418475db921a89452e94e51affd46949c6
George Hazan <ghazan@miranda.im>
2021-02-20 14:43:46 +0300

    fixes #2748 + update for batch files

--- tools/openssl/build_win32.cmd
+++ tools/openssl/build_win32.cmd
@@ -10,3 +10,29 @@ perl "%OPENSSL_ROOT_DIR%\Configure" shared VC-WIN32 /D\"_USING_V110_SDK71_\" /D\
 
 call %VS141COMNTOOLS%\..\..\VC\Auxiliary\Build\vcvars32.bat 
 nmake 
+
+copy libcrypto-1_1.mir %MIRANDA_BUILD_ROOT%\pre-build\bin15\32\Libs
+copy libssl-1_1.mir    %MIRANDA_BUILD_ROOT%\pre-build\bin15\32\Libs
+
+copy libcrypto-1_1.pdb %MIRANDA_BUILD_ROOT%\pre-symbols\bin15\32
+copy libssl-1_1.pdb    %MIRANDA_BUILD_ROOT%\pre-symbols\bin15\32
+
+copy libcrypto-1_1.mir %MIRANDA_DEV_ROOT%\bin15\Release\Libs
+copy libcrypto-1_1.pdb %MIRANDA_DEV_ROOT%\bin15\Release\Libs
+copy libssl-1_1.mir    %MIRANDA_DEV_ROOT%\bin15\Release\Libs
+copy libssl-1_1.pdb    %MIRANDA_DEV_ROOT%\bin15\Release\Libs
+
+copy libcrypto-1_1.mir %MIRANDA_DEV_ROOT%\bin15\Debug\Libs
+copy libcrypto-1_1.pdb %MIRANDA_DEV_ROOT%\bin15\Debug\Libs
+copy libssl-1_1.mir    %MIRANDA_DEV_ROOT%\bin15\Debug\Libs
+copy libssl-1_1.pdb    %MIRANDA_DEV_ROOT%\bin15\Debug\Libs
+
+copy libcrypto-1_1.mir %MIRANDA_DEV_ROOT%\bin16\Release\Libs
+copy libcrypto-1_1.pdb %MIRANDA_DEV_ROOT%\bin16\Release\Libs
+copy libssl-1_1.mir    %MIRANDA_DEV_ROOT%\bin16\Release\Libs
+copy libssl-1_1.pdb    %MIRANDA_DEV_ROOT%\bin16\Release\Libs
+
+copy libcrypto-1_1.mir %MIRANDA_DEV_ROOT%\bin16\Debug\Libs
+copy libcrypto-1_1.pdb %MIRANDA_DEV_ROOT%\bin16\Debug\Libs
+copy libssl-1_1.mir    %MIRANDA_DEV_ROOT%\bin16\Debug\Libs
+copy libssl-1_1.pdb    %MIRANDA_DEV_ROOT%\bin16\Debug\Libs
--- tools/openssl/build_win64.cmd
+++ tools/openssl/build_win64.cmd
@@ -9,3 +9,29 @@ perl "%OPENSSL_ROOT_DIR%\Configure" shared VC-WIN64A
 
 call %VS141COMNTOOLS%\..\..\VC\Auxiliary\Build\vcvars64.bat 
 nmake 
+
+copy libcrypto-1_1.mir %MIRANDA_BUILD_ROOT%\pre-build\bin15\64\Libs
+copy libssl-1_1.mir    %MIRANDA_BUILD_ROOT%\pre-build\bin15\64\Libs
+
+copy libcrypto-1_1.pdb %MIRANDA_BUILD_ROOT%\pre-symbols\bin15\64
+copy libssl-1_1.pdb    %MIRANDA_BUILD_ROOT%\pre-symbols\bin15\64
+
+copy libcrypto-1_1.mir %MIRANDA_DEV_ROOT%\bin15\Release64\Libs
+copy libcrypto-1_1.pdb %MIRANDA_DEV_ROOT%\bin15\Release64\Libs
+copy libssl-1_1.mir    %MIRANDA_DEV_ROOT%\bin15\Release64\Libs
+copy libssl-1_1.pdb    %MIRANDA_DEV_ROOT%\bin15\Release64\Libs
+
+copy libcrypto-1_1.mir %MIRANDA_DEV_ROOT%\bin15\Debug64\Libs
+copy libcrypto-1_1.pdb %MIRANDA_DEV_ROOT%\bin15\Debug64\Libs
+copy libssl-1_1.mir    %MIRANDA_DEV_ROOT%\bin15\Debug64\Libs
+copy libssl-1_1.pdb    %MIRANDA_DEV_ROOT%\bin15\Debug64\Libs
+
+copy libcrypto-1_1.mir %MIRANDA_DEV_ROOT%\bin16\Release64\Libs
+copy libcrypto-1_1.pdb %MIRANDA_DEV_ROOT%\bin16\Release64\Libs
+copy libssl-1_1.mir    %MIRANDA_DEV_ROOT%\bin16\Release64\Libs
+copy libssl-1_1.pdb    %MIRANDA_DEV_ROOT%\bin16\Release64\Libs
+
+copy libcrypto-1_1.mir %MIRANDA_DEV_ROOT%\bin16\Debug64\Libs
+copy libcrypto-1_1.pdb %MIRANDA_DEV_ROOT%\bin16\Debug64\Libs
+copy libssl-1_1.mir    %MIRANDA_DEV_ROOT%\bin16\Debug64\Libs
+copy libssl-1_1.pdb    %MIRANDA_DEV_ROOT%\bin16\Debug64\Libs
