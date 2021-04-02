386ac322e5b1047842a7cbd4b4ff1b5602e8d796
George Hazan <ghazan@miranda.im>
2021-04-02 11:03:11 +0300

    for #2826 (unused resource removed)

--- plugins/Dbx_sqlite/src/resource.h
+++ plugins/Dbx_sqlite/src/resource.h
@@ -1,11 +1,4 @@
 //{{NO_DEPENDENCIES}}
-// Включаемый файл, созданный в Microsoft Visual C++.
-// Используется D:\Others\SVN\MirandaNG\trunk\plugins\Db3x_mmap\res\db3x_mmap.rc
-//
-#define IDREMOVE                        3
-#define IDD_OPTIONS                     105
-#define IDC_STANDARD                    1007
-#define IDC_TOTAL                       1008
 
 // Next default values for new objects
 // 
