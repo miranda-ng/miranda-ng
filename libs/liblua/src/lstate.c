9a749bc628747c148394314a97fdeae6e032dc0b
dartraiden <wowemuh@gmail.com>
2021-04-03 15:36:24 +0300

    liblua: pick up Miranda-specific commits

--- libs/liblua/src/lstate.c
+++ libs/liblua/src/lstate.c
@@ -408,6 +408,9 @@ LUA_API lua_State *lua_newstate (lua_Alloc f, void *ud) {
 
 
 LUA_API void lua_close (lua_State *L) {
+  if (!L)
+    return;
+
   lua_lock(L);
   L = G(L)->mainthread;  /* only the main thread can be closed */
   close_state(L);
