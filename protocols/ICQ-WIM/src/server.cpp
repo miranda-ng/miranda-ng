211e1869f026d7180c8d7cdbcd7618f3ff00462d
dartraiden <wowemuh@gmail.com>
2021-05-28 16:13:09 +0300

    ICQ and MRA logins can be not only numbers

--- protocols/ICQ-WIM/src/server.cpp
+++ protocols/ICQ-WIM/src/server.cpp
@@ -189,10 +189,10 @@ void CIcqProto::ConnectionFailed(int iReason, int iErrorCode)
 		wcscpy_s(Popup.lpwzContactName, m_tszUserName);
 		switch (iReason) {
 		case LOGINERR_BADUSERID:
-			mir_snwprintf(Popup.lpwzText, TranslateT("You have not entered an ICQ number or password.\nConfigure this in Options -> Network -> ICQ and try again."));
+			mir_snwprintf(Popup.lpwzText, TranslateT("You have not entered a login or password.\nConfigure this in Options -> Network -> ICQ and try again."));
 			break;
 		case LOGINERR_WRONGPASSWORD:
-			mir_snwprintf(Popup.lpwzText, TranslateT("Connection failed.\nYour ICQ number or password was rejected (%d)."), iErrorCode);
+			mir_snwprintf(Popup.lpwzText, TranslateT("Connection failed.\nYour login or password was rejected (%d)."), iErrorCode);
 			break;
 		case LOGINERR_NONETWORK:
 		case LOGINERR_NOSERVER:
