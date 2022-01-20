198b9722220e77db1515450534e363107c35bc2f
George Hazan <ghazan@miranda.im>
2022-01-20 16:31:23 +0300

    fixes #2994 (Deprecate Discord support)

--- plugins/PluginUpdater/src/DlgUpdate.cpp
+++ plugins/PluginUpdater/src/DlgUpdate.cpp
@@ -561,6 +561,7 @@ static renameTable[] =
 
 	{ L"advaimg.dll",                    nullptr },
 	{ L"aim.dll",                        nullptr },
+	{ L"discord.dll",                    nullptr },
 	{ L"extraicons.dll",                 nullptr },
 	{ L"firstrun.dll",                   nullptr },
 	{ L"flashavatars.dll",               nullptr },
