a1af87253ded620840639d5948c390d885d1bced
George Hazan <ghazan@miranda.im>
2022-02-02 17:14:33 +0300

    Visual Studio 2022 project files

---
+++ bin17/pascal.bat
@@ -0,0 +1,36 @@
+@echo off
+
+if "%1" == "64" (set fpcpl=64) else (set fpcpl=)
+
+pushd ..\plugins
+
+pushd Actman
+call make.bat fpc%fpcpl% 16
+if errorlevel 1 goto :Error
+popd
+
+pushd mRadio
+call make.bat fpc%fpcpl% 16
+if errorlevel 1 goto :Error
+popd
+
+pushd Watrack
+call make.bat fpc%fpcpl% 16
+if errorlevel 1 goto :Error
+cd icons
+call makeicons.bat fpc%fpcpl% 16
+if errorlevel 1 goto :Error
+popd
+
+pushd HistoryPlusPlus
+call make.bat %1
+if errorlevel 1 goto :Error
+popd
+
+popd
+goto :eof
+
+:Error
+echo ============================= FAIL! =============================
+pause
+exit
