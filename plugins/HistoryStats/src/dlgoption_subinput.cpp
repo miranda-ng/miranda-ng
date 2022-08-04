miranda-ng/miranda-ng

commit 500dd4848842f6d4207584417448586d060fbd6a

Contact: group of functions gathered into the personal namespace

plugins/HistoryStats/src/dlgoption_subinput.cpp
@@ -73,7 +73,7 @@ void DlgOption::SubInput::onWMInitDialog()
 	m_hChatSessionTimeout = m_Options.insertEdit(hTemp, TranslateT("Time between two chat sessions (seconds)"), L"", OptionsCtrl::OCF_NUMBER);
 	m_hAverageMinTime = m_Options.insertEdit(hTemp, TranslateT("Minimum time to assume when calculating average (days)"), L"", OptionsCtrl::OCF_NUMBER);
 	m_hWordDelimiters = m_Options.insertEdit(hTemp, TranslateT("Word delimiting characters"));
-	hTemp = m_Options.insertGroup(nullptr, TranslateT("Contact filtering"), OptionsCtrl::OCF_ROOTGROUP | OptionsCtrl::OCF_NODISABLECHILDS);
+	hTemp = m_Options.insertGroup(nullptr, TranslateT("CContact filtering"), OptionsCtrl::OCF_ROOTGROUP | OptionsCtrl::OCF_NODISABLECHILDS);
 	m_hProtocols = m_Options.insertGroup(hTemp, TranslateT("Ignore all contacts with protocol..."));
 
 	hTemp2 = m_Options.insertGroup(hTemp, TranslateT("History read mode for metacontacts"), 0);

plugins/HistoryStats/src/dlgoption_suboutput.cpp
@@ -98,7 +98,7 @@ void DlgOption::SubOutput::onWMInitDialog()
 	OptionsCtrl::Item hTemp;
 	OptionsCtrl::Item hTemp2;
 
-	hTemp                              = m_Options.insertGroup(nullptr, TranslateT("Contact filtering and totals"), OptionsCtrl::OCF_ROOTGROUP | OptionsCtrl::OCF_NODISABLECHILDS);
+	hTemp                              = m_Options.insertGroup(nullptr, TranslateT("CContact filtering and totals"), OptionsCtrl::OCF_ROOTGROUP | OptionsCtrl::OCF_NODISABLECHILDS);
 		m_hRemoveEmptyContacts          = m_Options.insertCheck(hTemp, TranslateT("Remove contacts with empty history"));
 		m_hRemoveOutChatsZero           = m_Options.insertCheck(hTemp, TranslateT("Remove contacts with only incoming chats"), OptionsCtrl::OCF_DISABLECHILDSONUNCHECK);
 			m_hRemoveOutBytesZero        = m_Options.insertCheck(m_hRemoveOutChatsZero, TranslateT("Remove only if you never answered"));

plugins/HistoryStats/src/iconlib.cpp
@@ -11,7 +11,7 @@ IconLib::IconInfo IconLib::m_IconInfo[] = {
 	{ IDI_HISTORYSTATS  , "main_menu"     , nullptr,                   LPGENW("Create statistics (main menu)")     },
 	{ IDI_HISTORYSTATS  , "menu_show"     , nullptr,                   LPGENW("Show statistics (main menu)")       },
 	{ IDI_HISTORYSTATS  , "menu_config"   , nullptr,                   LPGENW("Configure... (main menu)")          },
-	{ IDI_HISTORYSTATS  , "contact_menu"  , nullptr,                   LPGENW("Contact menu")                      },
+	{ IDI_HISTORYSTATS  , "contact_menu"  , nullptr,                   LPGENW("CContact menu")                      },
 	{ IDI_EXCLUDE_NO    , "exclude_no"    , nullptr,                   LPGENW("Unexcluded contacts")               },
 	{ IDI_EXCLUDE_YES   , "exclude_yes"   , nullptr,                   LPGENW("Excluded contacts")                 },
 	{ IDI_TREE_CHECK1   , "tree_check1"   , LPGENW("Options tree"), LPGENW("Checkbox")                          },

commit 175b06257bc63adadbe5f038c703856e1ec413c6

HistoryStats: fix regression introduced in https://github.com/miranda-ng/miranda-ng/commit/500dd4848842f6d4207584417448586d060fbd6a

plugins/HistoryStats/src/dlgoption_subinput.cpp
@@ -73,7 +73,7 @@ void DlgOption::SubInput::onWMInitDialog()
 	m_hChatSessionTimeout = m_Options.insertEdit(hTemp, TranslateT("Time between two chat sessions (seconds)"), L"", OptionsCtrl::OCF_NUMBER);
 	m_hAverageMinTime = m_Options.insertEdit(hTemp, TranslateT("Minimum time to assume when calculating average (days)"), L"", OptionsCtrl::OCF_NUMBER);
 	m_hWordDelimiters = m_Options.insertEdit(hTemp, TranslateT("Word delimiting characters"));
-	hTemp = m_Options.insertGroup(nullptr, TranslateT("CContact filtering"), OptionsCtrl::OCF_ROOTGROUP | OptionsCtrl::OCF_NODISABLECHILDS);
+	hTemp = m_Options.insertGroup(nullptr, TranslateT("Contact filtering"), OptionsCtrl::OCF_ROOTGROUP | OptionsCtrl::OCF_NODISABLECHILDS);
 	m_hProtocols = m_Options.insertGroup(hTemp, TranslateT("Ignore all contacts with protocol..."));
 
 	hTemp2 = m_Options.insertGroup(hTemp, TranslateT("History read mode for metacontacts"), 0);

plugins/HistoryStats/src/dlgoption_suboutput.cpp
@@ -98,7 +98,7 @@ void DlgOption::SubOutput::onWMInitDialog()
 	OptionsCtrl::Item hTemp;
 	OptionsCtrl::Item hTemp2;
 
-	hTemp                              = m_Options.insertGroup(nullptr, TranslateT("CContact filtering and totals"), OptionsCtrl::OCF_ROOTGROUP | OptionsCtrl::OCF_NODISABLECHILDS);
+	hTemp                              = m_Options.insertGroup(nullptr, TranslateT("Contact filtering and totals"), OptionsCtrl::OCF_ROOTGROUP | OptionsCtrl::OCF_NODISABLECHILDS);
 		m_hRemoveEmptyContacts          = m_Options.insertCheck(hTemp, TranslateT("Remove contacts with empty history"));
 		m_hRemoveOutChatsZero           = m_Options.insertCheck(hTemp, TranslateT("Remove contacts with only incoming chats"), OptionsCtrl::OCF_DISABLECHILDSONUNCHECK);
 			m_hRemoveOutBytesZero        = m_Options.insertCheck(m_hRemoveOutChatsZero, TranslateT("Remove only if you never answered"));

plugins/HistoryStats/src/iconlib.cpp
@@ -11,7 +11,7 @@ IconLib::IconInfo IconLib::m_IconInfo[] = {
 	{ IDI_HISTORYSTATS  , "main_menu"     , nullptr,                   LPGENW("Create statistics (main menu)")     },
 	{ IDI_HISTORYSTATS  , "menu_show"     , nullptr,                   LPGENW("Show statistics (main menu)")       },
 	{ IDI_HISTORYSTATS  , "menu_config"   , nullptr,                   LPGENW("Configure... (main menu)")          },
-	{ IDI_HISTORYSTATS  , "contact_menu"  , nullptr,                   LPGENW("CContact menu")                      },
+	{ IDI_HISTORYSTATS  , "contact_menu"  , nullptr,                   LPGENW("Contact menu")                      },
 	{ IDI_EXCLUDE_NO    , "exclude_no"    , nullptr,                   LPGENW("Unexcluded contacts")               },
 	{ IDI_EXCLUDE_YES   , "exclude_yes"   , nullptr,                   LPGENW("Excluded contacts")                 },
 	{ IDI_TREE_CHECK1   , "tree_check1"   , LPGENW("Options tree"), LPGENW("Checkbox")                          },