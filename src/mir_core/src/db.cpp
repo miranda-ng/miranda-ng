c1570430a5de59816125d5852b69a771835f2937
George Hazan <ghazan@miranda.im>
2023-01-07 14:53:23 +0300

    Telegram: fix for seeing outgoing messages twice

--- src/mir_core/src/db.cpp
+++ src/mir_core/src/db.cpp
@@ -458,8 +458,9 @@ MIR_CORE_DLL(MEVENT) db_event_replace(MCONTACT hContact, const DBEVENTINFO *dbei
 		ret = db_event_getById(dbei->szModule, dbei->szId);
 	if (!ret)
 		ret = db_event_add(hContact, dbei);
-	else
-		db_event_edit(hContact, ret, dbei);
+	// do not uncomment
+	// else
+	//	db_event_edit(hContact, ret, dbei);
 	return ret;
 }
 
