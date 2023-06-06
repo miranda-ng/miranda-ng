be8a64b8d423f460c4ea9aeae1d2f20ed078dc6c
George Hazan <george.hazan@gmail.com>
2023-06-06 19:13:16 +0300

    fixes #3538 (Jabber: перенести опцию об использовании HTTP FIle Upload в настройки протокола)

--- protocols/JabberG/src/jabber_opt.cpp
+++ protocols/JabberG/src/jabber_opt.cpp
@@ -705,6 +705,8 @@ public:
 		m_options.AddOption(LPGENW("Messaging"), LPGENW("Enable server-side history (XEP-0136)"), m_proto->m_bEnableMsgArchive);
 		m_options.AddOption(LPGENW("Messaging"), LPGENW("Enable Message Archive Management (XEP-0313)"), m_proto->m_bEnableMam);
 		m_options.AddOption(LPGENW("Messaging"), LPGENW("Enable carbon copies (XEP-0280)"), m_proto->m_bEnableCarbons);
+		if (mir_strlen(ptrA(m_proto->getStringA("HttpUpload"))))
+			m_options.AddOption(LPGENW("Messaging"), LPGENW("Enable HTTP Upload (XEP-0363)"), m_proto->m_bUseHttpUpload);
 		if (g_plugin.bJingle)
 			m_options.AddOption(LPGENW("Messaging"), LPGENW("Enable VOIP (experimental)"), m_proto->m_bEnableVOIP);
 
