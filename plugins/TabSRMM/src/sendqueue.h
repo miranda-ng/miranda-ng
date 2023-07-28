65b8ac5da658208d9b8540eaa39299283ea99d04
George Hazan <george.hazan@gmail.com>
2023-07-28 23:01:32 +0300

    tabSRMM: fix for timer id conflict

--- plugins/TabSRMM/src/sendqueue.h
+++ plugins/TabSRMM/src/sendqueue.h
@@ -27,7 +27,7 @@
 #ifndef __SENDQUEUE_H
 #define __SENDQUEUE_H
 
-#define TIMERID_MSGSEND      100
+#define TIMERID_MSGSEND      200
 #define TIMEOUT_TYPEOFF      10000      // send type off after 10 seconds of inactivity
 #define SB_CHAR_WIDTH        45
 #define SEND_FLAGS PREF_UNICODE
