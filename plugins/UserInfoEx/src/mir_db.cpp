8bdb78f666213cb8094b26ab8f55e0613c94e26c
George Hazan <george.hazan@gmail.com>
2023-12-26 14:19:22 +0300

    unused code removed

--- plugins/UserInfoEx/src/mir_db.cpp
+++ plugins/UserInfoEx/src/mir_db.cpp
@@ -56,18 +56,6 @@ uint32_t	WhenAdded(uint32_t dwUIN, LPCSTR)
 namespace Module {
 
 /**
-* Enum Proc for DBModule_IsEmpty
-* @param	pszSetting	- the setting
-* @param	lParam		- DBCONTACTENUMSETTINGS - (LPARAM)&dbces
-* @retval	TRUE		- always true
-**/
-
-static int IsEmptyEnumProc(LPCSTR, void*)
-{
-	return 1;
-}
-
-/**
 * This function tests, whether a module belongs to a metacontact protocol
 * @param	pszModule	- the module to read the setting from (e.g. USERINFO)
 * @retval	TRUE		- the module belongs to a metacontact protocol
