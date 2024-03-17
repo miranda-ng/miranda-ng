31705a905d2fa6e81a3c8cc40c45e7c0366d6c49
George Hazan <george.hazan@gmail.com>
2024-03-17 12:29:48 +0300

    fixes #4285 (NewStory: прокрутка при редактировании реализована, но не работает)

--- plugins/NewStory/src/history_control.cpp
+++ plugins/NewStory/src/history_control.cpp
@@ -244,7 +244,7 @@ void NewstoryListData::BeginEditItem()
 	wszText.Replace(L"\n", L"\r\n");
 
 	uint32_t dwStyle = WS_CHILD | WS_BORDER | WS_VSCROLL | ES_MULTILINE | ES_AUTOVSCROLL;
-	hwndEditBox = CreateWindow(L"EDIT", wszText, dwStyle, 0, top, rc.right - rc.left, itemHeight, m_hwnd, NULL, g_plugin.getInst(), NULL);
+	hwndEditBox = CreateWindow(L"EDIT", wszText, dwStyle, 0, top, rc.right - rc.left, height, m_hwnd, NULL, g_plugin.getInst(), NULL);
 	mir_subclassWindow(hwndEditBox, HistoryEditWndProc);
 	SendMessage(hwndEditBox, WM_SETFONT, (WPARAM)g_fontTable[fontid].hfnt, 0);
 	SendMessage(hwndEditBox, EM_SETMARGINS, EC_RIGHTMARGIN, 100);
