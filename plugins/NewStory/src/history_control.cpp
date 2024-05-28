5c1f4855926c423c241a494896b9b62d43956bff
George Hazan <george.hazan@gmail.com>
2024-05-28 15:37:24 +0300

    fixes #4440 (NewStory: история проматывается в самый конец при изменении размера окна истории по вертикали)

--- plugins/NewStory/src/history_control.cpp
+++ plugins/NewStory/src/history_control.cpp
@@ -105,7 +105,7 @@ void NewstoryListData::OnResize(int newWidth, int newHeight)
 
 	if (newHeight != cachedWindowHeight) {
 		cachedWindowHeight = newHeight;
-		FixScrollPosition(true);
+		FixScrollPosition();
 		bDraw = true;
 	}
 
