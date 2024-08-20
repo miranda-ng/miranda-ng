0e423aaec68bcf17bf868d046e03f3a7dbed4aa1
dartraiden <wowemuh@gmail.com>
2024-08-21 01:59:00 +0300

    MirOTR: remove hack for ICQ

--- plugins/MirOTR/src/otr.h
+++ plugins/MirOTR/src/otr.h
@@ -6,9 +6,7 @@ extern OtrlMessageAppOps ops;
 /// @todo : add OTR version 3 instance tag choice (currently we use the most secure/active one)
 #define MIROTR_PROTO_LATEST 3 // on change, also update HELLO below
 #define MIROTR_PROTO_HELLO "?OTRv23?"
-/* OTR link normally is https://otr.cypherpunks.ca/ but it's blocked by ICQ as of March 2015 (even something like "ot-r.cyp her_ pun ks. ca")
-	It might be a good idea to use http://wikipedia.org/wiki/Off-the-Record_Messaging instead as it's not yet blocked by ICQ but might still help users to find a client/plugin */
-#define MIROTR_PROTO_HELLO_MSG LPGENW(" has requested an Off-the-Record private conversation. However, you do not have a plugin to support that.\nSee http://wikipedia.org/wiki/Off-the-Record_Messaging for more information.")
+#define MIROTR_PROTO_HELLO_MSG LPGENW(" has requested an Off-the-Record private conversation. However, you do not have a plugin to support that.\nSee https://otr.cypherpunks.ca for more information.")
 
 extern "C" {
 
