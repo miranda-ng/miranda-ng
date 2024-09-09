ebf83d38be5ad294e3ed47bc33f947cee7b1a92f
George Hazan <george.hazan@gmail.com>
2024-09-09 20:50:24 +0300

    fixes #4617 (Подсветка (highlight) не срабатывает на ответы (reply))

--- plugins/TabSRMM/src/muchighlight.cpp
+++ plugins/TabSRMM/src/muchighlight.cpp
@@ -164,11 +164,28 @@ skip_textpatterns:
 
 	// optionally, match the nickname against the list of nicks to highlight
 	if ((m_iMode & MATCH_NICKNAME) && (dwFlags & MATCH_NICKNAME) && pgce->pszNick.w && m_iNickPatterns > 0) {
+		DBEVENTINFO dbei = {};
+		if (pgce->hEvent)
+			db_event_get(pgce->hEvent, &dbei);
+
 		for (UINT i = 0; i < m_iNickPatterns && !nResult; i++) {
 			if (pgce->pszNick.w)
-				nResult = wildcmpw(pgce->pszNick.w, m_NickPatterns[i]) ? MATCH_NICKNAME : 0;
+				if (wildcmpw(pgce->pszNick.w, m_NickPatterns[i])) {
+					nResult = MATCH_NICKNAME;
+					break;
+				}
+
+			if (dbei.szReplyId)
+				if (wildcmpw(_A2T(dbei.szReplyId), m_NickPatterns[i])) {
+					nResult = MATCH_NICKNAME;
+					break;
+				}
+
 			if ((m_iMode & MATCH_UIN) && pgce->pszUserInfo.w)
-				nResult = wildcmpw(pgce->pszUserInfo.w, m_NickPatterns[i]) ? MATCH_NICKNAME : 0;
+				if (wildcmpw(pgce->pszUserInfo.w, m_NickPatterns[i])) {
+					nResult = MATCH_NICKNAME;
+					break;
+				}
 		}
 	}
 
