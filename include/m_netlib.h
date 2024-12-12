fa043865c03f6c3a033c0576e4f2be980d27f4fa
George Hazan <george.hazan@gmail.com>
2024-12-12 23:19:59 +0300

    fix for mir_app headers

--- include/m_netlib.h
+++ include/m_netlib.h
@@ -770,7 +770,7 @@ public:
 	MWebSocket();
 	~MWebSocket();
 
-	__forceinline HNETLIBCONN getConn() const { return m_hConn; }
+	__forceinline HANDLE getConn() const { return m_hConn; }
 
 	// packet processor
 	virtual void process(const uint8_t *buf, size_t cbLen) = 0;
