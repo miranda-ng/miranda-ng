e481614f93972d2e831150fcc67550185a1e9c70
George Hazan <george.hazan@gmail.com>
2025-03-04 18:22:45 +0300

    fixes #4903 (Steam: заменить значок FFC на Invisible)

--- protocols/Steam/proto_steam/src/resource.h
+++ protocols/Steam/proto_steam/src/resource.h
@@ -5,9 +5,9 @@
 #define IDI_ICON1                       105
 #define IDI_ICON2                       104
 #define IDI_ICON3                       128
+#define IDI_ICON4                       130
 #define IDI_ICON5                       131
 #define IDI_ICON6                       158
-#define IDI_ICON8                       129
 
 // Next default values for new objects
 // 
