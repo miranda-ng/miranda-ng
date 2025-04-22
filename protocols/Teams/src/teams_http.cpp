2d6c10c9d420d6e1f00cd2d7649f66728ee3e90f
George Hazan <george.hazan@gmail.com>
2025-04-22 19:54:50 +0300

    code cleaning

--- protocols/Teams/src/teams_http.cpp
+++ protocols/Teams/src/teams_http.cpp
@@ -26,10 +26,7 @@ AsyncHttpRequest::AsyncHttpRequest(int type, SkypeHost host, LPCSTR url, MTHttpR
 	case HOST_GRAPH:     m_szUrl = "skypegraph.skype.com"; break;
 	case HOST_LOGIN:     m_szUrl = "login.microsoftonline.com"; break;
 	case HOST_TEAMS:     m_szUrl = TEAMS_BASE_HOST; break;
-	case HOST_PRESENCE:
-		flags |= NLHRF_REDIRECT;
-		m_szUrl = "presence." TEAMS_BASE_HOST "/v1";
-		break;
+	case HOST_PRESENCE:  m_szUrl = "presence." TEAMS_BASE_HOST "/v1"; break;
 
 	case HOST_DEFAULT_V2:
 		AddHeader("MS-IC3-Product", "Sfl");
@@ -138,6 +135,8 @@ MHttpResponse* CTeamsProto::DoSend(AsyncHttpRequest *pReq)
 		break;
 
 	case HOST_PRESENCE:
+		pReq->flags |= NLHRF_REDIRECT;
+
 		if (m_szSkypeToken)
 			pReq->AddHeader("X-Skypetoken", m_szSkypeToken);
 
