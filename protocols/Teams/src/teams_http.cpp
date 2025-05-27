f86bb06d4f7225f17677d9bd3af8f40f5a0b5b3d
George Hazan <george.hazan@gmail.com>
2025-05-27 14:54:57 +0300

    Teams: old Skype search site removed

--- protocols/Teams/src/teams_http.cpp
+++ protocols/Teams/src/teams_http.cpp
@@ -23,7 +23,6 @@ AsyncHttpRequest::AsyncHttpRequest(int type, SkypeHost host, LPCSTR url, MTHttpR
 	switch (host) {
 	case HOST_API:       m_szUrl = "api.skype.com"; break;
 	case HOST_CONTACTS:  m_szUrl = "contacts.skype.com/contacts/v2"; break;
-	case HOST_GRAPH:     m_szUrl = "skypegraph.skype.com"; break;
 	case HOST_LOGIN:     m_szUrl = "login.microsoftonline.com"; break;
 	case HOST_TEAMS:     m_szUrl = TEAMS_BASE_HOST; break;
 	case HOST_TEAMS_API: m_szUrl = TEAMS_BASE_HOST "/api/mt/beta"; break;
@@ -130,7 +129,7 @@ MHttpResponse* CTeamsProto::DoSend(AsyncHttpRequest *pReq)
 	case HOST_TEAMS:
 	case HOST_TEAMS_API:
 		if (!pReq->FindHeader("Authorization"))
-			pReq->AddHeader("Authorization", "Bearer " + m_szAccessToken);
+		 	pReq->AddHeader("Authorization", "Bearer " + m_szAccessToken);
 		if (m_szSkypeToken)
 			pReq->AddHeader("X-Skypetoken", m_szSkypeToken);
 		pReq->AddHeader("Accept", "application/json");
@@ -145,7 +144,6 @@ MHttpResponse* CTeamsProto::DoSend(AsyncHttpRequest *pReq)
 		break;
 
 	case HOST_API:
-	case HOST_GRAPH:
 		if (m_szSkypeToken)
 			pReq->AddHeader("X-Skypetoken", m_szSkypeToken);
 		pReq->AddHeader("Accept", "application/json");
