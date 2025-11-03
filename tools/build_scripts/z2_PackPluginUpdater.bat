3b9f25b0b158670494417dd705fd1829205bff29
George Hazan <george.hazan@gmail.com>
2025-11-03 13:10:14 +0300

    fixes #5159 (PluginUpdater cannot update FreeImage)

--- tools/build_scripts/z2_PackPluginUpdater.bat
+++ tools/build_scripts/z2_PackPluginUpdater.bat
@@ -32,7 +32,7 @@ REM end Core
 REM Libs
 %ZipIt% "%Arch%\Libs\libdeltachat.zip" "Libs\libdeltachat.dll"
 %ZipIt% "%Arch%\Libs\tdapi.zip" "Libs\tdapi.mir"
-%ZipIt% "%Arch%\Libs\FreeImage.zip" "Libs\FreeImage.mir"
+%ZipIt% "%Arch%\Libs\freeimage.zip" "Libs\FreeImage.mir"
 %ZipIt% "%Arch%\Libs\libcrypto-3.zip" "Libs\libcrypto-3.mir"
 %ZipIt% "%Arch%\Libs\libssl-3.zip" "Libs\libssl-3.mir"
 REM end Libs
