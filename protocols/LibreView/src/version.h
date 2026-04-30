c974f6a3b32585f5387b5653361bc6dfab29a169
dartraiden <wowemuh@gmail.com>
2026-04-30 16:00:15 +0300

    LibreView: fix description

--- protocols/LibreView/src/version.h
+++ protocols/LibreView/src/version.h
@@ -7,7 +7,7 @@
 
 #define __PLUGIN_NAME             "LibreView"
 #define __FILENAME                "LibreView.dll"
-#define __DESCRIPTION             "Retrieves LibreView glucose data and displays it in your contact list."
+#define __DESCRIPTION             "Retrieves glucose data from LibreView service and displays it in your contact list."
 #define __AUTHOR                  "Miranda NG team"
 #define __AUTHORWEB               "https://miranda-ng.org/p/LibreView"
 #define __COPYRIGHT               "© 2026 Miranda NG team"
